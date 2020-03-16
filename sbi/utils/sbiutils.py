4b43912d9e9271737a775ba54a4c52c89069e2bb
janfb <j.f.boelts@gmail.com>
2020-03-16 17:43:36 +0100

    fix batch dim bug. add function for rejection sampling from posterior under prior

--- sbi/utils/sbiutils.py
+++ sbi/utils/sbiutils.py
@@ -1,5 +1,9 @@
-import torch.nn as nn
+import time
+from typing import Tuple
+
 import torch
+import torch.nn as nn
+
 
 # XXX standardize? zscore?
 # XXX want to insert it in Sequential
@@ -14,7 +18,7 @@ class Normalize(nn.Module):
         return (tensor - self.mean) / self.std
 
 
-def build_inputs_and_contexts(inputs, context, true_context, normalize):
+def build_inputs_and_contexts(inputs, context, true_context, correct_for_leakage):
     """
     Formats inputs and context into the correct shape
 
@@ -22,7 +26,7 @@ def build_inputs_and_contexts(inputs, context, true_context, normalize):
         inputs: Tensor, input variables.
         context: Tensor or None, conditioning variables. If a Tensor, it must have the same number or rows as the inputs. If None, the context is ignored.
         true_context: if context=None, replace it with true_context
-        normalize:
+        correct_for_leakage:
             If True, we normalize the output density
             by drawing samples, estimating the acceptance
             ratio, and then scaling the probability with it
@@ -30,6 +34,7 @@ def build_inputs_and_contexts(inputs, context, true_context, normalize):
     Returns:
         inputs, context as torch.tensors
     """
+
     inputs = torch.as_tensor(inputs)
 
     if len(inputs.shape) == 1:
@@ -37,26 +42,24 @@ def build_inputs_and_contexts(inputs, context, true_context, normalize):
             None,
         ]  # append batch dimension
 
-    if context is not None:
+    # use "default context" if None is provided
+    if context is None:
+        context = true_context
 
-        if len(context.shape) > 1 and context.shape[0] > 1 and normalize:
-            # if multiple observations, with snape avoid expensive leakage
-            # correction by rejection sampling
-            raise ValueError(
-                "Only a single context is allowed for log-prob when normalizing the density. "
-                "Please use a for-loop over your inputs and contexts."
-            )
+    # if multiple observations, with snape avoid expensive leakage
+    # correction by rejection sampling
+    if len(context.shape) > 1 and context.shape[0] > 1 and correct_for_leakage:
+        raise ValueError(
+            "Only a single context is allowed for log-prob when normalizing the density. "
+            "Please use a for-loop over your inputs and contexts."
+        )
 
-        if len(context.shape) == 1:
-            context = context[
-                None,
-            ]  # append batch dimension
-        context = torch.as_tensor(context)
-    else:
-        # if observation x1 is not provided, use the x0 used for training
-        context = true_context[
+    # @ append batch dim
+    if len(context.shape) == 1:
+        context = context[
             None,
         ]
+    context = torch.as_tensor(context)
 
     if context.shape[0] != inputs.shape[0]:
         # multiple parameters, single observation:
@@ -71,3 +74,74 @@ def build_inputs_and_contexts(inputs, context, true_context, normalize):
         )
 
     return inputs, context
+
+
+def sample_posterior_within_prior(
+    posterior_nn: torch.nn.Module,
+    prior: torch.distributions.Distribution,
+    context: torch.Tensor,
+    num_samples: int = 1,
+    patience: int = 5,
+) -> Tuple[torch.Tensor, float]:
+    """Generate samples from a posterior that have support under the prior using rejection sampling. 
+    
+    This is relevant for posteriors with leakage outside the prior:
+    Sample for the posterior (torch neural net) and reject samples if outside of prior support. Estimate acceptance prob by counting accepted samples, used e.g., for correcting the posterior density for evaluation. 
+    
+    Arguments:
+        posterior_nn {torch.nn.Module} -- neural net representing the posterio
+        prior {torch.distributions.Distribution} -- torch distribution prior
+        context {torch.Tensor} -- context for the posterior, i.e., the observed data to condition on. 
+    
+    Keyword Arguments:
+        num_samples {int} -- number of sample to generate (default: {1})
+        patience {int} -- anker in time in case sampling takes too long due to strong leakage (default: {5})
+    
+    Returns:
+        Tuple[torch.Tensor, float] -- Accepted samples, and estimated acceptance probability
+    """
+
+    # turn on nn eval mode
+    posterior_nn.eval()
+
+    samples = []
+    num_accepted = 0
+    num_sampled = 0
+    tstart = time.time()
+    time_over = time.time() - tstart > (patience * 60)
+
+    # sample until done or patience over
+    while num_accepted < num_samples and not time_over:
+
+        n_remaining = num_samples - num_accepted
+
+        sample = posterior_nn.sample(n_remaining, context=context)
+        num_sampled += n_remaining
+
+        # get mask of samples within prior
+        mask = torch.isfinite(
+            prior.log_prob(sample)
+        )  # log prob is inf outside of prior
+        num_valid = mask.sum()
+
+        if num_valid > 0:
+            samples.append(sample[mask,].reshape(num_valid, -1))
+            num_accepted += num_valid
+
+        # update timer
+        time_over = time.time() - tstart > (patience * 60)
+
+    # accumulate list of accepted samples in single tensor
+    samples = torch.stack(samples).reshape(num_accepted, -1)
+
+    # estimate acceptance probability
+    acceptance_prob = float(num_accepted / num_sampled)
+
+    # turn back on training mode
+    posterior_nn.train()
+
+    assert (
+        samples.shape[0] == num_samples
+    ), f"sampling from posterior within prior with patience {patience} failed : {samples.shape[0]} vs. {num_samples}."
+
+    return samples, acceptance_prob
