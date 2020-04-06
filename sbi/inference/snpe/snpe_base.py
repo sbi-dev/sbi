provide data and fixed normalization

sbi/inference/snpe/snpe_base.py
@@ -28,6 +28,7 @@ class SnpeBase:
         simulator,
         prior,
         true_observation,
+        pilot_data=None,
         num_pilot_samples=100,
         density_estimator=None,
         calibration_kernel=None,
@@ -78,6 +79,7 @@ class SnpeBase:
         self._simulator = set_simulator_attributes(simulator, prior, true_observation)
         self._prior = prior
         self._true_observation = true_observation
+        self._pilot_data = pilot_data
         self._device = get_default_device() if device is None else device
         self.z_score_obs = z_score_obs
         self._simulation_batch_size = simulation_batch_size
@@ -89,14 +91,20 @@ class SnpeBase:
         self._model_bank = []
         self._retrain_from_scratch_each_round = retrain_from_scratch_each_round
 
-        # run prior samples
-        (self.pilot_parameters, self.pilot_observations,) = simulate_in_batches(
-            simulator=self._simulator,
-            parameter_sample_fn=lambda num_samples: self._prior.sample((num_samples,)),
-            num_samples=num_pilot_samples,
-            simulation_batch_size=self._simulation_batch_size,
-            x_dim=self._true_observation.shape[1:],  # do not pass batch_dim
-        )
+        if pilot_data is not None:
+            self.pilot_parameters = self._pilot_data[0][: self.num_pilot_samples]
+            self.pilot_observations = self._pilot_data[1][: self.num_pilot_samples]
+        else:
+            # run prior samples
+            (self.pilot_parameters, self.pilot_observations,) = simulate_in_batches(
+                simulator=self._simulator,
+                parameter_sample_fn=lambda num_samples: self._prior.sample(
+                    (num_samples,)
+                ),
+                num_samples=num_pilot_samples,
+                simulation_batch_size=self._simulation_batch_size,
+                x_dim=self._true_observation.shape[1:],  # do not pass batch_dim
+            )
 
         # create the deep neural density estimator
         if density_estimator is None:
@@ -185,7 +193,20 @@ class SnpeBase:
             tbar.set_description(round_description)
 
             # run simulations for the round
-            self._run_sims(round_, num_simulations_per_round[round_])
+            if self._pilot_data is not None:
+                self._parameter_bank.append(
+                    self._pilot_data[0][: num_simulations_per_round[round_]]
+                )
+                self._observation_bank.append(
+                    self._pilot_data[1][: num_simulations_per_round[round_]]
+                )
+                self._prior_masks.append(
+                    torch.ones(num_simulations_per_round[round_], 1)
+                    if round_ == 0
+                    else torch.zeros(num_simulations_per_round[round_], 1)
+                )
+            else:
+                self._run_sims(round_, num_simulations_per_round[round_])
 
             # Fit posterior using newly aggregated data set.
             self._train(round_=round_, **kwargs)
@@ -203,6 +224,14 @@ class SnpeBase:
                 f"Best validation performance: {self._summary['best-validation-log-probs'][-1]:.4f}\n\n"
             )
 
+            # aaa = self._neural_posterior.get_leakage_correction(
+            #     context=self._true_observation, num_rejection_samples=100
+            # )
+
+            print("starting to sample")
+            mysample = self._neural_posterior.sample(1)
+            print("mysample", mysample)
+
             # Update tensorboard and summary dict.
             self._summary_writer, self._summary = utils.summarize(
                 summary_writer=self._summary_writer,
@@ -213,7 +242,7 @@ class SnpeBase:
                 observation_bank=self._observation_bank,
                 simulator=self._simulator,
                 posterior_samples_acceptance_rate=self._neural_posterior.get_leakage_correction(
-                    context=self._true_observation
+                    context=self._true_observation, num_rejection_samples=20,
                 ),
             )
 
@@ -388,6 +417,8 @@ class SnpeBase:
         converged = False
         while not converged:
 
+            print("========= Starting new epoch ===========")
+
             # Train for a single epoch.
             self._neural_posterior.neural_net.train()
             for batch in train_loader:
@@ -439,6 +470,8 @@ class SnpeBase:
                     log_prob_sum += log_prob.sum().item()
             validation_log_prob = log_prob_sum / num_validation_examples
 
+            print("==== Validation log_prob:  ", validation_log_prob, "====")
+
             # Check for improvement in validation performance over previous epochs.
             if validation_log_prob > best_validation_log_prob:
                 best_validation_log_prob = validation_log_prob
@@ -457,6 +490,8 @@ class SnpeBase:
         # Update summary.
         self._summary["epochs"].append(epochs)
         self._summary["best-validation-log-probs"].append(best_validation_log_prob)
+
+        print("==================================== Done")
 
 
 class PotentialFunctionProvider:

tests/test_linearGaussian_apt.py
@@ -10,9 +10,47 @@ from sbi.simulators.linear_gaussian import (
     get_true_posterior_samples_linear_gaussian_uniform_prior,
     linear_gaussian,
 )
-import tests.utils_for_testing.linearGaussian_logprob as test_utils
 
 torch.manual_seed(0)
+import numpy as np
+
+
+def load_data():
+    # load the training data
+    datafile = "/Users/deismic/Documents/Studium_TUM/MasterThesis/prinzetal/results/energy/classifier/prior_simulated/samples_array_subset1.npz"
+    data = np.load(datafile)
+
+    params = data["params"]
+    stats = data["stats"]
+
+    params = torch.as_tensor(params, dtype=torch.float32)
+    stats = torch.as_tensor(stats, dtype=torch.float32)
+
+    # define observation
+    x_o = np.load(
+        "/Users/deismic/Documents/Studium_TUM/MasterThesis/prinzetal/thesis_results/experimental/summstats/845_082/190807_summstats_prep845_082_0044.npz"
+    )["summ_stats"]
+    x_o = torch.as_tensor(x_o, dtype=torch.float32).unsqueeze(0)
+
+    x_o = (x_o - torch.mean(stats, dim=0)) / torch.std(stats, dim=0)
+    params = (params - torch.mean(params, dim=0)) / torch.std(params, dim=0)
+    stats = (stats - torch.mean(stats, dim=0)) / torch.std(stats, dim=0)
+
+    prior = utils.torchutils.BoxUniform(
+        low=-1.73 * torch.ones(31), high=1.73 * torch.ones(31)
+    )
+
+    num_params = 3
+    num_stats = 3
+    params = params[:, :num_params]
+    stats = stats[:, :num_stats]
+    prior = utils.torchutils.BoxUniform(
+        low=-1.73 * torch.ones(num_params), high=1.73 * torch.ones(num_params)
+    )
+    x_o = x_o[:, :num_stats]
+
+    return params, stats, x_o, prior
+
 
 # running all combinations is excessive. The standard test is (3, "gaussian", "snpe_c"),
 # and we then vary only one parameter at a time to test single-d, uniform, and snpe-b
@@ -31,50 +69,37 @@ def test_apt_on_linearGaussian_based_on_mmd(
 ):
     """Test whether APT infers well a simple example where ground truth is available."""
 
-    true_observation = torch.zeros(num_dim)
+    x_o = torch.zeros(num_dim)
     num_samples = 100
 
-    if prior_str == "gaussian":
-        prior = distributions.MultivariateNormal(
-            loc=torch.zeros(num_dim), covariance_matrix=torch.eye(num_dim)
-        )
-        target_samples = get_true_posterior_samples_linear_gaussian_mvn_prior(
-            true_observation, num_samples=num_samples
-        )
-    else:
-        prior = utils.BoxUniform(-1.0 * torch.ones(num_dim), torch.ones(num_dim))
-        target_samples = get_true_posterior_samples_linear_gaussian_uniform_prior(
-            true_observation, num_samples=num_samples, prior=prior
-        )
+    prior = utils.BoxUniform(-1.73 * torch.ones(num_dim), -1.73 * torch.ones(num_dim))
 
-    neural_net = utils.posterior_nn(model="maf", prior=prior, context=true_observation)
+    neural_net = utils.posterior_nn(model="nsf", prior=prior, context=x_o)
 
-    if algorithm_str == "snpe_b":
-        snpe = SnpeB(
-            simulator=linear_gaussian,
-            true_observation=true_observation,
-            density_estimator=neural_net,
-            prior=prior,
-            z_score_obs=True,
-            simulation_batch_size=simulation_batch_size,
-            use_combined_loss=False,
-            retrain_from_scratch_each_round=False,
-            discard_prior_samples=False,
-        )
-    elif algorithm_str == "snpe_c":
-        snpe = SnpeC(
-            simulator=linear_gaussian,
-            true_observation=true_observation,
-            density_estimator=neural_net,
-            prior=prior,
-            num_atoms=-1,
-            z_score_obs=True,
-            simulation_batch_size=simulation_batch_size,
-            use_combined_loss=False,
-            retrain_from_scratch_each_round=False,
-            discard_prior_samples=False,
-            train_with_mcmc=False,
-        )
+    dataset_params = prior.sample(torch.tensor([1000]))
+    dataset_stats = linear_gaussian(dataset_params)
+    print("dataset_params", dataset_params)
+    print("dataset_stats", dataset_stats)
+    print("x_o", x_o)
+
+    dataset_params, dataset_stats, x_o, prior = load_data()
+
+    print("dataset_params", dataset_params)
+    print("dataset_stats", dataset_stats)
+    print("x_o", x_o)
+
+    def dummy_simulate(parameter_vals):
+        return 3 * torch.ones((1, 3))
+
+    snpe = SnpeC(
+        pilot_data=(dataset_params, dataset_stats),
+        simulator=dummy_simulate,
+        true_observation=x_o,
+        density_estimator=neural_net,
+        prior=prior,
+        z_score_obs=False,
+        train_with_mcmc=False,
+    )
 
     # run inference
     num_rounds, num_simulations_per_round = 1, 1000
@@ -83,58 +108,12 @@ def test_apt_on_linearGaussian_based_on_mmd(
     )
 
     # draw samples from posterior
-    samples = posterior.sample(num_samples)
+    samples = posterior.sample(1)
 
-    # compute the mmd
-    mmd = utils.unbiased_mmd_squared(target_samples, samples)
+    print("=======sample", samples)
 
-    # check if mmd is larger than expected
-    max_mmd = 0.03
 
-    print("mmd for apt is:  ", mmd)
-
-    assert (
-        mmd < max_mmd
-    ), f"MMD={mmd} is more than 2 stds above the average performance."
-
-    # Checks for log_prob()
-    if prior_str == "gaussian":
-        # For the Gaussian prior, we compute the D-KL between ground truth and posterior
-        dkl = test_utils.get_dkl_gaussian_prior(posterior, true_observation, num_dim)
-
-        max_dkl = 0.05 if num_dim == 1 else 0.8
-
-        assert (
-            dkl < max_dkl
-        ), f"D-KL={dkl} is more than 2 stds above the average performance."
-
-    elif prior_str == "uniform":
-        # Check whether the returned probability outside of the support is zero
-        posterior_prob = test_utils.get_prob_outside_uniform_prior(posterior, num_dim)
-        assert (
-            posterior_prob == 0.0
-        ), "The posterior probability outside of the prior support is not zero"
-
-        # Check whether normalization (i.e. scaling up the density due
-        # to leakage into regions without prior support) scales up the density by the
-        # correct factor.
-        (
-            posterior_likelihood_unnorm,
-            posterior_likelihood_norm,
-            acceptance_prob,
-        ) = test_utils.get_normalization_uniform_prior(
-            posterior, prior, true_observation
-        )
-        # The acceptance probability should be *exactly* the ratio of the unnormalized
-        # and the normalized likelihood. However, we allow for an error margin of 1%,
-        # since the estimation of the acceptance probability is random (based on
-        # rejection sampling).
-        assert (
-            acceptance_prob * 0.99
-            < posterior_likelihood_unnorm / posterior_likelihood_norm
-            < acceptance_prob * 1.01
-        ), "Normalizing the posterior density using the acceptance probability failed."
-
+test_apt_on_linearGaussian_based_on_mmd(3, "uniform", "snpe_c", 10)
 
 # test multi-round SNPE
 @pytest.mark.slow