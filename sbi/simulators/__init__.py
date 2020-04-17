88092d4e38ede199c1c2a9cb76a09147b3e44242
janfb <j.f.boelts@gmail.com>
2020-04-17 15:41:08 +0200

    remove nonlinear gaussian deprecated simulator base.

--- sbi/simulators/__init__.py
+++ sbi/simulators/__init__.py
@@ -1,3 +1,2 @@
 from sbi.simulators.linear_gaussian import linear_gaussian
-from sbi.simulators.nonlinear_gaussian import NonlinearGaussianSimulator
 from sbi.simulators.simutils import simulate_in_batches
