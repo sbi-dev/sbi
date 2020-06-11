ef28a2ebc644764a50ea3ea59ba755e2a7754b9c
Alvaro Tejero-Cantero <alvaro.tejero@tum.de>
2020-06-11 17:41:45 +0200

    Added short license notice.

--- sbi/mcmc/slice_numpy.py
+++ sbi/mcmc/slice_numpy.py
@@ -1,3 +1,6 @@
+# This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
+# under the Affero General Public License v3, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 import os
 import sys
@@ -39,7 +42,7 @@ class MCMCSampler:
         """
         Generates MCMC samples. Should be implemented in a subclass.
         """
-        raise NotImplementedError('Should be implemented as a subclass.')
+        raise NotImplementedError("Should be implemented as a subclass.")
 
 
 class SliceSampler(MCMCSampler):
