cac4a9375f43bf411af88b1500cc2d9b42592975
Alvaro Tejero-Cantero <alvaro.tejero@tum.de>
2020-06-11 17:22:25 +0200

    Ensure Py3.7 perf with type annotations; snippet 'annf<TAB>'.

--- sbi/inference/base.py
+++ sbi/inference/base.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from abc import ABC
 from copy import deepcopy
 from datetime import datetime
--- sbi/user_input/user_input_checks.py
+++ sbi/user_input/user_input_checks.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from typing import cast, Callable, Optional, Sequence, Tuple
 import warnings
 
