b03a361b60c4d9b4d86242b95a6eae32c733947d
janfb <jan.boelts@tum.de>
2020-06-19 15:35:50 +0200

    add smcabc and tests.

--- tests/abc_test.py
+++ tests/abc_test.py
@@ -1,5 +1,4 @@
 import pytest
-import torch
 from torch import eye, ones, zeros
 
 from sbi.simulators.linear_gaussian import (
@@ -7,15 +6,15 @@ from sbi.simulators.linear_gaussian import (
     true_posterior_linear_gaussian_mvn_prior,
 )
 
-from sbi.inference.abc.mcabc import MCABC
+from sbi.inference import MCABC, SMCABC
 from torch.distributions import MultivariateNormal
-from pyro.distributions import Empirical
 from tests.test_utils import check_c2st
 
 
 @pytest.mark.parametrize("num_dim", (1, 2))
 def test_mcabc_inference_on_linear_gaussian(num_dim):
     x_o = zeros((1, num_dim))
+    num_samples = 1000
 
     # likelihood_mean will be likelihood_shift+theta
     likelihood_shift = -1.0 * ones(num_dim)
@@ -27,7 +26,7 @@ def test_mcabc_inference_on_linear_gaussian(num_dim):
     gt_posterior = true_posterior_linear_gaussian_mvn_prior(
         x_o[0], likelihood_shift, likelihood_cov, prior_mean, prior_cov
     )
-    target_samples = gt_posterior.sample((10000,))
+    target_samples = gt_posterior.sample((num_samples,))
 
     def simulator(theta):
         return linear_gaussian(theta, likelihood_shift, likelihood_cov)
@@ -36,5 +35,35 @@ def test_mcabc_inference_on_linear_gaussian(num_dim):
 
     phat = infer(1000000, quantile=0.001)
 
-    check_c2st(phat.sample((10000,)), target_samples, alg="MCABC")
+    check_c2st(phat.sample((num_samples,)), target_samples, alg="MCABC")
 
+
+@pytest.mark.parametrize("num_dim", (1, 2))
+def test_smcabc_inference_on_linear_gaussian(num_dim):
+    x_o = zeros((1, num_dim))
+    num_samples = 1000
+    likelihood_shift = -1.0 * ones(num_dim)
+    likelihood_cov = 0.3 * eye(num_dim)
+
+    prior_mean = zeros(num_dim)
+    prior_cov = eye(num_dim)
+    prior = MultivariateNormal(loc=prior_mean, covariance_matrix=prior_cov)
+    gt_posterior = true_posterior_linear_gaussian_mvn_prior(
+        x_o[0], likelihood_shift, likelihood_cov, prior_mean, prior_cov
+    )
+    target_samples = gt_posterior.sample((num_samples,))
+
+    def simulator(theta):
+        return linear_gaussian(theta, likelihood_shift, likelihood_cov)
+
+    infer = SMCABC(simulator, prior, x_o, simulation_batch_size=10000)
+
+    phat = infer(
+        num_particles=1000,
+        num_initial_pop=5000,
+        epsilon_decay=0.5,
+        num_simulation_budget=30000,
+        qt_decay=True,
+    )
+
+    check_c2st(phat.sample((num_samples,)), target_samples, alg="SMCABC")
