4b1113357c58ce902c9b470ea100fe5a232c6777
michael <michael.deistler@t-online.de>
2020-09-10 16:02:42 +0200

    3D animation for tutorial

--- tutorials/toy_posterior_for_07_cc.py
+++ tutorials/toy_posterior_for_07_cc.py
@@ -20,7 +20,8 @@ class ExamplePosterior:
 
     def __init__(self):
         # Vector that is orthogonal to the plane that has high probability.
-        self.normal_vec = torch.tensor([1, 0.5, -1, -2, 1, 1.5, 1, -1])
+        self.normal_vec = torch.tensor([0.9, 1.1, -1.0])
+        self.noise_factor = 1.0
 
     def sample(self, sample_shape: torch.Size):
         """
@@ -30,8 +31,8 @@ class ExamplePosterior:
         hyperplane in the 8D parameter space. We then accept with probability
         (1.-distance). If the distance is larger than 1.0, we never accept.
         """
-        num_dim = 8
-        all_samples = torch.empty(0, 8)
+        num_dim = 3
+        all_samples = torch.empty(0, num_dim)
         num_accepted = 0
         while num_accepted < sample_shape[0]:
             proposed_samples = utils.BoxUniform(
@@ -39,7 +40,9 @@ class ExamplePosterior:
             ).sample(sample_shape)
             vec_prior_samples = proposed_samples * self.normal_vec
             dist_to_zero = torch.abs(torch.sum(vec_prior_samples, dim=1))
-            accept_or_not = torch.rand(dist_to_zero.shape) > dist_to_zero
+            accept_or_not = (
+                self.noise_factor * torch.rand(dist_to_zero.shape) > dist_to_zero
+            )
             accepted_samples = proposed_samples[accept_or_not]
 
             num_accepted += accepted_samples.shape[0]
@@ -56,7 +59,9 @@ class ExamplePosterior:
         """
         vec_prior_samples = theta * self.normal_vec
         dist_to_zero = torch.abs(torch.sum(vec_prior_samples, dim=1))
-        acceptance_prob = torch.max(torch.zeros(dist_to_zero.shape), 1 - dist_to_zero)
+        acceptance_prob = torch.max(
+            torch.zeros(dist_to_zero.shape), self.noise_factor - dist_to_zero
+        )
         return torch.log(acceptance_prob)
 
     def set_default_x(self, x: Tensor):
