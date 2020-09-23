51f0fe2e09281f4971f9480b329500e495895866
Jan-Matthis <mail@jan-matthis.de>
2020-09-23 14:33:47 +0200

    Add vectorized slice sampler as an option

--- sbi/mcmc/__init__.py
+++ sbi/mcmc/__init__.py
@@ -1,3 +1,4 @@
 from sbi.mcmc.slice_numpy import SliceSampler
+from sbi.mcmc.slice_numpy_vectorized import SliceSamplerVectorized
 from sbi.mcmc.slice import Slice
 from sbi.mcmc.init_strategy import prior_init, sir
