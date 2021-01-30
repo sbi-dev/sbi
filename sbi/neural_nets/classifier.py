a9248900eb35cddebccefaf111e9819e2f5d4aee
janfb <j.f.boelts@gmail.com>
2021-01-30 15:08:37 +0100

    small fix kwargs, documentation.

--- sbi/neural_nets/classifier.py
+++ sbi/neural_nets/classifier.py
@@ -17,11 +17,12 @@ class StandardizeInputs(nn.Module):
         self.dim_x = dim_x
         self.dim_y = dim_y
 
-    def forward(self, t):
-        assert type(t) is list
-        assert len(t) == 2
+    def forward(self, inputs: list) -> Tensor:
+        assert (
+            type(inputs) is list and len(inputs) == 2
+        ), """Inputs to (s)nre classifier must be a list containing raw theta and x."""
         out = torch.cat(
-            [self.embedding_net_x(t[0]), self.embedding_net_y(t[1]),], dim=1,
+            [self.embedding_net_x(inputs[0]), self.embedding_net_y(inputs[1]),], dim=1,
         )
         return out
 
