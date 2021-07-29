Sampling methods failed, change likelihood if logprob of self is called instead of _q ??? Fixed

sbi/inference/posteriors/ratio_based_posterior.py
@@ -159,7 +159,6 @@ class RatioBasedPosterior(NeuralPosterior):
         mcmc_parameters: Optional[Dict[str, Any]] = None,
         rejection_sampling_parameters: Optional[Dict[str, Any]] = None,
         vi_parameters: Optional[Dict[str, Any]] = None,
-
     ) -> Tensor:
         r"""
         Return samples from posterior distribution $p(\theta|x)$ with MCMC.
@@ -222,10 +221,8 @@ class RatioBasedPosterior(NeuralPosterior):
                 **mcmc_parameters,
             )
         elif sample_with == "rejection":
-            rejection_sampling_parameters = (
-                self._potentially_replace_rejection_parameters(
-                    rejection_sampling_parameters
-                )
+            rejection_sampling_parameters = self._potentially_replace_rejection_parameters(
+                rejection_sampling_parameters
             )
             if "proposal" not in rejection_sampling_parameters:
                 rejection_sampling_parameters["proposal"] = self._prior
@@ -238,10 +235,10 @@ class RatioBasedPosterior(NeuralPosterior):
                 **rejection_sampling_parameters,
             )
         elif sample_with == "vi":
-            # TODO check if train was called before. And warn if not
+            # TODO Check if train was called. And warn if not
             vi_parameters = self._potentially_replace_vi_parameters(vi_parameters)
-            method = vi_parameters.get("method", "naive")
-            method_params = vi_parameters.get("method_params", {})
+            method = vi_parameters.get("sampling_method", "naive")
+            method_params = vi_parameters.get("sampling_method_params", {})
             track_gradients = vi_parameters.get("track_gradients", False)
             sample_shape = torch.Size(sample_shape)
             if method.lower() == "naive":
@@ -250,18 +247,23 @@ class RatioBasedPosterior(NeuralPosterior):
                 else:
                     samples = self._q.sample(sample_shape)
             elif method.lower() == "ir":
-                potential_fn = potential_fn_provider(self._prior, self.net, x, "rejection")
+                potential_fn = potential_fn_provider(self._prior, self.net, x, "vi")
                 samples = importance_resampling(
-                    sample_shape.numel(), potential_fn=potential_fn, proposal=self._q, **method_params
+                    sample_shape.numel(),
+                    potential_fn=potential_fn,
+                    proposal=self._q,
+                    **method_params,
                 )
             elif method.lower() == "imh":
-                 potential_fn = potential_fn_provider(self._prior, self.net, x, "rejection")
-                 samples = independent_mh(sample_shape.numel(), potential_fn,self._q, **method_params)
+                potential_fn = potential_fn_provider(self._prior, self.net, x, "vi")
+                samples = independent_mh(
+                    sample_shape.numel(), potential_fn, self._q, **method_params
+                )
             elif method.lower() == "rejection":
                 rejection_sampling_parameters = self._potentially_replace_rejection_parameters(
                     rejection_sampling_parameters
                 )
-                rejection_sampling_parameters["proposal"] = self
+                rejection_sampling_parameters["proposal"] = self._q
                 samples, _ = rejection_sample(
                     potential_fn=potential_fn_provider(
                         self._prior, self.net, x, "rejection"
@@ -270,12 +272,14 @@ class RatioBasedPosterior(NeuralPosterior):
                     **rejection_sampling_parameters,
                 )
             elif method.lower() == "slice":
-                potential_fn = potential_fn_provider(self._prior, self.net, x, "rejection")
+                potential_fn = potential_fn_provider(self._prior, self.net, x, "vi")
                 samples = random_direction_slice_sampler(
                     sample_shape.numel(), potential_fn, self._q, **method_params
                 )
             else:
-                raise NotImplementedError("The sampling methods from the vi posterior are currently restricted to naive, ir, imh, rejection and slice")
+                raise NotImplementedError(
+                    "The sampling methods from the vi posterior are currently restricted to naive, ir, imh, rejection and slice"
+                )
         else:
             raise NameError(
                 "The only implemented sampling methods are `mcmc`, `rejection` and `vi`."
@@ -449,10 +453,7 @@ class RatioBasedPosterior(NeuralPosterior):
 
     @staticmethod
     def _log_ratios_over_trials(
-        x: Tensor,
-        theta: Tensor,
-        net: nn.Module,
-        track_gradients: bool = False,
+        x: Tensor, theta: Tensor, net: nn.Module, track_gradients: bool = False,
     ) -> Tensor:
         r"""Return log ratios summed over iid trials of `x`.
 
@@ -512,11 +513,7 @@ class PotentialFunctionProvider:
     """
 
     def __call__(
-        self,
-        prior,
-        classifier: nn.Module,
-        x: Tensor,
-        method: str,
+        self, prior, classifier: nn.Module, x: Tensor, method: str,
     ) -> Callable:
         r"""Return potential function for posterior $p(\theta|x)$.
 
@@ -546,6 +543,8 @@ class PotentialFunctionProvider:
             return partial(self.posterior_potential, track_gradients=False)
         elif method == "rejection":
             return partial(self.posterior_potential, track_gradients=True)
+        elif method == "vi":
+            return partial(self.posterior_potential, track_gradients=False)
         else:
             NotImplementedError
 

sbi/vi/divergence_optimizers.py
@@ -180,16 +180,17 @@ class DivergenceOptimizer(ABC):
 
         # Loss and summary
         self.eps = eps
+        self.num_step = 0
+        self.warm_up_was_done = False
         self.losses = np.ones(2000)
         self.moving_average = np.ones(2000)
         self.moving_std = np.ones(2000)
         self.moving_slope = np.ones(2000)
-        self.num_step = 0
 
         # The loss that will be used
         self._loss = lambda x: torch.zeros(1), torch.zeros(1)
         # Hyperparameters to change adaptively
-        self.HYPER_PARAMETERS = ["n_particles", "clip_value"]
+        self.HYPER_PARAMETERS = ["n_particles", "clip_value", "eps"]
 
     @abstractmethod
     def _generate_loss_function(self):
@@ -233,6 +234,8 @@ class DivergenceOptimizer(ABC):
                 loss = -torch.mean(self.q.log_prob(samples))
             loss.backward()
             self._optimizer.step()
+        # Denote that warmup was already done
+        self.warm_up_was_done = True
 
     def update_state(self):
         """ This updates the current state. """
@@ -309,6 +312,7 @@ class DivergenceOptimizer(ABC):
         self.moving_average = np.ones(2000)
         self.moving_std = np.ones(2000)
         self.moving_slope = np.ones(2000)
+        self.num_step = 0
 
     def update_loss_stats(self, loss, window=20):
         """Updates current loss statistics of the optimizer
@@ -318,14 +322,11 @@ class DivergenceOptimizer(ABC):
             window: Window size for running statistics
         """
         if self.num_step >= len(self.losses):
-            self.losses = np.append(self.losses, np.zeros(len(self.losses)))
-            self.moving_average = np.append(
-                self.moving_average, np.zeros(len(self.moving_average))
-            )
-            self.moving_std = np.append(self.moving_std, np.zeros(len(self.moving_std)))
-            self.moving_slope = np.append(
-                self.moving_slope, np.zeros(len(self.moving_slope))
-            )
+            self.losses = np.append(self.losses, np.zeros(2000))
+            self.moving_average = np.append(self.moving_average, np.zeros(2000))
+            self.moving_std = np.append(self.moving_std, np.zeros(2000))
+            self.moving_slope = np.append(self.moving_slope, np.zeros(2000))
+
         if self.num_step == 0:
             self.losses[self.num_step] = loss
             self.moving_average[self.num_step] = loss
@@ -364,17 +365,21 @@ class DivergenceOptimizer(ABC):
         for key, val in kwargs.items():
             if key in self.HYPER_PARAMETERS:
                 paras[key] = val
-            if key in self._scheduler.__dict__:
-                self._scheduler.__dict__[key] = val
-            if key in self._optimizer.defaults:
-                for para in self._optimizer.param_groups:
-                    para[key] = val
+
             if key == "self":
                 posterior = kwargs[key]
                 self.posterior = posterior
                 self.q = posterior._q
                 self.set_likelihood_fn(posterior)
                 self.prior = posterior._prior
+        opt_kwargs = filter_kwrags_for_func(type(self._optimizer).__init__, kwargs)
+        scheduler_kwargs = filter_kwrags_for_func(
+            type(self._scheduler).__init__, kwargs
+        )
+        scheduler_kwargs["gamma"] = scheduler_kwargs.get("gamma", 1.0)
+        self._optimizer = type(self._optimizer)(self.q.parameters(), **opt_kwargs)
+        self._scheduler = type(self._scheduler)(self._optimizer, **scheduler_kwargs)
+        self._scheduler._step_count = 2
 
 
 class ElboOptimizer(DivergenceOptimizer):
@@ -850,14 +855,13 @@ class FDivergenceOptimizer(ElboOptimizer):
     def _loss_TV(self, x_obs):
         """ This is the loss to minimize the total variation"""
         particles = self.generate_elbo_particles(x_obs).exp()
-        surrogate_loss = torch.mean(torch.abs(particles-1))
+        surrogate_loss = torch.mean(torch.abs(particles - 1))
         return surrogate_loss, surrogate_loss.detach()
 
     def _loss_KL_pol2(self, x_obs):
         particles = self.generate_elbo_particles(x_obs)
-        surrogate_loss = torch.mean(particles**2 + particles) 
+        surrogate_loss = torch.mean(particles ** 2 + particles)
         return surrogate_loss, surrogate_loss.detach()
-
 
     def _loss_JS(self, x_obs, domain=1000):
         """ This at most is something proportional to the JS
@@ -880,5 +884,4 @@ class FDivergenceOptimizer(ElboOptimizer):
         else:
             loss = surrogate_loss
         return surrogate_loss, loss.clone().detach()
-
 

sbi/vi/sampling.py
@@ -186,15 +186,9 @@ def independent_mh(num_samples, potential_fn, proposal, T=20, num_samples_batch=
     samples = proposal.sample((num_samples_batch,))
     sampling_steps = int(T / (int(num_samples / num_samples_batch)))
     for t in range(T):
-        potential = (
-            potential_fn(samples)
-            - proposal.log_prob(samples)
-        )
+        potential = potential_fn(samples) - proposal.log_prob(samples)
         new_samples = proposal.sample((num_samples_batch,))
-        new_potential = (
-            potential_fn(new_samples)
-            - proposal.log_prob(new_samples)
-        )
+        new_potential = potential_fn(new_samples) - proposal.log_prob(new_samples)
         acceptance_probability = torch.exp(new_potential - potential)
         u = torch.rand(num_samples_batch)
         mask = u < acceptance_probability
@@ -215,7 +209,7 @@ def random_direction_slice_sampler(
 
     for t in range(T):
         potential_function = potential_fn(samples)
-        y = torch.rand(num_samples) * potential_function.exp()
+        y = torch.rand(num_samples_batch) * potential_function.exp()
 
         random_directions = torch.randn(samples.shape)
         random_directions /= torch.linalg.norm(random_directions, dim=-1).unsqueeze(-1)
@@ -223,7 +217,6 @@ def random_direction_slice_sampler(
         ub = torch.zeros((num_samples_batch,))
         mask_lb = y < potential_function.exp()
         mask_ub = mask_lb.clone()
-        
 
         while mask_lb.any() and mask_ub.any():
             lb[mask_lb] -= steps
@@ -240,14 +233,11 @@ def random_direction_slice_sampler(
             potential_function_lb = potential_fn(proposed_samples_lb)
             potential_function_ub = potential_fn(proposed_samples_ub)
 
-
             mask_lb[mask_lb.clone()] = y[mask_lb] < potential_function_lb.exp()
             mask_ub[mask_ub.clone()] = y[mask_ub] < potential_function_ub.exp()
-        x = torch.rand(num_samples) * (ub - lb) + lb
+        x = torch.rand(num_samples_batch) * (ub - lb) + lb
         samples = samples + x.unsqueeze(-1) * random_directions
         if ((t + 1) % sampling_steps) == 0:
             final_samples.append(samples)
     return torch.vstack(final_samples)
-
-
 