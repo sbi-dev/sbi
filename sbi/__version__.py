eabb033440a8fe005f0f1a2e51cf0b286c76f78f
janfb <jan.boelts@tum.de>
2021-11-11 17:17:25 +0100

    bump version

--- sbi/__version__.py
+++ sbi/__version__.py
@@ -1,6 +1,6 @@
 # This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
 # under the Affero General Public License v3, see <https://www.gnu.org/licenses/>.
 
-VERSION = (0, 17, 1)
+VERSION = (0, 17, 2)
 
 __version__ = ".".join(map(str, VERSION))
