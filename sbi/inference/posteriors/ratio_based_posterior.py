23f5db34b834491e634a96991e4ab3523672bf16
michael.deistler <michael.deistler@uni-tuebingen.de>
2021-12-16 14:40:47 +0100

    Fix for rejection sampling

--- sbi/inference/posteriors/ratio_based_posterior.py
+++ sbi/inference/posteriors/ratio_based_posterior.py
@@ -473,7 +473,7 @@ class PotentialFunctionProvider:
         classifier: nn.Module,
         x: Tensor,
         method: str,
-        transform: torch_tf.Transform = torch_tf.identity_transform,
+        transform: Optional[torch_tf.Transform] = None,
     ) -> Callable:
         r"""Return potential function for posterior $p(\theta|x)$.
 
@@ -494,7 +494,12 @@ class PotentialFunctionProvider:
         self.prior = prior
         self.device = next(classifier.parameters()).device
         self.x = atleast_2d(x).to(self.device)
-        self.transform = transform
+        if transform is None:
+            self.transform = torch_tf.IndependentTransform(
+                torch_tf.identity_transform, reinterpreted_batch_ndims=1
+            )
+        else:
+            self.transform = transform
 
         if method == "slice":
             return partial(self.pyro_potential, track_gradients=False)
