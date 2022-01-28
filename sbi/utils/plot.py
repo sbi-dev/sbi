6eab5c93fc25419aa720e88d58f1c94060d38ab3
Julia Linhart <julialinhart@MacBook-of-Julia-Linhart-2.local>
2022-01-28 11:07:30 +0100

    changes in plot.py 2

--- sbi/utils/plot.py
+++ sbi/utils/plot.py
@@ -438,7 +438,7 @@ def conditional_pairplot(
         warn_about_deprecation: With sbi v0.15.0, we depracated the import of this
             function from `sbi.utils`. Instead, it should be imported from
             `sbi.analysis`.
-        fig: matplotlib figure to plot on.
+        fig: matplotlib figure to plot on. 
         axes: matplotlib axes corresponding to fig.
         **kwargs: Additional arguments to adjust the plot, see the source code in
             `_get_default_opts()` in `sbi.utils.plot` for more details.
@@ -558,7 +558,7 @@ def _pairplot_scaffold(
         opts: Dictionary built by the functions that call `pairplot_scaffold`. Must
             contain at least `labels`, `subset`, `figsize`, `subplots`,
             `fig_subplots_adjust`, `title`, `title_format`, ..
-        fig: matplotlib figure to plot on. 
+        fig: matplotlib figure to plot on.
         axes: matplotlib axes corresponding to fig.
 
     Returns: figure and axis
