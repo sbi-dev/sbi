fc12290e61c6c0fe95429115f6bb7d30f37a61d6
janfb <jan.boelts@tum.de>
2022-06-29 18:33:01 +0200

    use only training data for neural net building.

--- sbi/inference/snpe/snpe_base.py
+++ sbi/inference/snpe/snpe_base.py
@@ -284,7 +284,11 @@ class PosteriorEstimator(NeuralInference, ABC):
 
             # Get theta,x to initialize NN
             theta, x, _ = self.get_simulations(starting_round=start_idx)
-            self._neural_net = self._build_neural_net(theta.to("cpu"), x.to("cpu"))
+            # Use only training data for building the neural net (z-scoring transforms)
+            self._neural_net = self._build_neural_net(
+                theta[self.train_indices].to("cpu"),
+                x[self.train_indices].to("cpu"),
+            )
             self._x_shape = x_shape_from_simulation(x.to("cpu"))
 
             test_posterior_net_for_multi_d_x(
