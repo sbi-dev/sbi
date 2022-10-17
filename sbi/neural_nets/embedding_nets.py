44d158bb620a7e4e49af5e24104fccb31e29bd69
coschroeder <cornelius.schroeder@gmail.com>
2022-10-17 14:47:25 +0200

    embedding_net: included suggestions from code review

--- sbi/neural_nets/embedding_nets.py
+++ sbi/neural_nets/embedding_nets.py
@@ -5,19 +5,27 @@ from torch import Size, Tensor, nn
 
 
 class FCEmbedding(nn.Module):
-    def __init__(self, input_dim: int, num_layers: int = 2, num_hiddens: int = 20):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        num_layers: int = 2,
+        num_hiddens: int = 20,
+    ):
         """Fully-connected multi-layer neural network to be used as embedding network.
 
         Args:
             input_dim: Dimensionality of input that will be passed to the embedding net.
-            num_layers: Number of layers of the embedding network.
+            output_dim: Dimensionality of the output.
+            num_layers: Number of layers of the embedding network. (Minimum of 2).
             num_hiddens: Number of hidden units in each layer of the embedding network.
         """
         super().__init__()
         layers = [nn.Linear(input_dim, num_hiddens), nn.ReLU()]
-        for _ in range(num_layers - 1):
+        for _ in range(num_layers - 2):
             layers.append(nn.Linear(num_hiddens, num_hiddens))
             layers.append(nn.ReLU())
+        layers.append(nn.Linear(num_hiddens, output_dim))
         self.net = nn.Sequential(*layers)
 
     def forward(self, x: Tensor) -> Tensor:
@@ -35,42 +43,53 @@ class FCEmbedding(nn.Module):
 
 class CNNEmbedding(nn.Module):
     def __init__(
-        self, input_dim: int, output_dim: int, num_fc: int = 2, num_hiddens: int = 120
+        self,
+        input_dim: int,
+        output_dim: int,
+        num_fc: int = 2,
+        num_hiddens: int = 120,
+        out_channels_cnn_1: int = 10,
+        out_channels_cnn_2: int = 16,
+        kernel_size: int = 5,
+        pool_size=4,
     ):
-        """multi-layer (C)NN
-            first 2 layers are convolutional, followed by fully connected layers.
+        """Multi-layer (C)NN
+            First two layers are convolutional, followed by fully connected layers.
             Performing 1d convolution and max pooling with preset configs.
+
         Args:
             input_dim: Dimensionality of input.
-            num_conv: Number of con layers.
-            num_fc: Number fully connected layer, min of 2
-            num_hiddens: number of hidden dims in fc layers
+            num_conv: Number of convolutional layers.
+            num_fc: Number fully connected layer, minimum of 2.
+            num_hiddens: Number of hidden dimensions in fully-connected layers.
+            out_channels_cnn_1: Number of oputput channels for the first convolutional
+                layer.
+            out_channels_cnn_2: Number of oputput channels for the second
+                convolutional layer.
+            kernel_size: Kernel size for both convolutional layers.
+            pool_size: pool size for MaxPool1d operation after the convolutional
+                layers.
+
+            Remark: The implementation of the convolutional layers was not tested
+            rigourously. While it works for the default configuration parameters it might cause shape conflicts fot badly chosen parameters.
         """
         super().__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.num_hiddens = num_hiddens
 
-        # configs for conv layers
-        out_channels_cnn_1 = 10
-        out_channels_cnn_2 = 16
-        kernel_size = 5
-        pool_size = 4
-
-        # construct conv-pool subnet
+        # construct convolutional-pooling subnet
         pool = nn.MaxPool1d(pool_size)
         conv_layers = [
             nn.Conv1d(1, out_channels_cnn_1, kernel_size, padding="same"),
             nn.ReLU(),
             pool,
-        ]
-        conv_layers.append(
             nn.Conv1d(
                 out_channels_cnn_1, out_channels_cnn_2, kernel_size, padding="same"
-            )
-        )
-        conv_layers.append(nn.ReLU())
-        conv_layers.append(pool)
+            ),
+            nn.ReLU(),
+            pool,
+        ]
         self.conv_subnet = nn.Sequential(*conv_layers)
 
         # construct fully connected layers
@@ -84,7 +103,6 @@ class CNNEmbedding(nn.Module):
             fc_layers.append(nn.Linear(num_hiddens, num_hiddens))
             fc_layers.append(nn.ReLU())
         fc_layers.append(nn.Linear(num_hiddens, output_dim))
-        fc_layers.append(nn.ReLU())
 
         self.fc_subnet = nn.Sequential(*fc_layers)
 
@@ -102,47 +120,53 @@ class CNNEmbedding(nn.Module):
         return embedding
 
 
-class FCEmbedding_permutation_inv(nn.Module):
+class FCEmbeddingPermutationInv(nn.Module):
     """Permutation invariant embedding network.
-    takes as input a tensor with (batch, permutation_dim, input_dim)
+
+    Takes as input a tensor with (batch, permutation_dim, input_dim)
     and outputs (batch,output_dim).
     """
 
     def __init__(
         self,
+        single_trial_net: nn.Module,
         input_dim: int,
         output_dim: int,
         num_fc: int = 2,
-        first_layer_dim: int = 500,
-        num_hiddens: int = 120,
+        num_hiddens: int = 20,
+        combining_operation: str = "mean",
     ):
-        """permutation invariant multi-layer NN
-            fully connected layers takes (batch,permutation_dim, input_dim) as input.
-            takes the mean over permutation_dim.
+        """Permutation invariant multi-layer NN.
+            The single_trial_net is applied to each "trial" of the input
+            and is combined by the combining_operation (mean or sum).
+
         Args:
-            input_dim: Dimensionality of input.
-            num_fc: Number fully connected layer, min of 2
-            first_layer_dim: dimension of the first layer, the one which is taken the mean over.
-            num_hiddens: number of hidden dims in fc
+            single_trial_net: Network to process one trial, the combining_operation is
+                applied to its output. Taskes as input (batch, input_dim).
+                Remark: This network should be large enough as it acts on all (iid)
+                inputs seperatley and needs enough capacity to process the information
+                of all inputs.
+            input_dim: Dimensionality of input to the fully connected layers
+                (output_dimension of single_trial_net).
+            output_dim: Dimensionality of the output.
+            num_fc: Number of fully connected layer, minimum of 2.
+            num_hiddens: Number of hidden dimensions in fully-connected layers.
+            combining_operation: How to combine the permutational dimensions, one of
+                'mean' or 'sum'.
         """
         super().__init__()
+        self.single_trial_net = single_trial_net
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.first_layer_dim = first_layer_dim
         self.num_hiddens = num_hiddens
-
-        # layer on which the mean is taken from
-        self.first_layer = nn.Sequential(
-            *[nn.Linear(input_dim, first_layer_dim), nn.ReLU()]
-        )
+        self.combining_operation = combining_operation
 
         # construct fully connected layers
-        fc_layers = [nn.Linear(first_layer_dim, num_hiddens), nn.ReLU()]
+        fc_layers = [nn.Linear(input_dim, num_hiddens), nn.ReLU()]
         for _ in range(num_fc - 2):
             fc_layers.append(nn.Linear(num_hiddens, num_hiddens))
             fc_layers.append(nn.ReLU())
         fc_layers.append(nn.Linear(num_hiddens, output_dim))
-        fc_layers.append(nn.ReLU())
 
         self.fc_subnet = nn.Sequential(*fc_layers)
 
@@ -153,13 +177,16 @@ class FCEmbedding_permutation_inv(nn.Module):
         Returns:
             Network output (batch_size, output_dim).
         """
-        _, permutation_dim, _ = x.shape
+        batch, permutation_dim, _ = x.shape
 
-        e = self.first_layer(x[:, 0])
-        for i, item in enumerate(torch.transpose(x[:, 1:], 0, 1)):
-            e = e + self.first_layer(item)
+        e = self.single_trial_net(x.view(batch * permutation_dim, -1)).view(
+            batch, permutation_dim, -1
+        )
 
-        e = e / permutation_dim
+        if self.combining_operation == "mean":
+            e = e.mean(1)
+        elif self.combining_operation == "sum":
+            e = e.sum(1)
 
         embedding = self.fc_subnet(e)
 
