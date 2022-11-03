adfc0229f81dbc986ddc2aac70a36c00b6681acb
michaeldeistler <michael.deistler@uni-tuebingen.de>
2022-11-03 11:11:28 +0100

    Update CHANGELOG and bump version

--- sbi/__version__.py
+++ sbi/__version__.py
@@ -1,6 +1,6 @@
 # This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
 # under the Affero General Public License v3, see <https://www.gnu.org/licenses/>.
 
-VERSION = (0, 19, 2)
+VERSION = (0, 20, 0)
 
 __version__ = ".".join(map(str, VERSION))
