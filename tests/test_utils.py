6cdf678abf6c81f467585c08d13531866e527ac1
janfb <jan.boelts@tum.de>
2022-12-19 10:58:21 +0100

    add functional sbc text.'

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@
 
 from __future__ import annotations
 
-from typing import Tuple, Union
+from typing import Any, Callable, Dict, Optional, Tuple, Union
 
 import torch
 from torch import Tensor
@@ -11,9 +11,12 @@ from torch.distributions import Distribution
 
 from sbi.inference.posteriors.base_posterior import NeuralPosterior
 from sbi.inference.posteriors.direct_posterior import DirectPosterior
+from sbi.inference.potentials.base_potential import BasePotential
 from sbi.simulators.linear_gaussian import true_posterior_linear_gaussian_mvn_prior
+from sbi.types import Shape, TorchTransform
 from sbi.utils import BoxUniform, within_support
 from sbi.utils.metrics import c2st
+from sbi.utils.torchutils import ensure_theta_batched
 
 
 def kl_d_via_monte_carlo(
@@ -149,3 +152,141 @@ def check_c2st(x: Tensor, y: Tensor, alg: str, tol: float = 0.1) -> None:
     assert (
         (0.5 - tol) <= score <= (0.5 + tol)
     ), f"{alg}'s c2st={score:.2f} is too far from the desired near-chance performance."
+
+
+class PosteriorPotential(BasePotential):
+    allow_iid_x = False  # type: ignore
+
+    def __init__(
+        self,
+        posterior: Distribution,
+        prior: Distribution,
+        x_o: Optional[Tensor] = None,
+        device: str = "cpu",
+    ):
+        r"""Returns the potential for a closed-form posterior.
+
+        The potential is the same as the log-probability of the posterior,
+        but it is set to $-\inf$ outside of the prior bounds.
+
+        Args:
+            posterior: The posterior distribution
+            prior: The prior distribution.
+            x_o: The observed data at which to evaluate the posterior.
+
+        Returns:
+            The potential function.
+        """
+        super().__init__(prior, x_o, device)
+
+        assert (
+            x_o is None
+        ), "No need to pass x_o, passed Posterior must be fixed to x_o."
+        self.posterior = posterior
+
+    def __call__(self, theta: Tensor, track_gradients: bool = True) -> Tensor:
+        r"""Returns the potential for posterior-based methods.
+
+        Args:
+            theta: The parameter set at which to evaluate the potential function.
+            track_gradients: Whether to track the gradients.
+
+        Returns:
+            The potential.
+        """
+
+        theta = ensure_theta_batched(torch.as_tensor(theta))
+
+        with torch.set_grad_enabled(track_gradients):
+            posterior_log_prob = self.posterior.log_prob(theta)
+
+            # Force probability to be zero outside prior support.
+            in_prior_support = within_support(self.prior, theta)
+
+            posterior_log_prob = torch.where(
+                in_prior_support,
+                posterior_log_prob,
+                torch.tensor(float("-inf"), dtype=torch.float32, device=self.device),
+            )
+        return posterior_log_prob
+
+
+class TractablePosterior(NeuralPosterior):
+    r"""Posterior $p(\theta|x_o)$ with `log_prob()` and `sample()` methods, built from a
+    potential function with tractable posterior distribution.<br/><br/>"""
+
+    def __init__(
+        self,
+        potential_fn: Callable,
+        theta_transform: Optional[TorchTransform] = None,
+        device: Optional[str] = "cpu",
+        x_shape: Optional[torch.Size] = None,
+    ):
+        """
+        Args:
+            potential_fn: The potential function from which to draw samples.
+            theta_transform: Transformation that will be applied during sampling.
+                Allows to perform, e.g. MCMC in unconstrained space.
+            device: Training device, e.g., "cpu", "cuda" or "cuda:0". If None,
+                `potential_fn.device` is used.
+            x_shape: Shape of the observed data.
+        """
+        assert isinstance(potential_fn, PosteriorPotential)
+        super().__init__(potential_fn, theta_transform, device, x_shape)
+
+    def sample(
+        self,
+        sample_shape: Shape = torch.Size(),
+        x: Optional[Tensor] = None,
+        show_progress_bars: bool = True,
+        mcmc_method: Optional[str] = None,
+        mcmc_parameters: Optional[Dict[str, Any]] = None,
+    ) -> Tensor:
+        """See child classes for docstring."""
+
+        return self.potential_fn.posterior.sample(sample_shape)
+
+    def log_prob(
+        self,
+        theta: Tensor,
+    ) -> Tensor:
+        r"""Returns the log-probability of the posterior $p(\theta|x)$.
+
+        Args:
+            theta: Parameters $\theta$.
+
+        Returns:
+            `(len(θ),)`-shaped log posterior probability $\log p(\theta|x)$ for θ in the
+            support of the prior, -∞ (corresponding to 0 probability) outside.
+        """
+        theta = ensure_theta_batched(torch.as_tensor(theta))
+        return self.potential_fn.posterior.log_prob(theta)
+
+    def map(
+        self,
+        x: Optional[Tensor] = None,
+        num_iter: int = 1_000,
+        num_to_optimize: int = 100,
+        learning_rate: float = 0.01,
+        init_method: Union[str, Tensor] = "posterior",
+        num_init_samples: int = 1_000,
+        save_best_every: int = 10,
+        show_progress_bars: bool = False,
+        force_update: bool = False,
+    ) -> Tensor:
+        """Returns stored maximum-a-posterior estimate (MAP), otherwise calculates it.
+
+        See child classes for docstring.
+        """
+
+        return super().map(
+            x=x,
+            num_iter=num_iter,
+            num_to_optimize=num_to_optimize,
+            learning_rate=learning_rate,
+            init_method=init_method,
+            num_init_samples=num_init_samples,
+            save_best_every=save_best_every,
+            show_progress_bars=show_progress_bars,
+            force_update=force_update,
+        )
