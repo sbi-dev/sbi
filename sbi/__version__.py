d2cabf93e85e10560e542b6efcb531d8f41d6163
janfb <jan.boelts@tum.de>
2022-12-22 12:57:20 +0100

    prepare release.

--- sbi/__version__.py
+++ sbi/__version__.py
@@ -1,6 +1,6 @@
 # This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
 # under the Affero General Public License v3, see <https://www.gnu.org/licenses/>.
 
-VERSION = (0, 20, 0)
+VERSION = (0, 21, 0)
 
 __version__ = ".".join(map(str, VERSION))
