dcb2fee0945cce20dd1b5794cd8df73981e2512f
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2024-03-18 16:46:26 +0100

    fix: fixes circular deps at potential expense of isort

--- sbi/utils/__init__.py
+++ sbi/utils/__init__.py
@@ -4,7 +4,6 @@ from sbi.utils.conditional_density_utils import extract_and_transform_mog
 from sbi.utils.get_nn_models import classifier_nn, likelihood_nn, posterior_nn
 from sbi.utils.io import get_data_root, get_log_root, get_project_root
 from sbi.utils.kde import KDEWrapper, get_kde
-from sbi.utils.posterior_ensemble import NeuralPosteriorEnsemble
 from sbi.utils.potentialutils import pyro_potential_wrapper, transformed_potential
 from sbi.utils.restriction_estimator import (
     RestrictedPrior,
