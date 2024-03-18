5c5acdfdc44c5284f2a5ed394362768c52341215
Fabio Muratore <fabio.muratore@famura.net>
2024-03-18 16:15:36 +0100

    Export classes and function

---
+++ sbi/neural_nets/__init__.py
@@ -0,0 +1,16 @@
+from sbi.neural_nets.classifier import (
+    StandardizeInputs,
+    build_input_layer,
+    build_linear_classifier,
+    build_mlp_classifier,
+    build_resnet_classifier,
+)
+from sbi.neural_nets.density_estimators import DensityEstimator, NFlowsFlow
+from sbi.neural_nets.embedding_nets import (
+    CNNEmbedding,
+    FCEmbedding,
+    PermutationInvariantEmbedding,
+)
+from sbi.neural_nets.flow import build_made, build_maf, build_maf_rqs, build_nsf
+from sbi.neural_nets.mdn import build_mdn
+from sbi.neural_nets.mnle import MixedDensityEstimator, build_mnle
