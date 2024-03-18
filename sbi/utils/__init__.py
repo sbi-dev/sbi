ed1c6183ecb5c8b8b1b2539cd7979083d2132505
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2024-03-18 16:00:20 +0100

    fix: add NeuralPosteriorEnsemble to utils.__init__

--- sbi/utils/__init__.py
+++ sbi/utils/__init__.py
@@ -4,70 +4,36 @@ from sbi.utils.conditional_density_utils import extract_and_transform_mog
 from sbi.utils.get_nn_models import classifier_nn, likelihood_nn, posterior_nn
 from sbi.utils.io import get_data_root, get_log_root, get_project_root
 from sbi.utils.kde import KDEWrapper, get_kde
-from sbi.utils.potentialutils import pyro_potential_wrapper, transformed_potential
-from sbi.utils.restriction_estimator import (
-    RestrictedPrior,
-    RestrictionEstimator,
-    get_density_thresholder,
-)
-from sbi.utils.sbiutils import (
-    batched_mixture_mv,
-    batched_mixture_vmv,
-    check_dist_class,
-    check_warn_and_setstate,
-    clamp_and_warn,
-    del_entries,
-    expit,
-    get_simulations_since_round,
-    gradient_ascent,
-    handle_invalid_x,
-    logit,
-    mask_sims_from_prior,
-    match_theta_and_x_batch_shapes,
-    mcmc_transform,
-    mog_log_prob,
-    nle_nre_apt_msg_on_invalid_x,
-    npe_msg_on_invalid_x,
-    standardizing_net,
-    standardizing_transform,
-    warn_if_zscoring_changes_data,
-    within_support,
-    x_shape_from_simulation,
-    z_score_parser,
-)
-from sbi.utils.torchutils import (
-    BoxUniform,
-    assert_all_finite,
-    cbrt,
-    create_alternating_binary_mask,
-    create_mid_split_binary_mask,
-    create_random_binary_mask,
-    gaussian_kde_log_eval,
-    get_num_parameters,
-    get_temperature,
-    logabsdet,
-    merge_leading_dims,
-    random_orthogonal,
-    repeat_rows,
-    searchsorted,
-    split_leading_dim,
-    sum_except_batch,
-    tensor2numpy,
-    tile,
-)
-from sbi.utils.typechecks import (
-    is_bool,
-    is_int,
-    is_nonnegative_int,
-    is_positive_int,
-    is_power_of_two,
-)
-from sbi.utils.user_input_checks import (
-    check_estimator_arg,
-    check_prior,
-    process_prior,
-    process_x,
-    test_posterior_net_for_multi_d_x,
-    validate_theta_and_x,
-)
+from sbi.utils.posterior_ensemble import NeuralPosteriorEnsemble
+from sbi.utils.potentialutils import (pyro_potential_wrapper,
+                                      transformed_potential)
+from sbi.utils.restriction_estimator import (RestrictedPrior,
+                                             RestrictionEstimator,
+                                             get_density_thresholder)
+from sbi.utils.sbiutils import (batched_mixture_mv, batched_mixture_vmv,
+                                check_dist_class, check_warn_and_setstate,
+                                clamp_and_warn, del_entries, expit,
+                                get_simulations_since_round, gradient_ascent,
+                                handle_invalid_x, logit, mask_sims_from_prior,
+                                match_theta_and_x_batch_shapes, mcmc_transform,
+                                mog_log_prob, nle_nre_apt_msg_on_invalid_x,
+                                npe_msg_on_invalid_x, standardizing_net,
+                                standardizing_transform,
+                                warn_if_zscoring_changes_data, within_support,
+                                x_shape_from_simulation, z_score_parser)
+from sbi.utils.torchutils import (BoxUniform, assert_all_finite, cbrt,
+                                  create_alternating_binary_mask,
+                                  create_mid_split_binary_mask,
+                                  create_random_binary_mask,
+                                  gaussian_kde_log_eval, get_num_parameters,
+                                  get_temperature, logabsdet,
+                                  merge_leading_dims, random_orthogonal,
+                                  repeat_rows, searchsorted, split_leading_dim,
+                                  sum_except_batch, tensor2numpy, tile)
+from sbi.utils.typechecks import (is_bool, is_int, is_nonnegative_int,
+                                  is_positive_int, is_power_of_two)
+from sbi.utils.user_input_checks import (check_estimator_arg, check_prior,
+                                         process_prior, process_x,
+                                         test_posterior_net_for_multi_d_x,
+                                         validate_theta_and_x)
 from sbi.utils.user_input_checks_utils import MultipleIndependent
