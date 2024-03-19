45ddfd3419ca85405d360ab6cdf2755a69467300
Fabio Muratore <fabio.muratore@famura.net>
2024-03-19 15:41:07 +0100

    Doc strings now only contain the deprecation warning

--- sbi/utils/get_nn_models.py
+++ sbi/utils/get_nn_models.py
@@ -20,32 +20,8 @@ def classifier_nn(
     embedding_net_x: nn.Module = nn.Identity(),
     **kwargs,
 ) -> Callable:
-    r"""
-    Returns a function that builds a classifier for learning density ratios.
-
-    This function will usually be used for SNRE. The returned function is to be passed
-    to the inference class when using the flexible interface.
-
-    Note that in the view of the SNRE classifier we build below, x=theta and y=x.
-
-    Args:
-        model: The type of classifier that will be created. One of [`linear`, `mlp`,
-            `resnet`].
-        z_score_theta: Whether to z-score parameters $\theta$ before passing them into
-            the network, can take one of the following:
-            - `none`, or None: do not z-score.
-            - `independent`: z-score each dimension independently.
-            - `structured`: treat dimensions as related, therefore compute mean and std
-            over the entire batch, instead of per-dimension. Should be used when each
-            sample is, for example, a time series or an image.
-        z_score_x: Whether to z-score simulation outputs $x$ before passing them into
-            the network, same options as z_score_theta.
-        hidden_features: Number of hidden features.
-        embedding_net_theta:  Optional embedding network for parameters $\theta$.
-        embedding_net_x:  Optional embedding network for simulation outputs $x$. This
-            embedding net allows to learn features from potentially high-dimensional
-            simulation outputs.
-        kwargs: additional custom arguments passed to downstream build functions.
+    r"""This method is deprecated and will be removed in the next release.
+    Please use classifier_nn from sbi.neural_nets in the future.
     """
 
     warn(
@@ -77,34 +53,8 @@ def likelihood_nn(
     num_components: int = 10,
     **kwargs,
 ) -> Callable:
-    r"""
-    Returns a function that builds a density estimator for learning the likelihood.
-
-    This function will usually be used for SNLE. The returned function is to be passed
-    to the inference class when using the flexible interface.
-
-    Args:
-        model: The type of density estimator that will be created. One of [`mdn`,
-            `made`, `maf`, `maf_rqs`, `nsf`].
-        z_score_theta: Whether to z-score parameters $\theta$ before passing them into
-            the network, can take one of the following:
-            - `none`, or None: do not z-score.
-            - `independent`: z-score each dimension independently.
-            - `structured`: treat dimensions as related, therefore compute mean and std
-            over the entire batch, instead of per-dimension. Should be used when each
-            sample is, for example, a time series or an image.
-        z_score_x: Whether to z-score simulation outputs $x$ before passing them into
-            the network, same options as z_score_theta.
-        hidden_features: Number of hidden features.
-        num_transforms: Number of transforms when a flow is used. Only relevant if
-            density estimator is a normalizing flow (i.e. currently either a `maf` or a
-            `nsf`). Ignored if density estimator is a `mdn` or `made`.
-        num_bins: Number of bins used for the splines in `nsf`. Ignored if density
-            estimator not `nsf`.
-        embedding_net: Optional embedding network for parameters $\theta$.
-        num_components: Number of mixture components for a mixture of Gaussians.
-            Ignored if density estimator is not an mdn.
-        kwargs: additional custom arguments passed to downstream build functions.
+    r"""This method is deprecated and will be removed in the next release.
+    Please use likelihood_nn from sbi.neural_nets in the future.
     """
 
     warn(
@@ -138,36 +88,8 @@ def posterior_nn(
     num_components: int = 10,
     **kwargs,
 ) -> Callable:
-    r"""
-    Returns a function that builds a density estimator for learning the posterior.
-
-    This function will usually be used for SNPE. The returned function is to be passed
-    to the inference class when using the flexible interface.
-
-    Args:
-        model: The type of density estimator that will be created. One of [`mdn`,
-            `made`, `maf`, `maf_rqs`, `nsf`].
-        z_score_theta: Whether to z-score parameters $\theta$ before passing them into
-            the network, can take one of the following:
-            - `none`, or None: do not z-score.
-            - `independent`: z-score each dimension independently.
-            - `structured`: treat dimensions as related, therefore compute mean and std
-            over the entire batch, instead of per-dimension. Should be used when each
-            sample is, for example, a time series or an image.
-        z_score_x: Whether to z-score simulation outputs $x$ before passing them into
-            the network, same options as z_score_theta.
-        hidden_features: Number of hidden features.
-        num_transforms: Number of transforms when a flow is used. Only relevant if
-            density estimator is a normalizing flow (i.e. currently either a `maf` or a
-            `nsf`). Ignored if density estimator is a `mdn` or `made`.
-        num_bins: Number of bins used for the splines in `nsf`. Ignored if density
-            estimator not `nsf`.
-        embedding_net: Optional embedding network for simulation outputs $x$. This
-            embedding net allows to learn features from potentially high-dimensional
-            simulation outputs.
-        num_components: Number of mixture components for a mixture of Gaussians.
-            Ignored if density estimator is not an mdn.
-        kwargs: additional custom arguments passed to downstream build functions.
+    r"""This method is deprecated and will be removed in the next release.
+    Please use posterior_nn from sbi.neural_nets in the future.
     """
 
     warn(
