2881fa3dee6deb6de2c2743881ae38f83d8cec67
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2024-03-19 11:38:13 +0100

    fix: ran pre-commit hooks

--- sbi/utils/posterior_ensemble.py
+++ sbi/utils/posterior_ensemble.py
@@ -2,35 +2,56 @@
 # under the Affero General Public License v3, see <https://www.gnu.org/licenses/>.
 
 import warnings
+from typing import List, Optional, Union
 
+from torch import Tensor
+from torch.distributions import Distribution
+
+from sbi.inference.posteriors.ensemble_posterior import EnsemblePosterior
 from sbi.inference.posteriors.ensemble_posterior import (
-    EnsemblePosterior,
-    EnsemblePotential,
+    EnsemblePotential as EnsemblePotentialMoved,
 )
+from sbi.types import TorchTransform
 
 warnings.filterwarnings("default", category=DeprecationWarning, module=__name__)
 
 warnings.warn(
     """
-    NeuralPosteriorEnsemble was renamed EnsemblePosterior and moved to 
+    NeuralPosteriorEnsemble was renamed EnsemblePosterior and moved to
     sbi.inference.posteriors.ensemble_posterior along with EnsemblePotential.
     sbi.utils.posterior_ensemble will be deprecated in v0.24.
     """,
     DeprecationWarning,
+    stacklevel=2,
 )
 
 
-def NeuralPosteriorEnsemble(*args, **kwargs):
+def NeuralPosteriorEnsemble(
+    posteriors: List,
+    weights: Optional[Union[List[float], Tensor]] = None,
+    theta_transform: Optional[TorchTransform] = None,
+):
     warnings.warn(
-        "NeuralPosteriorEnsemble was renamed EnsemblePosterior and moved to sbi.inference.posteriors.ensemble_posterior. sbi.utils.posterior_ensemble will be deprecated in v0.24.",
+        "NeuralPosteriorEnsemble was renamed EnsemblePosterior and moved to \
+        sbi.inference.posteriors.ensemble_posterior. sbi.utils.posterior_ensemble \
+        will be deprecated in v0.24.",
         DeprecationWarning,
+        stacklevel=2,
     )
-    return EnsemblePosterior(*args, **kwargs)
+    return EnsemblePosterior(posteriors, weights, theta_transform)
 
 
-def EnsemblePotential(*args, **kwargs):
+def EnsemblePotential(
+    potential_fns: List,
+    weights: Tensor,
+    prior: Distribution,
+    x_o: Optional[Tensor],
+    device: str = "cpu",
+):
     warnings.warn(
-        "EnsemblePotential was moved to sbi.inference.posteriors.ensemble_posterior. sbi.utils.posterior_ensemble will be deprecated in v0.24.",
+        "EnsemblePotential was moved to sbi.inference.posteriors.ensemble_posterior. \
+        sbi.utils.posterior_ensemble will be deprecated in v0.24.",
         DeprecationWarning,
+        stacklevel=2,
     )
-    return EnsemblePotential(*args, **kwargs)
+    return EnsemblePotentialMoved(potential_fns, weights, prior, x_o, device)
