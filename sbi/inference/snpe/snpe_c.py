db840ee7940821c78e17885bf7c9574259b14315
Fabio Muratore <37794142+famura@users.noreply.github.com>
2024-03-20 10:45:28 +0100

    Rename types.py to avoid import clash in PyCharm (#993)
    * Renamed types.py to sbi_types.py
    
    * Ignore PyCharm's project folder
    
    * Ran ruff
    
    * Ran ruff
    
    * Also fixed new file
    
    * Checkout out the tutorials from upstream/main
    
    * Fixed one more (new) import

--- sbi/inference/snpe/snpe_c.py
+++ sbi/inference/snpe/snpe_c.py
@@ -13,7 +13,7 @@ from torch.distributions import Distribution, MultivariateNormal, Uniform
 from sbi import utils as utils
 from sbi.inference.posteriors.direct_posterior import DirectPosterior
 from sbi.inference.snpe.snpe_base import PosteriorEstimator
-from sbi.types import TensorboardSummaryWriter
+from sbi.sbi_types import TensorboardSummaryWriter
 from sbi.utils import (
     batched_mixture_mv,
     batched_mixture_vmv,
