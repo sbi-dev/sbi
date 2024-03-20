db91f59c746a4d3f79475f99364bee325bf1efc1
michaeldeistler <michael.deistler95@gmail.com>
2024-03-20 16:17:21 +0100

    Change shape handling to iid,batch,event

--- sbi/neural_nets/density_estimators/shape_handling.py
+++ sbi/neural_nets/density_estimators/shape_handling.py
@@ -2,22 +2,25 @@ import torch
 from torch import Tensor
 
 
-def reshape_to_batch_iid_event(
-    theta_or_x: Tensor, event_shape: torch.Size, batch_is_iid: bool
+def reshape_to_iid_batch_event(
+    theta_or_x: Tensor, event_shape: torch.Size, leading_is_iid: bool
 ) -> Tensor:
-    """Return theta or x such that its shape is `(batch_dim, iid_dim, event_shape)`.
+    """Return theta or x s.t. its shape is `(iid_shape, batch_shape, event_shape)`.
+
+    This follows the conventions used in pytorch distributions:
+    https://bochang.me/blog/posts/pytorch-distributions/
 
     Args:
         theta_or_x: The tensor to be reshaped. Can have any of the following shapes:
             - (event)
             - (batch, event)
             - (iid, event)
-            - (batch, iid, event)
+            - (iid, batch, event)
         event_shape: The shape of a single datapoint (without batch dimension or iid
             dimension).
-        batch_is_iid: Used only if `len(theta_or_x) == 2` and `theta_or_x.shape > 1`.
-            Defines whether the leading dimension is interpreted as batch dimension or
-            as iid dimension.
+        leading_is_iid: Used only if `theta_or_x` has exactly one dimension beyond the
+            `event` dims. Defines whether the leading dimension is interpreted as batch
+            dimension or as iid dimension.
 
     Returns:
         A tensor of shape `(batch, iid, event)`.
@@ -36,7 +39,7 @@ def reshape_to_batch_iid_event(
         return theta_or_x.unsqueeze(0).unsqueeze(0)
     elif len(leading_theta_or_x_shape) == 1:
         # Either a batch dimension or an iid dimension was passed.
-        return theta_or_x.unsqueeze(0) if batch_is_iid else theta_or_x.unsqueeze(1)
+        return theta_or_x.unsqueeze(1) if leading_is_iid else theta_or_x.unsqueeze(0)
     elif len(leading_theta_or_x_shape) == 2:
         # Batch dimension and iid dimension were passed.
         return theta_or_x
