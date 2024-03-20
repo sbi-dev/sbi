a4ac44a31decba631072137edb91b617a7b6baff
turnmanh <17703667+turnmanh@users.noreply.github.com>
2024-03-20 15:28:51 +0100

    added density est. interface

--- sbi/neural_nets/density_estimators/zuko_flow_estimator.py
+++ sbi/neural_nets/density_estimators/zuko_flow_estimator.py
@@ -1,14 +1,16 @@
 import math
-
 import torch
 import torch.nn as nn
+
+from sbi.neural_nets.density_estimators.base import DensityEstimator
+from torch.distributions import Distribution
+from zuko.distributions import DiagNormal, NormalizingFlow
 from zuko.nn import MLP
+from zuko.transforms import FreeFormJacobianTransform
 from zuko.utils import broadcast
 
-from sbi.neural_nets.vf_estimators.base import VectorFieldEstimator
 
-
-class ZukoFlowMatchingEstimator(VectorFieldEstimator):
+class ZukoFlowMatchingEstimator(DensityEstimator):
     def __init__(
         self,
         theta_shape: torch.Size,
@@ -48,20 +50,25 @@ class ZukoFlowMatchingEstimator(VectorFieldEstimator):
         self.frequency = torch.arange(1, frequency + 1, device=self.device) * math.pi
         self.eta = eta
 
-    def forward(
-        self, theta: torch.Tensor, x: torch.Tensor, t: torch.Tensor
-    ) -> torch.Tensor:
-        # positional encoding of time steps
-        t = self.frequency * t[..., None]
-        t = torch.cat((t.cos(), t.sin()), dim=-1)
+    def embedding_net(self):
+        raise NotImplementedError(
+            "The Flow Matching estimator does not support an embedding net for now."
+        )
 
-        # broadcast to match shapes of theta, x, and t
-        theta, x, t = broadcast(theta, x, t, ignore=1)
+    def log_prob(self, input: torch.Tensor, condition: torch.Tensor, **kwargs) -> torch.Tensor:
 
-        # return the estimated vector field
-        return self.net(torch.cat((theta, x, t), dim=-1))
+        # check for possible batch dimension in the condition
+        if len(condition.shape) > len(self._condition_shape):
+            raise ValueError(
+                "The condition has a batch dimension, which is currently not supported."
+            )
 
-    def loss(self, theta: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
+        dist = self.flow(x_o=condition)
+        log_prob = dist.log_prob(input)
+
+        return log_prob
+
+    def loss(self, theta: torch.Tensor, x: torch.Tensor, **kwargs) -> torch.Tensor:
         # randomly sample the time steps to compare the vector field at different time steps
         t = torch.rand(theta.shape[:-1], device=theta.device, dtype=theta.dtype)
         t_ = t[..., None]
@@ -75,3 +82,40 @@ class ZukoFlowMatchingEstimator(VectorFieldEstimator):
 
         # compute the mean squared error between the vector fields
         return (self.net(theta_prime, x, t) - vector_field).pow(2).mean()
+
+    def forward(
+        self, theta: torch.Tensor, x: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        # positional encoding of time steps
+        t = self.frequency * t[..., None]
+        t = torch.cat((t.cos(), t.sin()), dim=-1)
+
+        # broadcast to match shapes of theta, x, and t
+        theta, x, t = broadcast(theta, x, t, ignore=1)
+
+        # return the estimated vector field
+        return self.net(torch.cat((theta, x, t), dim=-1))
+
+    def sample(self, sample_shape: torch.Size, condition: torch.Tensor, **kwargs) -> torch.Tensor:
+        # check for possible batch dimension in the condition
+        if len(condition.shape) > len(self._condition_shape):
+            raise ValueError(
+                "The condition has a batch dimension, which is currently not supported."
+            )
+
+        dist = self.flow(x_o=condition)
+        samples = dist.sample(sample_shape)
+        return samples
+    
+    def flow(self, x_o: torch.Tensor) -> Distribution:
+        return NormalizingFlow(
+            transform=FreeFormJacobianTransform(
+                f=lambda t, theta: self.vf_estimator(theta, x_o, t),
+                t0=x_o.new_tensor(0.0),
+                t1=x_o.new_tensor(1.0),
+                phi=(x_o, *self.vf_estimator.net.parameters()),
+            ),
+            base=DiagNormal(
+                torch.zeros(self.theta_dim), torch.ones(self.theta_dim)
+            ).expand(x_o.shape[:-1]),
+        )
