917a2c724c45b50ec1535616c723004ba02962f1
bkmi <12955549+bkmi@users.noreply.github.com>
2024-04-22 10:27:45 +0200

    made zscore more flexible, removed net from ratioestimator, wip shapes

--- sbi/neural_nets/classifier.py
+++ sbi/neural_nets/classifier.py
@@ -13,46 +13,32 @@ from sbi.utils.sbiutils import standardizing_net, z_score_parser
 from sbi.utils.user_input_checks import check_data_device, check_embedding_net_device
 
 
-def build_z_scored_embedding_nets(
-    batch_x: Tensor,
-    batch_y: Tensor,
-    z_score_x: Optional[str] = "independent",
-    z_score_y: Optional[str] = "independent",
-    embedding_net_x: nn.Module = nn.Identity(),
-    embedding_net_y: nn.Module = nn.Identity(),
-) -> tuple[nn.Module, nn.Module]:
+def build_z_scored_embedding_net(
+    batch: Tensor,
+    z_score: Optional[str] = "independent",
+    embedding_net: nn.Module = nn.Identity(),
+) -> nn.Module:
     """Builds input layer for classifiers that optionally z-scores.
 
     Args:
-        batch_x: Batch of xs, used to infer dimensionality and (optional) z-scoring.
-        batch_y: Batch of ys, used to infer dimensionality and (optional) z-scoring.
-        z_score_x: Whether to z-score xs passing into the network, can be one of:
+        batch: Batch of xs, used to infer dimensionality and (optional) z-scoring.
+        z_score: Whether to z-score xs passing into the network, can be one of:
             - `none`, or None: do not z-score.
             - `independent`: z-score each dimension independently.
             - `structured`: treat dimensions as related, therefore compute mean and std
             over the entire batch, instead of per-dimension. Should be used when each
             sample is, for example, a time series or an image.
-        z_score_y: Whether to z-score ys passing into the network, same options as
-            z_score_x.
-        embedding_net_x: Optional embedding network for x.
-        embedding_net_y: Optional embedding network for y.
+        embedding_net: Optional embedding network for x.
 
     Returns:
         Input layer that optionally z-scores.
     """
-    z_score_x_bool, structured_x = z_score_parser(z_score_x)
-    if z_score_x_bool:
-        embedding_net_x = nn.Sequential(
-            standardizing_net(batch_x, structured_x), embedding_net_x
+    z_score_bool, structured = z_score_parser(z_score)
+    if z_score_bool:
+        embedding_net = nn.Sequential(
+            standardizing_net(batch, structured), embedding_net
         )
-
-    z_score_y_bool, structured_y = z_score_parser(z_score_y)
-    if z_score_y_bool:
-        embedding_net_y = nn.Sequential(
-            standardizing_net(batch_y, structured_y), embedding_net_y
-        )
-
-    return embedding_net_x, embedding_net_y
+    return embedding_net
 
 
 def build_linear_classifier(
@@ -97,9 +83,8 @@ def build_linear_classifier(
 
     neural_net = nn.Linear(x_numel + y_numel, 1)
 
-    embedding_net_x, embedding_net_y = build_z_scored_embedding_nets(
-        batch_x, batch_y, z_score_x, z_score_y, embedding_net_x, embedding_net_y
-    )
+    embedding_net_x = build_z_scored_embedding_net(batch_x, z_score_x, embedding_net_x)
+    embedding_net_y = build_z_scored_embedding_net(batch_y, z_score_y, embedding_net_y)
 
     return TensorRatioEstimator(
         net=neural_net,
@@ -154,9 +139,8 @@ def build_mlp_classifier(
         nn.Linear(hidden_features, 1),
     )
 
-    embedding_net_x, embedding_net_y = build_z_scored_embedding_nets(
-        batch_x, batch_y, z_score_x, z_score_y, embedding_net_x, embedding_net_y
-    )
+    embedding_net_x = build_z_scored_embedding_net(batch_x, z_score_x, embedding_net_x)
+    embedding_net_y = build_z_scored_embedding_net(batch_y, z_score_y, embedding_net_y)
 
     return TensorRatioEstimator(
         net=neural_net,
@@ -217,9 +201,8 @@ def build_resnet_classifier(
         use_batch_norm=use_batch_norm,
     )
 
-    embedding_net_x, embedding_net_y = build_z_scored_embedding_nets(
-        batch_x, batch_y, z_score_x, z_score_y, embedding_net_x, embedding_net_y
-    )
+    embedding_net_x = build_z_scored_embedding_net(batch_x, z_score_x, embedding_net_x)
+    embedding_net_y = build_z_scored_embedding_net(batch_y, z_score_y, embedding_net_y)
 
     return TensorRatioEstimator(
         net=neural_net,
--- sbi/neural_nets/ratio_estimators.py
+++ sbi/neural_nets/ratio_estimators.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 
 import torch
@@ -10,26 +12,11 @@ class RatioEstimator(nn.Module, ABC):
     The ratio estimator class is a wrapper around neural networks that enables
     evaluation of `unnormalized_log_ratio` for `theta`, `x` pairs. It also
     provides a method for combining `theta` and `x` into a single tensor.
-
-    Note:
-        We assume that the input to the ratio estimator is a tensor of shape
-        (sample, batch, event) where sample are iid draws, batch are
-        independent (not necessarily identically distributed) draws, and
-        event can be totally dependent.
-        See: https://bochang.me/blog/posts/pytorch-distributions/
     """
 
-    def __init__(
-        self,
-        net: nn.Module,
-    ) -> None:
-        r"""Base class for ratio estimators.
-
-        Args:
-            net: neural network taking in combined `theta` and `x`
-        """
+    def __init__(self) -> None:
+        r"""Base class for ratio estimators."""
         super().__init__()
-        self.net = net
 
     @abstractmethod
     def combine_theta_and_x(self, theta: Tensor, x: Tensor) -> Tensor:
@@ -50,12 +37,12 @@ class RatioEstimator(nn.Module, ABC):
         (batched) xs.
 
         Args:
-            theta: parameters of shape (sample, batch, theta_shape).
-            x: data of shape (sample, batch, x_shape).
+            theta
+            x
 
         Returns:
-            Sample-wise unnormalized log ratios.
-            Just like log_prob, the last dimension should be squeezed.
+            Sample-wise unnormalized log ratios. Just like log_prob, the last dimension
+            should be squeezed.
         """
         pass
 
@@ -64,10 +51,23 @@ class RatioEstimator(nn.Module, ABC):
         return self.unnormalized_log_ratio(*args, **kwargs)
 
 
+# TODO: following class does not yet actually use the shapes it claims to use!
 class TensorRatioEstimator(RatioEstimator):
+    """Base class for ratio estimators that take tensors as input.
+
+    Note:
+        We assume that the input to the ratio estimator is a tensor of shape
+        (sample_dim, batch_dim, *event_shape) where sample_dim are iid draws,
+        batch_dim are independent (not necessarily identically distributed)
+        draws, and event_shape can be totally dependent.
+        See: https://bochang.me/blog/posts/pytorch-distributions/
+    """
+
     def __init__(
         self,
         net: nn.Module,
+        theta_shape: torch.Size | tuple[int, ...],
+        x_shape: torch.Size | tuple[int, ...],
         embedding_net_theta: nn.Module = nn.Identity(),
         embedding_net_x: nn.Module = nn.Identity(),
     ) -> None:
@@ -75,19 +75,104 @@ class TensorRatioEstimator(RatioEstimator):
 
         Args:
             net: neural network taking in combined, embedded `theta` and `x`
+            theta_shape
+            x_shape
             embedding_net_theta
             embedding_net_x
         """
-        super().__init__(net=net)
+        super().__init__()
+        self.net = net
+        self.theta_shape = theta_shape
+        self.x_shape = x_shape
         self.embedding_net_theta = embedding_net_theta
         self.embedding_net_x = embedding_net_x
 
+    @staticmethod
+    def _check_shape(y: Tensor, shape: torch.Size | tuple[int, ...]) -> None:
+        r"""This method checks whether y has the correct shape.
+
+        Args:
+            y: Tensor of shape (*batch_shape, *y_shape).
+
+        Returns:
+            False:
+                1. If the y has a dimensionality that does not match
+                the expected input dimensionality.
+                2. If the shape of the y does not match the expected
+                input dimensionality.
+            True: otherwise
+        """
+        if len(y.shape) < len(shape):
+            raise ValueError(
+                f"Dimensionality of tensor is to small and does not match the\
+                expected input dimensionality {len(shape)}, as provided\
+                by tensor_shape."
+            )
+        else:
+            tensor_shape = y.shape[-len(shape) :]
+            if tuple(tensor_shape) != tuple(shape):
+                raise ValueError(
+                    f"Shape of tensor {tuple(tensor_shape)} does not match the \
+                    expected input dimensionality {tuple(shape)}, as \
+                    provided by tensor_shape. Please reshape it accordingly."
+                )
+
+    def _check_x_shape(self, x: Tensor) -> None:
+        r"""This method checks whether x has the correct shape.
+
+        Args:
+            x: Tensor of shape (*batch_shape, *x_shape).
+
+        Raises:
+            ValueError: If the x has a dimensionality that does not match
+                        the expected input dimensionality.
+            ValueError: If the shape of the x does not match the expected
+                        input dimensionality.
+        """
+        return self._check_shape(x, self.x_shape)
+
+    def _check_theta_shape(self, theta: Tensor) -> None:
+        r"""This method checks whether theta has the correct shape.
+
+        Args:
+            theta: Tensor of shape (*batch_shape, *theta_shape).
+
+        Raises:
+            ValueError: If the theta has a dimensionality that does not match
+                        the expected input dimensionality.
+            ValueError: If the shape of the theta does not match the expected
+                        input dimensionality.
+        """
+        return self._check_shape(theta, self.theta_shape)
+
     def combine_theta_and_x(self, theta: Tensor, x: Tensor, dim: int = -1) -> Tensor:
-        """Concatenate theta and x"""
+        """After embedding them, concatenate embedded_theta and embedded_x
+
+        Args:
+            theta: parameters of shape (sample_dim, batch_dim, *theta_shape).
+            x: data of shape (batch_dim, *x_shape).
+
+        Returns:
+            combined: shape (sample_dim, batch_dim, combined_event_dim)
+        """
+        self._check_theta_shape(theta)
+        self._check_x_shape(x)
         embedded_theta = self.embedding_net_theta(theta)
         embedded_x = self.embedding_net_x(x)
         return torch.cat([embedded_theta, embedded_x], dim=dim)
 
     def unnormalized_log_ratio(self, theta: Tensor, x: Tensor) -> Tensor:
+        r"""Return the unnormalized log ratios of the thetas given an x, or multiple
+        (batched) xs.
+
+        Args:
+            theta: parameters of shape (sample_dim, batch_dim, *theta_shape).
+            x: data of shape (batch_dim, *x_shape).
+
+        Returns:
+            Sample-wise unnormalized log ratios with shape (sample_dim, batch_dim).
+            Just like log_prob, the last dimension should be squeezed.
+        """
+
         z = self.combine_theta_and_x(theta, x)
         return self.net(z).squeeze(-1)
