75bf34d539fd1a00df6e520e3524380d589ab72f
manuelgloeckler <manu.gloeckler@hotmail.de>
2024-06-19 11:35:17 +0200

    Avioid potential circular import

--- sbi/simulators/linear_gaussian.py
+++ sbi/simulators/linear_gaussian.py
@@ -8,7 +8,7 @@ import torch
 from torch import Tensor
 from torch.distributions import Independent, MultivariateNormal, Uniform
 
-from sbi.utils import within_support
+from sbi.utils.sbiutils import within_support
 from sbi.utils.torchutils import atleast_2d
 
 
