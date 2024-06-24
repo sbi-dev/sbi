31086bf19bc0202c3bcbbc02c98e52293edf78bb
Peter Steinbach <p.steinbach@hzdr.de>
2024-06-24 14:39:37 +0200

    refactoring all class based methods into free functions

--- sbi/diagnostics/tarp.py
+++ sbi/diagnostics/tarp.py
@@ -1,20 +1,31 @@
+"""
+    Implementation taken from Lemos et al, 'Sampling-Based Accuracy Testing of
+    Posterior Estimators for General Inference' https://arxiv.org/abs/2302.03026
+
+    The TARP diagnostic is a global diagnostic which can be used to check a
+    trained posterior against a set of true values of theta.
+"""
+
 import warnings
-from typing import Tuple, Union
+from typing import Callable, Tuple, Union
 
 # import numpy as np
 import torch
 from joblib import Parallel, delayed
-from torch import Tensor
-from tqdm.auto import tqdm
-
 from sbi.inference.posteriors.base_posterior import NeuralPosterior
 from sbi.inference.posteriors.vi_posterior import VIPosterior
 from sbi.simulators.simutils import tqdm_joblib
+from torch import Tensor
+from tqdm.auto import tqdm
 
 
 def l2(x: Tensor, y: Tensor, axis=-1) -> Tensor:
     """
-    Calculates the L2 distance between two tensors.
+    Calculates the L2 distance between two tensors. Note, we cannot use the
+    torch.nn.MSELoss function as this sums across the batch dimension AND the
+    dimension given by <axis>. For tarp, we only require to sum across
+    the <axis> dimension.
+
     Args:
         x (Tensor): The first tensor.
         y (Tensor): The second tensor.
@@ -29,7 +40,11 @@ def l2(x: Tensor, y: Tensor, axis=-1) -> Tensor:
 
 def l1(x: Tensor, y: Tensor, axis=-1) -> Tensor:
     """
-    Calculates the L1 distance between two tensors.
+    Calculates the L1 distance between two tensors. Note, we cannot use the
+    torch.nn.L1Loss function as this sums across the batch dimension AND the
+    dimension given by <axis>. For tarp, we only require to sum across
+    the <axis> dimension.
+
     Args:
         x (Tensor): The first tensor.
         y (Tensor): The second tensor.
@@ -63,7 +78,8 @@ def infer_posterior_on_batch(
     -------
     Tensor
         A tensor of shape (num_posterior_samples, N, P) where N is the number of
-        samples given by xs and P is the output dimension of the neural posterior.
+        samples given by xs and P is the output dimension of the neural
+        posterior estimator.
     """
 
     samples = []
@@ -85,10 +101,213 @@ def infer_posterior_on_batch(
     return torch.cat(samples, dim=1)
 
 
+# this function currently does not perform any TARP related operation
+# the purpose of the function is (a) to align with the sbc interface and
+# (b) to provide the data which is required to run TARP
+def prepare_estimates(
+    xs: Tensor,
+    posterior: NeuralPosterior,
+    num_posterior_samples: int = 1000,
+    num_workers: int = 1,
+    infer_batch_size: int = 1,
+    show_progress_bar: bool = True,
+) -> Tensor:
+    """
+    Perform inference on batched x values using the provided posterior.
+    the purpose of the function is (a) to align with the sbc interface and
+    (b) to provide the data which is required to run TARP.
+
+    Args:
+        xs: observed data for tarp, simulated from thetas.
+        posterior: a posterior obtained from sbi.
+        num_posterior_samples: number of approximate posterior samples used
+            for ranking.
+        num_workers: number of CPU cores to use in parallel for running
+            infer_batch_size inferences.
+        infer_batch_size: batch size for workers.
+        show_progress_bar: whether to display a progress bar
+
+    Returns:
+        samples: posterior samples obtained by performing inference on xs
+            under the posterior
+
+    """
+    num_sim_samples = xs.shape[0]
+    xs_batches = torch.split(xs, infer_batch_size, dim=0)
+
+    if num_workers != 1:
+        # Parallelize the sequence of batches across workers.
+        # We use the solution proposed here: https://stackoverflow.com/a/61689175
+        # to update the pbar only after the workers finished a task.
+        with tqdm_joblib(
+            tqdm(
+                xs_batches,
+                disable=not show_progress_bar,
+                desc=f"Performing {num_sim_samples} posterior runs in"
+                f"{len(xs_batches)} batches.",
+                total=len(xs_batches),
+            )
+        ) as _:
+            samples: Tensor
+            samples = Parallel(
+                n_jobs=num_workers
+            )(  # pyright: ignore[reportAssignmentType]
+                delayed(infer_posterior_on_batch)(
+                    xs_batch, posterior, num_posterior_samples
+                )
+                for xs_batch in xs_batches
+            )
+    else:
+        pbar = tqdm(
+            total=num_sim_samples,
+            disable=not show_progress_bar,
+            desc=f"Running {num_sim_samples} samples for tarp analysis.",
+        )
+
+        with pbar:
+            samples = []
+            for xs_batch in xs_batches:
+                samples.append(
+                    infer_posterior_on_batch(xs_batch, posterior, num_posterior_samples)
+                )
+                pbar.update(infer_batch_size)
+            samples = torch.cat(samples, dim=1)
+
+    return samples
+
+
+def run_tarp(
+    samples: Tensor,
+    theta: Tensor,
+    references: Union[Tensor, None] = None,
+    distance: Callable = l2,
+    num_bins: Union[int, None] = None,
+    do_norm: bool = False,
+) -> Tuple[Tensor, Tensor]:
+    """
+    Estimates coverage of samples given true values theta with the TARP method.
+    Reference: `Lemos, Coogan et al 2023 <https://arxiv.org/abs/2302.03026>`_
+
+    The TARP diagnostic is a global diagnostic which can be used to check a
+    trained posterior against a set of true values of theta.
+
+    Args:
+        samples: The predicted parameter samples to compute the coverage of,
+                 these samples are expected to have shape
+                 ``(n_samples, n_sims, n_dims)``. These are obtained by
+                 sampling a trained posterior `n_samples` times. Multiple
+                 (posterior) samples for one observation are encouraged.
+        theta: The true parameter value theta. Theta is expected to
+                 have shape ``(n_sims, n_dims)``.
+        references: the reference points to use for the DRP regions, with
+                shape ``(n_references, n_sims, n_dims)``, or ``None``.
+                If ``None``, then reference points are chosen randomly from
+                the unit hypercube over the parameter space given by theta.
+                In other words, reference samples are drawn from the
+                following ``Uniform(low=theta.min(dim=-1),high=theta.max(dim=-1))``.
+        distance: the distance metric to use when computing the distance.
+                Should be a callable function that accepts two tensors and
+                computes the distance between them, e.g. given two tensors
+                of shape ``(batch, 3)`` and ``(batch,3)``, this function should
+                return ``(batch,1)`` distance values.
+                Possible values: ``sbi.diagnostics.tarp.l1`` or
+                ``sbi.diagnostics.tarp.l2``. ``l2`` is the default.
+        num_bins: number of bins to use for the credibility values.
+                If ``None``, then ``n_sims // 10`` bins are used.
+        do_norm : whether to normalize parameters before coverage test
+                (Default = True)
+
+    Returns:
+        ecp: Expected coverage probability (``ecp``)
+        alpha: credibility values
+
+    """
+    # TARP assumes that the predicted thetas are sampled from the "true"
+    # PDF num_samples times
+    theta = theta.detach() if len(theta.shape) != 2 else theta.detach().unsqueeze(0)
+    samples = samples.detach()
+
+    num_samples = samples.shape[0]  # samples per simulation
+    num_sims = samples.shape[-2]
+    num_dims = samples.shape[-1]
+
+    if num_bins is None:
+        num_bins = num_sims // 10
+
+    if theta.shape[-2] != num_sims:
+        raise ValueError("theta must have the same number of rows as samples")
+    if theta.shape[-1] != num_dims:
+        raise ValueError("theta must have the same number of columns as samples")
+
+    if do_norm:
+        lo = torch.min(theta, dim=-2, keepdims=True).values  # min along num_sims
+        hi = torch.max(theta, dim=-2, keepdims=True).values  # max along num_sims
+        samples = (samples - lo) / (hi - lo + 1e-10)
+        theta = (theta - lo) / (hi - lo + 1e-10)
+
+    assert len(theta.shape) == len(samples.shape)
+
+    if not isinstance(references, Tensor):
+        # obtain min/max per dimension of theta
+        lo = (
+            torch.min(theta, dim=-2).values.min(axis=0).values
+        )  # should be 0 if normalized
+        hi = (
+            torch.max(theta, dim=-2).values.max(axis=0).values
+        )  # should be 1 if normalized
+
+        refpdf = torch.distributions.Uniform(low=lo, high=hi)
+        references = refpdf.sample((1, num_sims))
+    else:
+        if len(references.shape) == 2:
+            # add singleton dimension in front
+            references = references.unsqueeze(0)
+
+        if len(references.shape) == 3 and references.shape[0] != 1:
+            raise ValueError(
+                f"""references must be a 2D array with a singular first
+                    dimension, received {references.shape}"""
+            )
+
+        if references.shape[-2] != num_sims:
+            raise ValueError(
+                f"references must have the same number samples as samples,"
+                f"received {references.shape[-2]} != {num_sims}"
+            )
+
+        if references.shape[-1] != num_dims:
+            raise ValueError(
+                "references must have the same number of dimensions as "
+                f"samples or theta, received {references.shape[-1]}"
+                f"!= {num_dims}"
+            )
+
+    assert len(references.shape) == len(
+        samples.shape
+    ), f"references {references.shape} != samples {samples.shape}"
+
+    # distances between references and samples
+    sample_dists = distance(references.expand(num_samples, -1, -1), samples)
+
+    # distances between references and true values
+    theta_dists = distance(references, theta)
+
+    # compute coverage, f in algorithm 2
+    coverage_values = torch.sum(sample_dists < theta_dists, axis=0) / num_samples
+    hist, bin_edges = torch.histogram(coverage_values, density=True, bins=num_bins)
+    stepsize = bin_edges[1] - bin_edges[0]
+    ecp = torch.cumsum(hist, dim=0) * stepsize
+
+    return torch.cat([Tensor([0]), ecp]), bin_edges
+
+
 class TARP:
     """
     Implementation taken from Lemos et al, 'Sampling-Based Accuracy Testing of
     Posterior Estimators for General Inference' https://arxiv.org/abs/2302.03026
+
+    The TARP diagnostic is a global diagnostic which can be used to check a
+    trained posterior against a set of true values of theta.
 
     This class implements the distance to random point as a diagnostic method
     for samples of posterior estimators.
@@ -124,7 +343,6 @@ class TARP:
                 then no seed
         """
         self.references = references
-        self.metric_name = metric
         self.n_bins = num_alpha_bins
         if self.n_bins and self.n_bins < 10:
             warnings.warn(
@@ -143,9 +361,16 @@ class TARP:
             )
         self.seed = seed
 
-    # this function currently does not perform any TARP related operation
-    # the purpose of the function is (a) to align with the sbc interface and
-    # (b) to provide the data which is required to run TARP
+        self.distance = None
+        if metric.lower() in ["l2", "euclidean"]:
+            self.distance = l2
+        elif metric.lower() in ["l1", "manhattan"]:
+            self.distance = l1
+        else:
+            raise ValueError(
+                "metric must be either 'euclidean' or 'manhattan'," f"received {metric}"
+            )
+
     def run(
         self,
         xs: Tensor,
@@ -169,50 +394,17 @@ class TARP:
 
         Returns:
             samples: posterior samples obtained by performing inference on xs
-                given the posterior
+                under the posterior
 
         """
-        num_sim_samples = xs.shape[0]
-        xs_batches = torch.split(xs, infer_batch_size, dim=0)
-
-        if num_workers != 1:
-            # Parallelize the sequence of batches across workers.
-            # We use the solution proposed here: https://stackoverflow.com/a/61689175
-            # to update the pbar only after the workers finished a task.
-            with tqdm_joblib(
-                tqdm(
-                    xs_batches,
-                    disable=not show_progress_bar,
-                    desc=f"Performing {num_sim_samples} posterior runs in"
-                    f"{len(xs_batches)} batches.",
-                    total=len(xs_batches),
-                )
-            ) as _:
-                samples: Tensor
-                samples = Parallel(n_jobs=num_workers)(  # pyright: ignore[reportAssignmentType]
-                    delayed(infer_posterior_on_batch)(
-                        xs_batch, posterior, num_posterior_samples
-                    )
-                    for xs_batch in xs_batches
-                )
-        else:
-            pbar = tqdm(
-                total=num_sim_samples,
-                disable=not show_progress_bar,
-                desc=f"Running {num_sim_samples} samples for tarp analysis.",
-            )
-
-            with pbar:
-                samples = []
-                for xs_batch in xs_batches:
-                    samples.append(
-                        infer_posterior_on_batch(
-                            xs_batch, posterior, num_posterior_samples
-                        )
-                    )
-                    pbar.update(infer_batch_size)
-                samples = torch.cat(samples, dim=1)
-
+        samples = prepare_estimates(
+            xs,
+            posterior,
+            num_posterior_samples,
+            num_workers,
+            infer_batch_size,
+            show_progress_bar,
+        )
         return samples
 
     def check(
@@ -226,99 +418,19 @@ class TARP:
 
         Args:
             samples: The predicted parameter samples to compute the coverage of,
-                     with shape ``(n_samples, n_sims, n_dims)``. These are
-                     obtained by sampling a trained posterior.  Multiple
+                     these samples are expected to have shape
+                     ``(n_samples, n_sims, n_dims)``. These are obtained by
+                     sampling a trained posterior `n_samples` times. Multiple
                      (posterior) samples for one observation are encouraged.
-            theta: The true parameter value theta, with shape ``(n_sims, n_dims)``.
+            theta: The true parameter value theta. Theta is expected to
+                     have shape ``(n_sims, n_dims)``.
 
         Returns:
             ecp: Expected coverage probability (``ecp``)
             alpha: credibility values
 
         """
-        # TARP assumes that the predicted thetas are sampled from the "true"
-        # PDF num_samples times
-        theta = theta.detach() if len(theta.shape) != 2 else theta.detach().unsqueeze(0)
-        samples = samples.detach()
 
-        num_samples = samples.shape[0]  # samples per simulation
-        num_sims = samples.shape[-2]
-        num_dims = samples.shape[-1]
-
-        if self.n_bins is None:
-            self.n_bins = num_sims // 10
-
-        if theta.shape[-2] != num_sims:
-            raise ValueError("theta must have the same number of rows as samples")
-        if theta.shape[-1] != num_dims:
-            raise ValueError("theta must have the same number of columns as samples")
-
-        if self.do_norm:
-            lo = torch.min(theta, dim=-2, keepdims=True).values  # min along num_sims
-            hi = torch.max(theta, dim=-2, keepdims=True).values  # max along num_sims
-            samples = (samples - lo) / (hi - lo + 1e-10)
-            theta = (theta - lo) / (hi - lo + 1e-10)
-
-        assert len(theta.shape) == len(samples.shape)
-
-        if not isinstance(self.references, Tensor):
-            # obtain min/max per dimension of theta
-            lo = (
-                torch.min(theta, dim=-2).values.min(axis=0).values
-            )  # should be 0 if normalized
-            hi = (
-                torch.max(theta, dim=-2).values.max(axis=0).values
-            )  # should be 1 if normalized
-
-            refpdf = torch.distributions.Uniform(low=lo, high=hi)
-            self.references = refpdf.sample((1, num_sims))
-        else:
-            if len(self.references.shape) == 2:
-                # add singleton dimension in front
-                self.references = self.references.unsqueeze(0)
-
-            if len(self.references.shape) == 3 and self.references.shape[0] != 1:
-                raise ValueError(
-                    f"""references must be a 2D array with a singular first
-                    dimension, received {self.references.shape}"""
-                )
-
-            if self.references.shape[-2] != num_sims:
-                raise ValueError(
-                    f"references must have the same number samples as samples,"
-                    f"received {self.references.shape[-2]} != {num_sims}"
-                )
-
-            if self.references.shape[-1] != num_dims:
-                raise ValueError(
-                    "references must have the same number of dimensions as "
-                    f"samples or theta, received {self.references.shape[-1]}"
-                    f"!= {num_dims}"
-                )
-
-        assert len(self.references.shape) == len(
-            samples.shape
-        ), f"references {self.references.shape} != samples {samples.shape}"
-
-        if self.metric_name.lower() in ["l2", "euclidean"]:
-            distance = l2
-        elif self.metric_name.lower() in ["l1", "manhattan"]:
-            distance = l1
-        else:
-            raise ValueError(
-                "metric must be either 'euclidean' or 'manhattan',"
-                f"received {self.metric_name}"
-            )
-
-        sample_dists = distance(self.references.expand(num_samples, -1, -1), samples)
-        theta_dists = distance(self.references, theta)
-
-        # compute coverage, f in algorithm 2
-        coverage_values = torch.sum(sample_dists < theta_dists, axis=0) / num_samples
-        hist, bin_edges = torch.histogram(
-            coverage_values, density=True, bins=self.n_bins
+        return run_tarp(
+            samples, theta, self.references, self.distance, self.n_bins, self.do_norm
         )
-        stepsize = bin_edges[1] - bin_edges[0]
-        ecp = torch.cumsum(hist, dim=0) * stepsize
-
-        return torch.cat([Tensor([0]), ecp]), bin_edges
--- tests/tarp_test.py
+++ tests/tarp_test.py
@@ -1,13 +1,13 @@
 import pytest
+from sbi.diagnostics.tarp import (TARP, infer_posterior_on_batch, l1, l2,
+                                  prepare_estimates, run_tarp)
+from sbi.inference import SNPE, simulate_for_sbi
+from sbi.simulators import linear_gaussian
+from sbi.utils import BoxUniform
 from scipy.stats import uniform
 from torch import Tensor, allclose, exp, eye, ones
 from torch.distributions import Normal, Uniform
 from torch.nn import L1Loss
-
-from sbi.diagnostics.tarp import TARP, infer_posterior_on_batch, l1, l2
-from sbi.inference import SNPE, simulate_for_sbi
-from sbi.simulators import linear_gaussian
-from sbi.utils import BoxUniform
 
 
 def generate_toy_gaussian(nsamples=100, nsims=100, ndims=5, covfactor=1.0):
@@ -155,6 +155,16 @@ def test_tarp_runs(onsamples):
     assert ecp.shape == alpha.shape
 
 
+def test_run_tarp_function(onsamples):
+    theta, samples = onsamples
+    tarp_ = TARP()
+
+    assert theta.shape != samples.shape
+
+    ecp, alpha = run_tarp(samples, theta)
+    assert ecp.shape == alpha.shape
+
+
 ######################################################################
 ## Reproduce Toy Examples in paper, see Section 4.1
 
@@ -169,6 +179,18 @@ def test_tarp_correct(onsamples):
 
     tarp = TARP(num_alpha_bins=30, metric="l1")
     ecp, alpha = tarp.check(samples, theta)
+
+    assert allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+
+
+def test_run_tarp_function_correct(onsamples):
+    theta, samples = onsamples
+
+    ecp, alpha = run_tarp(samples, theta, num_bins=30)
+
+    assert allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+
+    ecp, alpha = run_tarp(samples, theta, distance=l1, num_bins=30)
 
     assert allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
 
@@ -191,6 +213,24 @@ def test_tarp_correct_using_norm(onsamples):
     assert allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
 
 
+def test_run_tarp_function_correct_using_norm(onsamples):
+    theta, samples = onsamples
+
+    # tarp = TARP(num_alpha_bins=30, norm=True)
+    ecp, alpha = run_tarp(samples, theta, num_bins=30, do_norm=False)
+
+    assert allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+    assert (
+        ecp - alpha
+    ).abs().sum() < 1.0  # integral of residuals should vanish, fig.2 in paper
+
+    # tarp_ = TARP(num_alpha_bins=30, norm=True, metric="l1")
+    ecp, alpha = run_tarp(samples, theta, num_bins=30, do_norm=False, distance=l1)
+
+    # TARP detects that this is a correct representation of the posterior
+    assert allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+
+
 def test_tarp_detect_overdispersed(oversamples):
     theta, samples = oversamples
 
@@ -202,6 +242,23 @@ def test_tarp_detect_overdispersed(oversamples):
 
     tarp_ = TARP(num_alpha_bins=30, norm=True, metric="l1")
     ecp, alpha = tarp_.check(samples, theta)
+
+    # TARP detects that this is NOT a correct representation of the posterior
+    # hence we test for not allclose
+    assert not allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+
+
+def test_run_tarp_function_detect_overdispersed(oversamples):
+    theta, samples = oversamples
+
+    # tarp = TARP(num_alpha_bins=30, norm=True)
+    ecp, alpha = run_tarp(samples, theta, num_bins=30, do_norm=True)
+
+    assert not allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+    assert (ecp - alpha).abs().sum() > 3.0  # integral is nonzero, fig.2 in paper
+
+    # tarp_ = TARP(num_alpha_bins=30, norm=True, metric="l1")
+    ecp, alpha = run_tarp(samples, theta, num_bins=30, do_norm=True, distance=l1)
 
     # TARP detects that this is NOT a correct representation of the posterior
     # hence we test for not allclose
@@ -225,6 +282,23 @@ def test_tarp_detect_underdispersed(undersamples):
     assert not allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
 
 
+def test_run_tarp_function_detect_underdispersed(undersamples):
+    theta, samples = undersamples
+
+    # tarp = TARP(num_alpha_bins=30, norm=True)
+    ecp, alpha = run_tarp(samples, theta, num_bins=30, do_norm=True)
+
+    assert not allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+    assert (ecp - alpha).abs().sum() > 3.0  # integral is nonzero, fig.2 in paper
+
+    # tarp_ = TARP(num_alpha_bins=30, norm=True, metric="l1")
+    ecp, alpha = run_tarp(samples, theta, num_bins=30, do_norm=True, distance=l1)
+
+    # TARP detects that this is NOT a correct representation of the posterior
+    # hence we test for not allclose
+    assert not allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+
+
 def test_tarp_detect_bias(biased):
     theta, samples = biased
     print(theta.shape, samples.shape)
@@ -242,13 +316,31 @@ def test_tarp_detect_bias(biased):
     assert not allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
 
 
+def test_run_tarp_function_detect_bias(biased):
+    theta, samples = biased
+    print(theta.shape, samples.shape)
+
+    # tarp = TARP(num_alpha_bins=30, norm=True)
+    ecp, alpha = run_tarp(samples, theta, num_bins=30, do_norm=True)
+
+    assert not allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+    assert (ecp - alpha).abs().sum() > 3.0  # integral is nonzero, fig.2 in paper
+
+    # tarp_ = TARP(num_alpha_bins=30, norm=True, metric="l1")
+    ecp, alpha = run_tarp(samples, theta, num_bins=30, do_norm=True, distance=l1)
+
+    # TARP detects that this is NOT a correct representation of the posterior
+    # hence we test for not allclose
+    assert not allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+
+
 ######################################################################
 ## Check TARP with SBI
 
 
 @pytest.mark.slow
 @pytest.mark.parametrize("method", [SNPE])
-def test_batched_inference(method, model="mdn"):
+def test_batched_prepare_estimates(method, model="mdn"):
     """Tests running inference and checking samples with tarp."""
 
     num_dim = 2
@@ -283,8 +375,10 @@ def test_batched_inference(method, model="mdn"):
     assert samples.shape[1:] == thetas.shape
     assert samples.shape[0] == num_posterior_samples
 
-    tarp = TARP(num_alpha_bins=30, norm=True, metric="l2")
-    samples_ = tarp.run(xs, posterior, num_posterior_samples, infer_batch_size=32)
+    # tarp = TARP(num_alpha_bins=30, norm=True, metric="l2")
+    samples_ = prepare_estimates(
+        xs, posterior, num_posterior_samples, infer_batch_size=32
+    )
 
     assert samples_.shape != thetas.shape
     assert samples_.shape[1:] == thetas.shape
@@ -329,6 +423,48 @@ def test_consistent_tarp_results_with_posterior(method, model="mdn"):
 
     print(thetas.shape, samples.shape)
     ecp, alpha = tarp.check(samples, thetas)
+
+    assert allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
+    assert (ecp - alpha).abs().sum() < 1.0
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("method", [SNPE])
+def test_consistent_run_tarp_function_results_with_posterior(method, model="mdn"):
+    """Tests running inference and checking samples with tarp."""
+
+    num_dim = 2
+    prior = BoxUniform(-ones(num_dim), ones(num_dim))
+
+    num_simulations = 1000
+    max_num_epochs = 20
+    num_tarp_sims = 100
+
+    likelihood_shift = -1.0 * ones(num_dim)
+    likelihood_cov = 0.3 * eye(num_dim)
+
+    def simulator(theta):
+        return linear_gaussian(theta, likelihood_shift, likelihood_cov)
+
+    inferer = method(prior, show_progress_bars=False, density_estimator=model)
+
+    theta, x = simulate_for_sbi(simulator, prior, num_simulations)
+
+    _ = inferer.append_simulations(theta, x).train(
+        training_batch_size=100, max_num_epochs=max_num_epochs
+    )
+
+    posterior = inferer.build_posterior()
+    num_posterior_samples = 256
+
+    thetas = prior.sample((num_tarp_sims,))
+    xs = simulator(thetas)
+
+    # tarp = TARP(num_alpha_bins=30, norm=True, metric="l2")
+
+    samples = prepare_estimates(xs, posterior, num_posterior_samples)
+
+    ecp, alpha = run_tarp(samples, thetas, num_bins=30, do_norm=True)
 
     assert allclose((ecp - alpha).abs().max(), Tensor([0.0]), atol=1e-1)
     assert (ecp - alpha).abs().sum() < 1.0
