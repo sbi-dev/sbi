98493ddea72c09c954e5c2cdeb8376699773cfc9
Jan Boelts <jan.boelts@mailbox.org>
2024-07-26 17:02:29 +0200

    fix #1204: c2st with constant features; refactor.

--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -8,13 +8,11 @@ import math
 import numpy as np
 import pytest
 import torch
-from sklearn.neural_network import MLPClassifier
 from torch.distributions import MultivariateNormal as tmvn
 
 from sbi.utils.metrics import (
     biased_mmd_hypothesis_test,
     c2st,
-    c2st_scores,
     posterior_shrinkage,
     posterior_zscore,
     unbiased_mmd_squared_hypothesis_test,
@@ -51,8 +49,9 @@ C2ST_TESTCASECONFIG = [
     "dist_sigma, c2st_lowerbound, c2st_upperbound,",
     C2ST_TESTCASECONFIG,
 )
+@pytest.mark.parametrize("classifier", ("rf", "mlp"))
 def test_c2st_with_different_distributions(
-    dist_sigma, c2st_lowerbound, c2st_upperbound
+    dist_sigma, c2st_lowerbound, c2st_upperbound, classifier: str
 ):
     ndim = 10
     nsamples = 1024
@@ -65,79 +64,21 @@ def test_c2st_with_different_distributions(
     X = refdist.sample((nsamples,))
     Y = otherdist.sample((nsamples,))
 
-    obs_c2st = c2st(X, Y)
+    obs_c2st = c2st(X, Y, classifier=classifier)
 
-    assert len(obs_c2st) > 0
-    assert c2st_lowerbound < obs_c2st[0]
-    assert obs_c2st[0] <= c2st_upperbound
+    assert c2st_lowerbound < obs_c2st <= c2st_upperbound
 
 
-@pytest.mark.slow
-@pytest.mark.parametrize(
-    "dist_sigma, c2st_lowerbound, c2st_upperbound,",
-    C2ST_TESTCASECONFIG,
-)
-def test_c2st_with_different_distributions_mlp(
-    dist_sigma, c2st_lowerbound, c2st_upperbound
-):
-    ndim = 10
-    nsamples = 1024
+@pytest.mark.parametrize("dims_constant", (1, 2))
+def test_c2st_with_constant_features(dims_constant: int):
+    num_dim = 2
+    num_samples = 1024
+    x = torch.randn(num_samples, num_dim)
+    y = torch.randn(num_samples, num_dim)
+    x[:, :dims_constant] = 1.0
+    y[:, :dims_constant] = 1.0
 
-    refdist = tmvn(loc=torch.zeros(ndim), covariance_matrix=torch.eye(ndim))
-    otherdist = tmvn(
-        loc=dist_sigma + torch.zeros(ndim), covariance_matrix=torch.eye(ndim)
-    )
-
-    X = refdist.sample((nsamples,))
-    Y = otherdist.sample((nsamples,))
-
-    obs_c2st = c2st(X, Y, classifier="mlp")
-
-    assert len(obs_c2st) > 0
-    assert c2st_lowerbound < obs_c2st[0]
-    assert obs_c2st[0] <= c2st_upperbound
-
-
-@pytest.mark.slow
-@pytest.mark.parametrize(
-    "dist_sigma, c2st_lowerbound, c2st_upperbound,",
-    C2ST_TESTCASECONFIG,
-)
-def test_c2st_scores(dist_sigma, c2st_lowerbound, c2st_upperbound):
-    ndim = 10
-    nsamples = 1024
-
-    xnormal = tmvn(loc=torch.zeros(ndim), covariance_matrix=torch.eye(ndim))
-    ynormal = tmvn(
-        loc=dist_sigma + torch.zeros(ndim), covariance_matrix=torch.eye(ndim)
-    )
-
-    X = xnormal.sample((nsamples,))
-    Y = ynormal.sample((nsamples,))
-
-    obs_c2st = c2st_scores(X, Y)
-
-    assert hasattr(obs_c2st, "mean")
-    assert c2st_lowerbound < obs_c2st.mean()
-    assert obs_c2st.mean() <= c2st_upperbound
-
-    clf_class = MLPClassifier
-    clf_kwargs = {
-        "activation": "relu",
-        "hidden_layer_sizes": (8 * X.shape[1], X.shape[1]),
-        "max_iter": 100,
-        "solver": "adam",
-        "early_stopping": True,
-        "n_iter_no_change": 20,
-    }
-
-    obs2_c2st = c2st_scores(X, Y, clf_class=clf_class, clf_kwargs=clf_kwargs)
-
-    assert hasattr(obs2_c2st, "mean")
-    assert c2st_lowerbound < obs2_c2st.mean()
-    assert obs2_c2st.mean() <= c2st_upperbound
-
-    assert np.allclose(obs2_c2st, obs_c2st, atol=0.05)
+    c2st(x, y)
 
 
 @pytest.mark.slow
