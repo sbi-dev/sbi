ba93f9b6a9f5bcdf6191da505db54bfac571f6df
turnmanh <17703667+turnmanh@users.noreply.github.com>
2024-08-12 17:46:37 +0200

    fixed early stopping bug

--- sbi/inference/fmpe/base.py
+++ sbi/inference/fmpe/base.py
@@ -135,7 +135,7 @@ class FMPE(NeuralInference):
             self.optimizer = optim.Adam(
                 list(self._neural_net.net.parameters()), lr=learning_rate
             )
-            self.epoch, self._val_loss = 0, float("-Inf")
+            self.epoch, self._val_loss = 0, float("Inf")
 
         while self.epoch <= max_num_epochs and not self._converged(
             self.epoch, stop_after_epochs
