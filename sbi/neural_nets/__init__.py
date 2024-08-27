e62bd0e1e0a0e2eadc282f31a7d0db0611af2b6c
michaeldeistler <michael.deistler95@gmail.com>
2024-08-27 14:14:17 +0200

    no more imports of the builders from sbi.neural_nets

--- sbi/neural_nets/__init__.py
+++ sbi/neural_nets/__init__.py
@@ -1,26 +1,12 @@
-from sbi.neural_nets.classifier import (
-    build_linear_classifier,
-    build_mlp_classifier,
-    build_resnet_classifier,
-)
 from sbi.neural_nets.embedding_nets import (
     CNNEmbedding,
     FCEmbedding,
     PermutationInvariantEmbedding,
 )
-from sbi.neural_nets.estimators import ConditionalDensityEstimator, NFlowsFlow
 from sbi.neural_nets.factory import (
     classifier_nn,
     flowmatching_nn,
     likelihood_nn,
     posterior_nn,
+    posterior_score_nn,
 )
-from sbi.neural_nets.flow import (
-    build_made,
-    build_maf,
-    build_maf_rqs,
-    build_nsf,
-    build_zuko_maf,
-)
-from sbi.neural_nets.mdn import build_mdn
-from sbi.neural_nets.mnle import MixedDensityEstimator, build_mnle
---
+++ sbi/neural_nets/build_functions/__init__.py
@@ -0,0 +1,20 @@
+from sbi.neural_nets.build_functions.categorial import build_categoricalmassestimator
+from sbi.neural_nets.build_functions.classifier import (
+    build_linear_classifier,
+    build_mlp_classifier,
+    build_resnet_classifier,
+)
+from sbi.neural_nets.build_functions.flow import (
+    build_made,
+    build_maf,
+    build_maf_rqs,
+    build_nsf,
+    build_zuko_maf,
+)
+from sbi.neural_nets.build_functions.flow_matcher import (
+    build_mlp_flowmatcher,
+    build_resnet_flowmatcher,
+)
+from sbi.neural_nets.build_functions.mdn import build_mdn
+from sbi.neural_nets.build_functions.mnle import build_mnle
+from sbi.neural_nets.build_functions.score_nets import build_score_estimator
