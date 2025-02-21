fd0f96414412b596a4766717867d3d4f50f88b5a
manuelgloeckler <manu.gloeckler@hotmail.de>
2025-02-21 10:14:22 +0100

    Ruff linting

--- sbi/inference/posteriors/score_posterior.py
+++ sbi/inference/posteriors/score_posterior.py
@@ -1,7 +1,6 @@
 # This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
 # under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
-from functools import partial
 from typing import Dict, Optional, Union
 
 import torch
