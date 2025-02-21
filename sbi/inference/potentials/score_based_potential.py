bfe6df2c7e5d3bf6fe4a649228f9e33e5373997c
manuelgloeckler <manu.gloeckler@hotmail.de>
2025-02-21 10:12:06 +0100

    Remove assert that IID data is not supported

--- sbi/inference/potentials/score_based_potential.py
+++ sbi/inference/potentials/score_based_potential.py
@@ -232,9 +232,6 @@ class PosteriorScoreBasedPotential(BasePotential):
         x_density_estimator = reshape_to_batch_event(
             self.x_o, event_shape=self.score_estimator.condition_shape
         )
-        assert x_density_estimator.shape[0] == 1, (
-            "PosteriorScoreBasedPotential supports only x batchsize of 1`."
-        )
 
         flow = self.get_continuous_normalizing_flow(
             condition=x_density_estimator, atol=atol, rtol=rtol, exact=exact
