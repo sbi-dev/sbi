f7329712f0f104fe387648d3532de977e70c673c
manuelgloeckler <38903899+manuelgloeckler@users.noreply.github.com>
2025-03-14 19:24:24 +0100

    Fix abstract class error (#1451)
    * Fix
    
    * Instead raise error
    
    * Value error is better
    
    * formatting

--- sbi/inference/potentials/score_fn_iid.py
+++ sbi/inference/potentials/score_fn_iid.py
@@ -647,6 +647,9 @@ class JacCorrectedScoreFn(BaseGaussCorrectedScoreFunction):
     in some cases.
     """
 
+    def posterior_precision_est_fn(self, conditions: Tensor) -> Tensor:
+        raise ValueError("This method does not use the posterior precision estimation.")
+
     def marginal_denoising_posterior_precision_est_fn(
         self,
         time: Tensor,
