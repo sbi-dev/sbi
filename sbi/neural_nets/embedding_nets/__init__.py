996430bd7f5ec6b1f88e33eb14777373cf61adf9
Stefan Wahl <s.wahl704@gmail.com>
2025-03-18 10:44:11 +0100

    Update init

--- sbi/neural_nets/embedding_nets/__init__.py
+++ sbi/neural_nets/embedding_nets/__init__.py
@@ -3,3 +3,6 @@ from sbi.neural_nets.embedding_nets.fully_connected import FCEmbedding
 from sbi.neural_nets.embedding_nets.permutation_invariant import (
     PermutationInvariantEmbedding,
 )
+from sbi.neural_nets.embedding_nets.resnet import (
+    ResNet,
+)
