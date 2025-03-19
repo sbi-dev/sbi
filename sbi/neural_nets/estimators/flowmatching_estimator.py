770e29abaef283eb4605905ae7206a3474edfaec
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2025-03-19 09:50:34 +0100

    common methods for flow and sde estimators

--- sbi/neural_nets/estimators/flowmatching_estimator.py
+++ sbi/neural_nets/estimators/flowmatching_estimator.py
@@ -4,14 +4,14 @@ from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
-import zuko
+
 from torch import Tensor
 from torch.distributions import Transform
 from zuko.distributions import DiagNormal, NormalizingFlow
 from zuko.transforms import FreeFormJacobianTransform
 from zuko.utils import broadcast
 
-from sbi.neural_nets.estimators.base import ConditionalDensityEstimator
+from sbi.neural_nets.estimators.vector_field_estimator import ConditionalVectorFieldEstimator
 
 
 # abstract class to ensure forward signature for flow matching networks
@@ -20,14 +20,13 @@ class VectorFieldNet(nn.Module, ABC):
     def forward(self, theta: Tensor, x: Tensor, t: Tensor) -> Tensor: ...
 
 
-class FlowMatchingEstimator(ConditionalDensityEstimator):
+class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
     def __init__(
         self,
         net: VectorFieldNet,
         input_shape: torch.Size,
         condition_shape: torch.Size,
         embedding_net: nn.Module,
-        zscore_transform_input: Optional[Transform] = None,
         num_freqs: int = 3,
         noise_scale: float = 1e-3,
         **kwargs,
@@ -40,7 +39,6 @@ class FlowMatchingEstimator(ConditionalDensityEstimator):
             condition_shape: Shape of the condition, e.g., the data.
             noise_scale: Scale of the noise added to the vector field.
             embedding_net: Embedding network for the condition.
-            zscore_transform_input: Transform to z-score the input.
             num_freqs: Number of frequencies to use for the positional time encoding.
             noise_scale: Scale of the noise added to the vector field.
         """
@@ -51,9 +49,6 @@ class FlowMatchingEstimator(ConditionalDensityEstimator):
 
         self.noise_scale = noise_scale
         # Identity transform for z-scoring the input
-        if zscore_transform_input is None:
-            zscore_transform_input = zuko.transforms.IdentityTransform()
-        self.zscore_transform_input: Transform = zscore_transform_input
         self._embedding_net = embedding_net
 
         self.register_buffer("freqs", torch.arange(1, num_freqs + 1) * math.pi)
@@ -71,18 +66,17 @@ class FlowMatchingEstimator(ConditionalDensityEstimator):
 
         # embed the input and condition
         embedded_condition = self._embedding_net(condition)
-        zscored_input = self.zscore_transform_input(input)
 
         # broadcast to match shapes of theta, x, and t
-        zscored_input, embedded_condition, t = broadcast(
-            zscored_input,  # type: ignore
+        input, embedded_condition, t = broadcast(
+            input,  # type: ignore
             embedded_condition,
             t,
             ignore=1,
         )
 
         # return the estimated vector field
-        return self.net(theta=zscored_input, x=embedded_condition, t=t)
+        return self.net(theta=input, x=embedded_condition, t=t)
 
     def loss(self, input: Tensor, condition: Tensor, **kwargs) -> Tensor:
         """Return the loss for training the density estimator. More precisely,
@@ -111,56 +105,30 @@ class FlowMatchingEstimator(ConditionalDensityEstimator):
             (self.forward(theta_prime, condition, t) - vector_field) ** 2, dim=-1
         )
 
-    def log_prob(self, input: Tensor, condition: Tensor, **kwargs) -> Tensor:
-        input_batch_dim = input.shape[1]
-        condition_batch_dim = condition.shape[0]
-
-        assert condition_batch_dim == input_batch_dim, (
-            f"Batch shape of condition {condition_batch_dim} and input "
-            f"{input_batch_dim} do not match."
-        )
-
-        # the flow will apply and take into account input zscoring.
-        log_probs = self.flow(condition=condition).log_prob(input)
-        return log_probs
-
-    def sample(self, sample_shape: torch.Size, condition: Tensor, **kwargs) -> Tensor:
-        # the flow will take care of inverse z-scoring.
-        samples = self.flow(condition=condition).sample(sample_shape)
-        return samples
-
-    def sample_and_log_prob(
-        self, sample_shape: torch.Size, condition: Tensor, **kwargs
-    ) -> Tuple[Tensor, Tensor]:
-        samples, log_probs = self.flow(condition).rsample_and_log_prob(sample_shape)
-
-        return samples, log_probs
-
-    def flow(self, condition: Tensor) -> NormalizingFlow:
-        """Return the normalizing flow.
-
-        Here, the actual continuous normalizing flow is created via zuko. It
-        gives access to the log_prob() and sample() methods and internally calls
-        ODE routines to compute them.
+    def ode_fn(self, input: Tensor, condition: Tensor, t: Tensor) -> Tensor:
+        """
+        ODE flow function of the vector field estimator.
 
         Args:
-            condition: Condition for the normalizing flow.
+            input: variable whose distribution is estimated.
+            condition: Conditioning variable.
+            t: Time.
 
         Returns:
-            NormalizingFlow: flow with log_prob and sample methods via probability ODEs.
+            Estimated vector field.
         """
+        return self.forward(input, condition, t)
 
-        transform = zuko.transforms.ComposedTransform(
-            FreeFormJacobianTransform(
-                f=lambda t, input: self.forward(input, condition, t),
-                t0=condition.new_tensor(0.0),
-                t1=condition.new_tensor(1.0),
-                phi=(condition, *self.net.parameters()),
-            ),
-            self.zscore_transform_input,
-        )
+    def score(self, input: Tensor, condition: Tensor, t: Tensor) -> Tensor:
+        """Score function of the vector field estimator.
 
-        return NormalizingFlow(
-            transform=transform,
-            base=DiagNormal(self.zeros, self.ones).expand(condition.shape[:-1]),
-        )
+        Args:
+            input: variable whose distribution is estimated.
+            condition: Conditioning variable.
+            t: Time.
+
+        Returns:
+            Score function of the vector field estimator.
+        """
+        v = self.forward(input, condition, t)
+        return (-(1 - t) * v - input) / torch.maximum(t, torch.tensor(1e-6))
