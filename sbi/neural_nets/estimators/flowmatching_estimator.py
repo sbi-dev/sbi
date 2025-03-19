57e24838888b6ab310d035159aaff27bdfd176b9
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2025-03-19 18:38:58 +0100

    Update flowmatching_estimator.py

--- sbi/neural_nets/estimators/flowmatching_estimator.py
+++ sbi/neural_nets/estimators/flowmatching_estimator.py
@@ -1,14 +1,11 @@
 import math
 from abc import ABC, abstractmethod
-from typing import Optional, Tuple
+from typing import Optional
 
 import torch
 import torch.nn as nn
 
 from torch import Tensor
-from torch.distributions import Transform
-from zuko.distributions import DiagNormal, NormalizingFlow
-from zuko.transforms import FreeFormJacobianTransform
 from zuko.utils import broadcast
 
 from sbi.neural_nets.estimators.vector_field_estimator import ConditionalVectorFieldEstimator
@@ -21,14 +18,26 @@ class VectorFieldNet(nn.Module, ABC):
 
 
 class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
+    r"""
+    
+    Rectified flow matching estimator class that estimates the conditional vector field, 
+    i.e. the expectation E[x_t - x_0 | x_0 = x].
+
+    """
+    
+    SCORE_DEFINED: bool = True  # Whether the score is defined for this estimator. Required for gradient-based methods.
+    SDE_DEFINED: bool = True  # Whether the SDE functions - score, drift and diffusion - are defined for this estimator. 
+    MARGINALS_DEFINED: bool = True  # Whether the marginals are defined for this estimator. Required for iid methods.
+    
     def __init__(
         self,
         net: VectorFieldNet,
         input_shape: torch.Size,
         condition_shape: torch.Size,
-        embedding_net: nn.Module,
+        embedding_net: Optional[nn.Module] = None,
         num_freqs: int = 3,
         noise_scale: float = 1e-3,
+        zscore_transform_input=None,  # This is ignored and will be removed.
         **kwargs,
     ) -> None:
         """Creates a vector field estimator for Flow Matching.
@@ -49,7 +58,7 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
 
         self.noise_scale = noise_scale
         # Identity transform for z-scoring the input
-        self._embedding_net = embedding_net
+        self._embedding_net = embedding_net if embedding_net is not None else nn.Identity()
 
         self.register_buffer("freqs", torch.arange(1, num_freqs + 1) * math.pi)
         self.register_buffer('zeros', torch.zeros(input_shape))
@@ -78,7 +87,7 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
         # return the estimated vector field
         return self.net(theta=input, x=embedded_condition, t=t)
 
-    def loss(self, input: Tensor, condition: Tensor, **kwargs) -> Tensor:
+    def loss(self, input: Tensor, condition: Tensor, times: Optional[Tensor] = None, **kwargs) -> Tensor:
         """Return the loss for training the density estimator. More precisely,
         we compute the conditional flow matching loss with naive optimal
         trajectories as described in the original paper.
@@ -86,11 +95,16 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
         Args:
             theta: Parameters.
             x: Observed data.
+            times: Time steps to compute the loss at. Optional, will sample from [0, 1] if not provided.
+        
+        Returns:
+            Loss value.
         """
         # randomly sample the time steps to compare the vector field at
         # different time steps
-        t = torch.rand(input.shape[:-1], device=input.device, dtype=input.dtype)
-        t_ = t[..., None]
+        if times is None:
+            times = torch.rand(input.shape[:-1], device=input.device, dtype=input.dtype)
+        t_ = times[..., None]
 
         # sample from probability path at time t
         # TODO: Change to notation from Lipman et al. or Tong et al.
@@ -102,7 +116,7 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
 
         # compute the mean squared error between the vector fields
         return torch.mean(
-            (self.forward(theta_prime, condition, t) - vector_field) ** 2, dim=-1
+            (self.forward(theta_prime, condition, times) - vector_field) ** 2, dim=-1
         )
 
     def ode_fn(self, input: Tensor, condition: Tensor, t: Tensor) -> Tensor:
@@ -132,3 +146,65 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
         """
         v = self.forward(input, condition, t)
         return (-(1 - t) * v - input) / torch.maximum(t, torch.tensor(1e-6))
+    
+
+    def drift_fn(self, input: Tensor, times: Tensor) -> Tensor:
+        """Drift function for the flow matching estimator.
+
+        Args:
+            input: Original data, x0.
+            times: SDE time variable in [0,1].
+
+        Returns:
+            Drift function at a given time.
+        """
+        return - input / torch.maximum(1 - times, torch.tensor(1e-6))
+
+    def diffusion_fn(self, input: Tensor, times: Tensor) -> Tensor:
+        """Diffusion function for the flow matching estimator.
+
+        Args:
+            input: Original data, x0.
+            times: SDE time variable in [0,1].
+
+        Returns:
+            Diffusion function at a given time.
+        """
+        return torch.sqrt(2 * times /  torch.maximum(1 - times, torch.tensor(1e-6)))
+    
+    def mean_t_fn(self, times: Tensor) -> Tensor:
+        r"""Linear coefficient of the perturbation kernel mean for the flow matching estimator.
+
+        The general form of the perturbation kernel for rectified flows is:
+            mean_t(t) = (1 - t) * mean_0 + t * mean_base
+        
+        So far, the implementation of iid methods assumes that the mean_base is 0.
+
+        Args:
+            times: SDE time variable in [0,1].
+
+        Returns:
+            Mean function at a given time.
+        """
+        mean_t = 1 - times
+        for _ in range(len(self.input_shape)):
+            mean_t = mean_t.unsqueeze(-1)
+        return mean_t
+
+    def std_fn(self, times: Tensor) -> Tensor:
+        """Standard deviation of the perturbation kernel for the flow matching estimator.
+
+        The general form of the perturbation kernel for rectified flows is:
+            std_t(t) = t * std_base
+
+        Args:
+            times: SDE time variable in [0,1].
+
+        Returns:
+            Standard deviation at a given time.
+        """
+        std_t = times * self.std_base
+        for _ in range(len(self.input_shape)):
+            std_t = std_t.unsqueeze(-1)
+        return std_t
+  
