7e3a1086bd596b713e3bbb84c8873b3340ff932f
hayden-johnson <haydenjohnson054@gmail.com>
2025-03-19 15:37:33 +0100

    refactor to leverage MCMCPosterior class and return sampler

--- sbi/utils/mcmc_utils.py
+++ sbi/utils/mcmc_utils.py
@@ -1,82 +1,82 @@
 # This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
 # under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
+import inspect
 import logging
-from typing import Callable, Optional, Union
+from typing import Any, Callable, Optional
 
-from numpy import ndarray
+import torch
 from torch import Tensor
 
-from sbi.samplers.mcmc import SliceSamplerSerial, SliceSamplerVectorized
-from sbi.utils.torchutils import tensor2numpy
+from sbi.inference import MCMCPosterior
+from sbi.utils import mcmc_transform
 
 
-def get_mcmc_samples(
-    potential_fn: Callable,
-    initial_params: Union[ndarray, Tensor],
-    num_samples: int = 1000,
-    vectorized: bool = True,
-    num_workers: int = 1,
-    num_chains: int = 1,
-    x: Optional[Union[ndarray, Tensor]] = None,
-    **kwargs,
-) -> ndarray:
+def get_direct_sampler(
+    potential_fn: Callable, prior: Any, x: Optional[Tensor] = None, **kwargs
+) -> Callable:
     """
-    Run slice MCMC sampling using a specified potential function.
+    Returns a sampler from a MCMCPosterior object, given user-defined potential
+    function and prior.
+
+    The user-defined potential can be conditional (accepts theta and x as arguments)
+    or unconditional (accepting only theta).
 
     Args:
         potential_fn: User defined potential function. Must be of type Callable.
-        initial_params: Initial parameters for the MCMC sampler. First dimension
-            must match num_chains (default=1).
-        num_samples: Number of samples to draw.
-        vectorized: Whether to use the vectorized implementation.
-        num_workers: Number of workers to use for vectorized sampling.
-        x: The input to the potential function.
+        prior: Prior distribution for parameter transformation and initialization.
+        x: Conditional x value. Provided if using a conditional potential function.
 
     Returns:
-        Samples from the posterior distribution of shape
-            [num_chains, num_samples, parameter_dim]
+        Callable sampling function from MCMCPosterior object.
     """
-    # Check that the first dimension of initial_params matches num_chains.
-    assert initial_params.shape[0] == num_chains, (
-        "first dimension of initial_params must match num_chains"
+    # build transformation to unrestricted space for sampling
+    transform = mcmc_transform(prior)
+
+    # potential_fn must take 1 or 2 required arguments: (theta) or (theta, x)
+    num_args = num_required_args(potential_fn)
+    assert num_args > 0 and num_args < 3, (
+        "potential_fn must take 1-2 required arguments"
     )
+    is_conditional = num_required_args == 2
 
-    # Convert initial_params to NumPy array if necessary.
-    if isinstance(initial_params, Tensor):
-        initial_params = tensor2numpy(initial_params)
+    if is_conditional:
+        assert x is not None, "x must be provided if potential_fn is conditional"
+        posterior = MCMCPosterior(potential_fn, prior, theta_transform=transform)
+        posterior.set_default_x(x)
 
-    if x is None:
+    else:
         logging.warning(
             "x has not been provided. Using unconditional potential function."
         )
-    else:
-        if isinstance(x, Tensor):
-            x = tensor2numpy(x)
 
-    # Define a log probability function that the sampler expects.
-    def lp_fn(theta: ndarray, x: Optional[ndarray] = x) -> float:
-        logp = potential_fn(theta, x=x)
-        return logp
+        # define an unconditional potential function (ignores x)
+        def unconditional_potential_fn(theta, x):
+            return potential_fn(theta)
 
-    if vectorized:
-        # Instantiate the vectorized slice sampler.
-        sampler = SliceSamplerVectorized(
-            log_prob_fn=lp_fn,
-            init_params=initial_params,
-            num_workers=num_workers,
-            num_chains=num_chains,
-            **kwargs,
+        posterior = MCMCPosterior(
+            unconditional_potential_fn, prior, theta_transform=transform, **kwargs
         )
-    else:
-        # Instantiate the serial slice sampler.
-        sampler = SliceSamplerSerial(
-            log_prob_fn=lp_fn,
-            init_params=initial_params,
-            num_chains=num_chains,
-            **kwargs,
-        )
+        posterior.set_default_x(torch.zeros(1))  # set default_x to dummy value
 
-    # Run the sampler
-    samples = sampler.run(num_samples)
-    return samples
+    return posterior.sample
+
+
+def num_required_args(func):
+    """
+    Utility for counting the number of positional args in a function.
+
+    Args:
+        func: A callable function.
+
+    Returns:
+        Number of required positional arguments.
+    """
+    sig = inspect.signature(func)
+    return sum(
+        1
+        for param in sig.parameters.values()
+        if param.kind
+        in (inspect.Parameter.POSITIONAL_ONLY, inspect.Parameter.POSITIONAL_OR_KEYWORD)
+        and param.default is inspect._empty
+    )
