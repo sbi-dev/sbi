0ade94aeb2d59ad19a00aabd962b76e3ad37dcf2
Seth Axen <seth@sethaxen.com>
2025-03-19 11:15:38 +0100

    Add Pyro distribution wrapper and basic properties tests

--- sbi/utils/pyroutils.py
+++ sbi/utils/pyroutils.py
@@ -1,10 +1,16 @@
 # This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
 # under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
-from typing import Any, Callable
+from abc import abstractmethod
+from typing import Any, Callable, Tuple
 
+import pyro.distributions
+import torch
+import torch.distributions.constraints as constraints
 from pyro import poutine as poutine
 from torch.distributions import biject_to
+
+from sbi.neural_nets.estimators.base import ConditionalEstimator
 
 
 def get_transforms(model: Callable, *model_args: Any, **model_kwargs: Any):
@@ -33,3 +39,125 @@ def get_transforms(model: Callable, *model_args: Any, **model_kwargs: Any):
             transforms[name] = biject_to(fn.support).inv
 
     return transforms
+
+
+class EstimatorDistribution(pyro.distributions.TorchDistribution):
+    """
+    Base class for a conditioned `sbi` estimator wrapped as a Pyro distribution.
+    """
+
+    @property
+    def support(self):
+        # TODO: check if we can infer a more specific support from the estimator
+        return constraints.real  # Assume continuous values
+
+    def __init__(self, estimator: Any, condition: torch.Tensor):
+        self._estimator = estimator
+        self._condition_shape, event_shape = self.get_condition_and_event_shapes(
+            estimator
+        )
+        self._check_condition_shape(condition)
+        self._condition = condition
+        self._condition_reshaped = condition.reshape(-1, *self._condition_shape)
+        super().__init__(
+            batch_shape=condition.shape[: -len(self._condition_shape)],
+            event_shape=event_shape,
+        )
+
+    @abstractmethod
+    def get_condition_and_event_shapes(estimator: Any) -> Tuple[torch.Size, torch.Size]:
+        pass
+
+    @abstractmethod
+    def _estimator_log_prob(
+        self, x: torch.Tensor, condition: torch.Tensor
+    ) -> torch.Tensor:
+        """The log probability method of the estimator."""
+        pass
+
+    @property
+    def estimator(self):
+        """The conditioned estimator."""
+        return self._estimator
+
+    @property
+    def condition(self):
+        """The conditioning parameter."""
+        return self._condition
+
+    def _check_condition_shape(self, condition: torch.Tensor):
+        """Check that the shape of `condition` is compatible with the estimator."""
+        if len(condition.shape) < len(self._condition_shape):
+            raise ValueError(
+                "Dimensionality of condition is too small and does not match the "
+                f"expected dimensionality {len(self._condition_shape)}. "
+                "It should be compatible with condition_shape "
+                f"{self._condition_shape}."
+            )
+        else:
+            condition_shape = condition.shape[-len(self._condition_shape) :]
+            if condition_shape != self._condition_shape:
+                raise ValueError(
+                    f"Condition shape {condition_shape} is not compatible with "
+                    f"estimator condition shape {self._condition_shape}"
+                )
+
+    def sample(self, sample_shape: torch.Size = torch.Size()) -> torch.Tensor:
+        """Generate samples from the conditioned estimator."""
+        if not hasattr(self.estimator, "sample"):
+            raise NotImplementedError("Sampling is not implemented for this estimator.")
+        draws = self.estimator.sample(sample_shape, condition=self._condition_reshaped)
+        # reshape (batch_dim,) -> batch_shape
+        return draws.reshape(*sample_shape, *self.batch_shape, *self.event_shape)
+
+    def log_prob(self, x: torch.Tensor) -> torch.Tensor:
+        """Compute log probability of x given theta."""
+        # ConditionalEstimator expects a batch shape of (batch_dim,)
+        # and sample shape of (sample_dim,)
+        sample_shape = x.shape[: -len(self.event_shape) - len(self.batch_shape)]
+        x = x.reshape(-1, self.batch_shape.numel(), *self.event_shape)
+        print(
+            x.shape, self.batch_shape, self.event_shape, self._condition_reshaped.shape
+        )
+        lp = self._estimator_log_prob(x, self._condition_reshaped)
+        print(lp.shape)
+        # reshape (sample_dim, batch_dim,) -> (*sample_shape, *batch_shape)
+        return lp.reshape(*sample_shape, *self.batch_shape)
+
+    def expand(self, batch_shape: torch.Size, _instance=None):
+        """Expand the batch shape of the distribution."""
+        # because batch shape of the distribution is entirely determined by the
+        # condition, we only need to expand the condition
+        condition = self.condition.expand(*batch_shape, *self._condition_shape)
+        return type(self)(self.estimator, condition)
+
+
+class ConditionedEstimatorDistribution(EstimatorDistribution):
+    """
+    A conditioned `sbi` estimator wrapped as a Pyro distribution.
+    """
+
+    @staticmethod
+    def get_condition_and_event_shapes(
+        estimator: ConditionalEstimator,
+    ) -> Tuple[torch.Size, torch.Size]:
+        return estimator.condition_shape, estimator.input_shape
+
+    def __init__(self, estimator: ConditionalEstimator, condition: torch.Tensor):
+        """
+        Condition an `sbi` estimator and wrap it as a Pyro distribution.
+
+        Args:
+            estimator: A trained conditional estimator
+            condition: The conditioning parameter for the estimator.
+        """
+        super().__init__(estimator, condition)
+
+    @property
+    def condition_shape(self):
+        return self.estimator.condition_shape
+
+    def _estimator_log_prob(
+        self, x: torch.Tensor, condition: torch.Tensor
+    ) -> torch.Tensor:
+        return self.estimator.log_prob(x, condition=condition)
--- tests/pyroutils_test.py
+++ tests/pyroutils_test.py
@@ -2,9 +2,14 @@
 # under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
 import pyro
+import pytest
 import torch
 
-from sbi.utils.pyroutils import get_transforms
+from sbi.inference import NLE, NPE
+from sbi.utils.pyroutils import (
+    ConditionedEstimatorDistribution,
+    get_transforms,
+)
 
 
 def test_unbounded_transform():
@@ -25,3 +30,55 @@ def test_unbounded_transform():
 
     assert to_unbounded(prior(1000)).max() > 1.0
     assert to_bounded(to_unbounded(prior(1000))).max() < 1.0
+
+
+@pytest.mark.parametrize(
+    "trainer_cls, distribution_cls",
+    [
+        (NLE, ConditionedEstimatorDistribution),
+        (NPE, ConditionedEstimatorDistribution),
+    ],
+)
+def test_estimator_distribution_basic_properties(
+    trainer_cls,
+    distribution_cls,
+    num_simulations: int = 100,
+    dim: int = 5,
+):
+    prior = torch.distributions.MultivariateNormal(
+        loc=torch.zeros(dim), covariance_matrix=torch.diag(torch.ones(dim))
+    )
+    theta = prior.sample(torch.Size([num_simulations]))
+    x = torch.distributions.Normal(theta, 1.0).sample()
+    trainer = trainer_cls(prior=prior).append_simulations(theta=theta, x=x)
+    density_estimator = trainer.train()
+
+    # Test basic properties
+    estimator_dist = distribution_cls(estimator=density_estimator, condition=theta)
+    assert isinstance(estimator_dist, distribution_cls)
+    assert estimator_dist.estimator == density_estimator
+    assert estimator_dist.condition_shape == torch.Size([dim])
+    assert estimator_dist.batch_shape == torch.Size([num_simulations])
+    assert estimator_dist.event_shape == torch.Size([dim])
+    assert estimator_dist.support == torch.distributions.constraints.real
+
+    # Test sample method
+    assert estimator_dist.sample().shape == torch.Size([num_simulations, dim])
+    x_samples = estimator_dist.sample(torch.Size([3]))
+    assert x_samples.shape == torch.Size([3, num_simulations, dim])
+
+    # Test log_prob method
+    assert estimator_dist.log_prob(x).shape == torch.Size([num_simulations])
+    assert estimator_dist.log_prob(x_samples).shape == torch.Size([3, num_simulations])
+
+    # Test expand method
+    estimator_dist_expanded = estimator_dist.expand(torch.Size([2, num_simulations]))
+    assert isinstance(estimator_dist_expanded, distribution_cls)
+    assert estimator_dist_expanded.batch_shape == torch.Size([2, num_simulations])
+    assert estimator_dist_expanded.event_shape == estimator_dist.event_shape
+    assert estimator_dist_expanded.condition_shape == estimator_dist.condition_shape
+    assert torch.equal(
+        estimator_dist_expanded.condition,
+        theta.expand(torch.Size([2, num_simulations, dim])),
+    )
+    assert estimator_dist_expanded.estimator == estimator_dist.estimator
