dd48e41162f44d4a24656bfe332ee3cbb7a93a62
kartik-sama <samasaikartik@gmail.com>
2025-03-20 21:13:16 +0100

    init path for simulators modified to make use of true_posterior_linear_gaussian_mvn_prior in tests

--- sbi/simulators/__init__.py
+++ sbi/simulators/__init__.py
@@ -1,2 +1,6 @@
-from sbi.simulators.linear_gaussian import diagonal_linear_gaussian, linear_gaussian
+from sbi.simulators.linear_gaussian import (
+    diagonal_linear_gaussian,
+    linear_gaussian,
+    true_posterior_linear_gaussian_mvn_prior,
+)
 from sbi.simulators.simutils import simulate_in_batches
