8cc1ed0002a9d7fc6366e741701502ff504e5c72
Kristof Schroeder <kristof_schroeder@web.de>
2025-03-20 11:12:50 +0100

    refactor npse tests

--- tests/linearGaussian_npse_test.py
+++ tests/linearGaussian_npse_test.py
@@ -1,10 +1,7 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import Optional
 
 import pytest
-import pytest_cases
-from coverage.files import actual_path
-from pyro.contrib.autoname import scope
 from torch import eye, ones, zeros
 from torch.distributions import MultivariateNormal
 
@@ -19,55 +16,10 @@ from sbi.simulators.linear_gaussian import (
 from .test_utils import check_c2st, get_dkl_gaussian_prior
 
 
-def _initialize_experiment(
-    num_dim: int, num_samples: int, num_simulations: int, prior_type: str
-):
-    """Set up priors, generate target samples, and create simulation data."""
-    if prior_type in {"gaussian", None}:
-        prior_mean = zeros(num_dim)
-        prior_cov = eye(num_dim)
-        prior = MultivariateNormal(loc=prior_mean, covariance_matrix=prior_cov)
-    elif prior_type == "uniform":
-        prior = utils.BoxUniform(-2.0 * ones(num_dim), 2.0 * ones(num_dim))
-        prior_mean, prior_cov = None, None
-    else:
-        raise NotImplementedError(f"Unsupported prior type: {prior_type}")
-
-    x_o = zeros((1, num_dim))
-    likelihood_shift = -ones(num_dim)
-    likelihood_cov = 0.3 * eye(num_dim)
-
-    if prior_type in {"gaussian", None}:
-        gt_posterior = true_posterior_linear_gaussian_mvn_prior(
-            x_o, likelihood_shift, likelihood_cov, prior_mean, prior_cov
-        )
-        target_samples = gt_posterior.sample((num_samples,))
-    else:  # prior_type == "uniform"
-        target_samples = samples_true_posterior_linear_gaussian_uniform_prior(
-            x_o, likelihood_shift, likelihood_cov, prior=prior, num_samples=num_samples
-        )
-
-    theta = prior.sample((num_simulations,))
-    x = linear_gaussian(theta, likelihood_shift, likelihood_cov)
-    prior = None if prior_type is None else prior
-
-    return (
-        prior,
-        target_samples,
-        theta,
-        x,
-        x_o,
-        likelihood_shift,
-        likelihood_cov,
-        prior_mean,
-        prior_cov,
-    )
-
-
 @dataclass
 class NpseTestCase:
     num_dim: int
-    prior_type: str
+    prior_type: Optional[str]
     sde_type: str
     sample_with: Optional[str] = None
     iid_method: Optional[str] = None
@@ -101,11 +53,13 @@ class NpseTestCase:
     @property
     def prior(self):
         if self.prior_type in {"gaussian", None}:
-            prior = MultivariateNormal(loc=self.prior_mean,
-                                       covariance_matrix=self.prior_cov)
+            prior = MultivariateNormal(
+                loc=self.prior_mean, covariance_matrix=self.prior_cov
+            )
         elif self.prior_type == "uniform":
-            prior = utils.BoxUniform(-2.0 * ones(self.num_dim),
-                                     2.0 * ones(self.num_dim))
+            prior = utils.BoxUniform(
+                -2.0 * ones(self.num_dim), 2.0 * ones(self.num_dim)
+            )
         else:
             raise NotImplementedError(f"Unsupported prior type: {self.prior_type}")
         return prior
@@ -115,51 +69,59 @@ class NpseTestCase:
         x = linear_gaussian(theta, self.likelihood_shift, self.likelihood_cov)
         return theta, x
 
-    def get_target_samples(self, num_samples):
+    def get_target_samples(self, num_samples: int):
         if self.prior_type in {"gaussian", None}:
             gt_posterior = true_posterior_linear_gaussian_mvn_prior(
-                self.x_o, self.likelihood_shift, self.likelihood_cov, self.prior_mean,
-                self.prior_cov
+                self.x_o,
+                self.likelihood_shift,
+                self.likelihood_cov,
+                self.prior_mean,
+                self.prior_cov,
             )
             target_samples = gt_posterior.sample((num_samples,))
         else:  # prior_type == "uniform"
             target_samples = samples_true_posterior_linear_gaussian_uniform_prior(
-                self.x_o, self.likelihood_shift, self.likelihood_cov, prior=self.prior,
-                num_samples=num_samples
+                self.x_o,
+                self.likelihood_shift,
+                self.likelihood_cov,
+                prior=self.prior,
+                num_samples=num_samples,
             )
         return target_samples
 
-class NpseTestCaseCollection:
-    @pytest_cases.parametrize("sde_type, num_dim, prior_str",
-                              [
-                                  ("vp", 1, "gaussian"),
-                                  ("vp", 3, "uniform"),
-                                  ("vp", 3, "gaussian"),
-                                  ("ve", 3, "uniform"),
-                                  ("subvp", 3, "uniform"),
-                              ],)
-    def case_simple(self, sde_type, num_dim, prior_str) -> NpseTestCase:
-        return NpseTestCase(num_dim, prior_str, sde_type)
 
-    @pytest_cases.parametrize("sde_type, num_dim", [("vp", 1), ("vp", 3)])
-    def case_gaussian(self, sde_type, num_dim) -> NpseTestCase:
-        return NpseTestCase(num_dim, "gaussian", sde_type)
+test_cases_gaussian = [
+    pytest.param(NpseTestCase(1, "gaussian", "vp"), id="gaussian_prior-dim_1-vp"),
+    pytest.param(NpseTestCase(1, "gaussian", "vp"), id="gaussian_prior-dim_1-vp"),
+    pytest.param(NpseTestCase(2, "gaussian", "ve"), id="gaussian_prior-dim_2-ve"),
+    pytest.param(NpseTestCase(2, "gaussian", "vp"), id="gaussian_prior-dim_2-vp"),
+    pytest.param(NpseTestCase(2, "gaussian", "subvp"), id="gaussian_prior-dim_2-subvp"),
+]
+
+test_cases_uniform = [
+    pytest.param(NpseTestCase(3, "uniform", "ve"), id="uniform_prior-dim_3-ve"),
+    pytest.param(NpseTestCase(3, "uniform", "vp"), id="uniform_prior-dim_3-vp"),
+    pytest.param(NpseTestCase(3, "uniform", "subvp"), id="uniform_prior-dim_3-subvp"),
+]
+
+test_cases_none = [
+    pytest.param(NpseTestCase(2, None, "ve"), id="None_prior-dim_3-ve"),
+    pytest.param(NpseTestCase(2, None, "vp"), id="None_prior-dim_3-vp"),
+    pytest.param(NpseTestCase(2, None, "subvp"), id="None_prior-dim_3-subvp"),
+]
+
+test_cases_all = test_cases_gaussian + test_cases_uniform + test_cases_none
+iid_methods = ["fnpe", "gauss", "auto_gauss", "jac_gauss"]
+sample_with = ["sde", "ode"]
 
 
-@pytest_cases.fixture
-# @pytest_cases.parametrize("num_simulations", [5, 10])
-# @pytest_cases.parametrize("training_batch_size", [200, None])
-# @pytest_cases.parametrize("stop_after_epochs", [200, None])
-@pytest_cases.parametrize_with_cases("test_case", cases=NpseTestCaseCollection)
-def npse_trained_model(test_case: NpseTestCase):
-    inference = NPSE(test_case.prior, sde_type=test_case.sde_type, show_progress_bars=True)
-    num_simulations = 5
+def _train_npse(
+    test_case: NpseTestCase, num_simulations, stop_after_epochs, training_batch_size
+):
+    inference = NPSE(
+        test_case.prior, show_progress_bars=True, sde_type=test_case.sde_type
+    )
     theta, x = test_case.get_training_data(num_simulations)
-    inference.append_simulations(theta, x)
-    training_batch_size = x.shape[0]
-    stop_after_epochs = 1
-
-    """
     if stop_after_epochs is None and training_batch_size is None:
         score_estimator = inference.train()
     elif stop_after_epochs is not None and training_batch_size is None:
@@ -167,132 +129,100 @@ def npse_trained_model(test_case: NpseTestCase):
     elif stop_after_epochs is None and training_batch_size is not None:
         score_estimator = inference.train(training_batch_size=training_batch_size)
     else:
-    """
-    score_estimator = inference.train(training_batch_size=training_batch_size,
-                                          stop_after_epochs=stop_after_epochs)
+        score_estimator = inference.append_simulations(theta, x).train(
+            stop_after_epochs=stop_after_epochs, training_batch_size=training_batch_size
+        )
     return inference, score_estimator
 
 
-# @pytest_cases.fixture
-# @pytest_cases.parametrize("sample_with", ["sde", "ode"])
-# def npse_posterior(npse_trained_model, sample_with):
-#     inference, score_estimator = npse_trained_model
-#     return inference.build_posterior(score_estimator, sample_with=sample_with)
-#
-#
-# @pytest_cases.fixture
-# @pytest_cases.parametrize("iid_method", ["fnpe", "gauss", "auto_gauss", "jac_gauss"])
-# def npse_samples(npse_posterior, iid_method):
-#     num_samples = 10
-#     return npse_posterior.sample((num_samples,), iid_method=iid_method)
+@pytest.fixture
+def npse_trained_model(request):
+    test_case: NpseTestCase = request.param
+    inference = NPSE(
+        test_case.prior, sde_type=test_case.sde_type, show_progress_bars=True
+    )
+    num_simulations = 5
+    stop_after_epochs = 1
+    theta, x = test_case.get_training_data(num_simulations)
+    inference.append_simulations(theta, x)
+    training_batch_size = x.shape[0]
+    inference, score_estimator = _train_npse(
+        test_case, num_simulations, stop_after_epochs, training_batch_size
+    )
+    return inference, score_estimator, test_case
 
 
 @pytest.mark.slow
-@pytest_cases.parametrize_with_cases("test_case",
-                                     cases=NpseTestCaseCollection.case_gaussian,
-                                     )
-def test_c2st(test_case: NpseTestCase, npse_samples, current_cases):
+@pytest.mark.parametrize("sample_with", sample_with)
+@pytest.mark.parametrize("iid_method", iid_methods)
+@pytest.mark.parametrize("npse_trained_model", test_cases_all, indirect=True)
+def test_c2st(npse_trained_model, iid_method, sample_with):
+    inference, score_estimator, test_case = npse_trained_model
     num_dim = test_case.num_dim
     sde_type = test_case.sde_type
     prior_str = test_case.prior_type
+    num_samples = 10
+
+    posterior = inference.build_posterior(score_estimator, sample_with=sample_with)
+    posterior.set_default_x(test_case.x_o)
+    npse_samples = posterior.sample((num_samples,), iid_method=iid_method)
     check_c2st(
         npse_samples,
         test_case.get_target_samples(npse_samples.shape[0]),
         alg=f"npse-{sde_type or 'vp'}-{prior_str}-{num_dim}D-{test_case.sample_with}",
     )
 
+    if prior_str == "gaussian":
+        gt_posterior = true_posterior_linear_gaussian_mvn_prior(
+            test_case.x_o,
+            test_case.likelihood_shift,
+            test_case.likelihood_cov,
+            test_case.prior_mean,
+            test_case.prior_cov,
+        )
 
-# @pytest.mark.slow
-# @pytest_cases.parametrize_with_cases("test_case",
-#                                      cases=NpseTestCaseCollection.case_gaussian,)
-# def test_kld_gaussian(test_case: NpseTestCase, npse_posterior):
-#     # For the Gaussian prior, we compute the KLd between ground truth and
-#     # posterior.
-#     dkl = get_dkl_gaussian_prior(
-#         npse_posterior,
-#         test_case.x_o[0],
-#         test_case.likelihood_shift,
-#         test_case.likelihood_cov,
-#         test_case.prior_mean,
-#         test_case.prior_cov,
-#     )
-#
-#     max_dkl = 0.15
-#
-#     assert dkl < max_dkl, (
-#         f"D-KL={dkl} is more than 2 stds above the average performance."
-#     )
+        map_ = posterior.map(show_progress_bars=True, num_iter=5)
+        assert ((map_ - gt_posterior.mean) ** 2).sum() < 0.5, "MAP is not close to GT."
 
-
-def _train_and_sample(num_dim, num_samples, num_simulations, prior_type, sde_type,
-                      stop_after_epochs, training_batch_size):
-    (
-        prior,
-        target_samples,
-        theta,
-        x,
-        x_o,
-        likelihood_shift,
-        likelihood_cov,
-        prior_mean,
-        prior_cov,
-    ) = _initialize_experiment(num_dim, num_samples, num_simulations, prior_type)
-
-    inference = NPSE(prior, show_progress_bars=True, sde_type=sde_type)
-
-    score_estimator = inference.append_simulations(theta, x).train(
-        stop_after_epochs=stop_after_epochs, training_batch_size=training_batch_size
-    )
-    posterior = inference.build_posterior(score_estimator)
-    posterior.set_default_x(x_o)
-    samples = posterior.sample((num_samples,), iid_method=iid_method)
-
-    return inference, score_estimator, posterior, samples
-
-
-@pytest.mark.parametrize(
-    "sde_type, prior_type",
-    [
-        pytest.param("ve", None, id="ve-None"),
-        pytest.param("vp", "gaussian", id="vp-gaussian"),
-    ],
-)
-@pytest.mark.parametrize(
-    "iid_method, num_trial",
-    [
-        pytest.param("fnpe", 3, id="fnpe-2trials", marks=pytest.mark.slow),
-        pytest.param("gauss", 3, id="gauss-6trials", marks=pytest.mark.slow),
-        pytest.param("auto_gauss", 8, id="auto_gauss-8trials"),
-        pytest.param(
-             "auto_gauss", 16, id="auto_gauss-16trials", marks=pytest.mark.slow
-        ),
-        pytest.param("jac_gauss", 8, id="jac_gauss-8trials", marks=pytest.mark.slow),
-    ],
-)
-def test_npse_iid_inference_snapshot(sde_type, prior_type, iid_method, num_trial,
-                                     snapshot):
-    num_simulations = 10
-    _, _, _, samples = _train_and_sample(1, 10, num_simulations,
-                                         prior_type, sde_type, 1,
-                                         training_batch_size=num_simulations)
-    assert samples == snapshot
 
 @pytest.mark.slow
-@pytest.mark.parametrize("npse_trained_model", [("vp", "gaussian")], indirect=True)
-def test_npse_map(npse_trained_model):
-    x_o = npse_trained_model["x_o"]
-    inference = npse_trained_model["inference"]
-    prior_mean = npse_trained_model["prior_mean"]
-    prior_cov = npse_trained_model["prior_cov"]
-    likelihood_shift = npse_trained_model["likelihood_shift"]
-    likelihood_cov = npse_trained_model["likelihood_cov"]
-
-    gt_posterior = true_posterior_linear_gaussian_mvn_prior(
-        x_o, likelihood_shift, likelihood_cov, prior_mean, prior_cov
+@pytest.mark.parametrize("npse_trained_model", test_cases_gaussian, indirect=True)
+def test_kld_gaussian(npse_trained_model):
+    # For the Gaussian prior, we compute the KLd between ground truth and
+    # posterior.
+    num_samples = 10
+    inference, score_estimator, test_case = npse_trained_model
+    posterior = inference.build_posterior(score_estimator)
+    posterior.set_default_x(test_case.x_o)
+    dkl = get_dkl_gaussian_prior(
+        posterior,
+        test_case.x_o[0],
+        test_case.likelihood_shift,
+        test_case.likelihood_cov,
+        test_case.prior_mean,
+        test_case.prior_cov,
+        num_samples=num_samples,
+    )
+    max_dkl = 0.15
+    assert dkl < max_dkl, (
+        f"D-KL={dkl} is more than 2 stds above the average performance."
     )
 
-    posterior = inference.build_posterior().set_default_x(x_o)
 
-    map_ = posterior.map(show_progress_bars=True, num_iter=5)
-
-    assert ((map_ - gt_posterior.mean) ** 2).sum() < 0.5, "MAP is not close to GT."
+@pytest.mark.parametrize("test_case", test_cases_all)
+@pytest.mark.parametrize("sample_with", sample_with)
+@pytest.mark.parametrize("iid_method", iid_methods)
+def test_npse_iid_inference_snapshot(
+    test_case: NpseTestCase, sample_with, iid_method, snapshot
+):
+    num_simulations = 5
+    num_samples = 2
+    stop_after_epochs = 1
+    training_batch_size = num_simulations
+    inference, score_estimator = _train_npse(
+        test_case, num_simulations, stop_after_epochs, training_batch_size
+    )
+    posterior = inference.build_posterior(score_estimator, sample_with=sample_with)
+    posterior.set_default_x(test_case.x_o)
+    samples = posterior.sample((num_samples,), iid_method=iid_method)
+    assert samples == snapshot
