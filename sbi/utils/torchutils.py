030049cd0630bfafab3c43bf1822a8180ad312c5
Jose Robledo <j.robledo@fz-juelich.de>
2025-03-20 17:03:04 +0100

    add torch.device to process_device

--- sbi/utils/torchutils.py
+++ sbi/utils/torchutils.py
@@ -15,7 +15,7 @@ from sbi.sbi_types import Array, OneOrMore
 from sbi.utils.typechecks import is_nonnegative_int, is_positive_int
 
 
-def process_device(device: str) -> str:
+def process_device(device: Union[str, torch.device]) -> str:
     """Set and return the default device to cpu or gpu (cuda, mps).
 
     Args:
