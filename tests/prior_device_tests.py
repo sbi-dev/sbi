d3f11032fb0f33e3c17e4faca4d459a88151c63c
Jose Robledo <j.robledo@fz-juelich.de>
2025-03-20 15:14:20 +0100

    add process device to tests

--- tests/prior_device_tests.py
+++ tests/prior_device_tests.py
@@ -1,13 +1,14 @@
 import pytest
 import torch
 
-from sbi.utils.torchutils import BoxUniform
+from sbi.utils.torchutils import BoxUniform, process_device
 from sbi.utils.user_input_checks_utils import PytorchReturnTypeWrapper
 
 
 @pytest.mark.gpu
-@pytest.mark.parametrize("device", ["cpu", "mps"])
+@pytest.mark.parametrize("device", ["cpu", "mps", "gpu"])
 def test_BoxUniform(device: str):
+    device = process_device(device)
     low = torch.tensor([0.0])
     high = torch.tensor([1.0])
     prior = BoxUniform(low, high)
@@ -16,14 +17,15 @@ def test_BoxUniform(device: str):
 
     prior.to(device)
     assert prior.device == device
-    assert prior.low.device.type == device
-    assert prior.high.device.type == device
+    assert prior.low.device.type == device.strip(":0")
+    assert prior.high.device.type == device.strip(":0")
     prior.sample((1,))
 
 
 @pytest.mark.gpu
-@pytest.mark.parametrize("device", ["cpu", "mps"])
+@pytest.mark.parametrize("device", ["cpu", "mps", "gpu"])
 def test_ReturnTypeWrapper(device: str):
+    device = process_device(device)
     prior = torch.distributions.Normal(loc=0.0, scale=1.0)
     prior = PytorchReturnTypeWrapper(prior)
 
