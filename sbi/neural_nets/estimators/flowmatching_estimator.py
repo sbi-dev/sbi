122cf77f24a95d9e7f057cc20e0bc66bfa675474
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2025-03-20 11:01:24 +0100

    add docstrings for FlowMatchingEstimator

--- sbi/neural_nets/estimators/flowmatching_estimator.py
+++ sbi/neural_nets/estimators/flowmatching_estimator.py
@@ -20,10 +20,28 @@ class VectorFieldNet(nn.Module, ABC):
 
 class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
     r"""
-
     Rectified flow matching estimator class that estimates the conditional vector field,
-    i.e. the expectation E[x_t - x_0 | x_0 = x].
+    :math:`v(\theta_t, t; x_o) = \mathbb{E}[\theta_1 - \theta_0 | \theta_t, x_o = x_o]`
 
+    This estimator implements the flow matching approach where the vector field is learned
+    by matching the flow between the base and target distributions. The vector field
+    represents the instantaneous change in the distribution at time t.
+    
+    References
+    ----------
+    .. [1] Liu, X., Gong, C., & Liu, Q. (2023).
+           "Flow Straight and Fast: Learning to Generate and Transfer Data with Rectified Flow"
+           *International Conference on Learning Representations (ICLR)*
+           https://arxiv.org/abs/2209.03003
+
+    .. [2] Lipman, Y., Chen, R. T. Q., Ben-Hamu, H., Nickel, M., & Le, M. (2023).
+           "Flow Matching for Generative Modeling"
+           *International Conference on Learning Representations (ICLR)*
+           https://arxiv.org/abs/2210.02747
+
+    .. [3] Singh, S., & Fischer, I. (2024).
+           "Stochastic Sampling from Deterministic Flow Models"
+           https://arxiv.org/abs/2410.02217
     """
 
     # Whether the score is defined for this estimator.
@@ -44,19 +62,21 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
         embedding_net: Optional[nn.Module] = None,
         num_freqs: int = 3,
         noise_scale: float = 1e-3,
-        zscore_transform_input=None,  # This is ignored and will be removed.
+        zscore_transform_input=None,  # This is ignored!
         **kwargs,
     ) -> None:
-        """Creates a vector field estimator for Flow Matching.
+        r"""Creates a vector field estimator for Flow Matching.
 
         Args:
             net: Neural network that estimates the vector field.
-            input_shape: Shape of the input, e.g., the parameters.
-            condition_shape: Shape of the condition, e.g., the data.
-            noise_scale: Scale of the noise added to the vector field.
+            input_shape: Shape of the input :math:`\theta`.
+            condition_shape: Shape of the condition :math:`x_o`.
             embedding_net: Embedding network for the condition.
             num_freqs: Number of frequencies to use for the positional time encoding.
-            noise_scale: Scale of the noise added to the vector field.
+            noise_scale: Scale of the noise added to the vector field
+                (:math:`\sigma_{min}` in [2]_).
+            zscore_transform_input: Whether to z-score the input.
+                This is deprecated. It is ignored and will be removed.
         """
 
         super().__init__(
@@ -99,13 +119,38 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
     def loss(
         self, input: Tensor, condition: Tensor, times: Optional[Tensor] = None, **kwargs
     ) -> Tensor:
-        """Return the loss for training the density estimator. More precisely,
+        r"""Return the loss for training the density estimator. More precisely,
         we compute the conditional flow matching loss with naive optimal
-        trajectories as described in the original paper.
+        trajectories as described in the original paper [1]_:
+
+        .. math::
+            \mathbb{E}_{\theta_0 \sim p_{data}, t \sim \text{Uniform}[0, 1]},
+            \theta_t = t \cdot \theta_1 + (1 - t) \cdot \theta_0,
+            \left[ \| v(\theta_t, t; x_o = x_o) - (\theta_1 - \theta_0) \|^2 \right]
+
+        where :math:`v(\theta_t, t; x_o)` is the vector field estimated by the neural network
+        (see Equation 1 in [1]_ with added conditioning on :math:`x_o`. The notation
+        is changed to match the standard SBI notation: :math:`\theta_0 = x_0` and
+        :math:`\theta_1 = x_1`).
+
+        The loss is computed as the mean squared error between the vector field:
+
+        .. math::
+            L(\theta_0, \theta_1, t, x_o) = \| v(\theta_t, t; x_o) - (\theta_1 - \theta_0) \|^2
+        
+            where
+
+            .. math::
+                \theta_1 \sim p_{base}
+                \theta_t = t \cdot \theta_1 + (1 - t) \cdot \theta_0
+
+            Additionally, the small noise :math:`\sigma_{min}` is added to the vector field
+            as per [2]_ to address numerical issues at small times.
+        
 
         Args:
-            theta: Parameters.
-            x: Observed data.
+            theta: Parameters (:math:`\theta_0`).
+            x: Observed data (:math:`x_o`).
             times: Time steps to compute the loss at.
                 Optional, will sample from [0, 1] if not provided.
 
@@ -120,33 +165,47 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
 
         # sample from probability path at time t
         # TODO: Change to notation from Lipman et al. or Tong et al.
-        epsilon = torch.randn_like(input)
-        theta_prime = (1 - t_) * input + (t_ + self.noise_scale) * epsilon
+        theta_1 = torch.randn_like(input)
+        theta_t = (1 - t_) * input + (t_ + self.noise_scale) * theta_1
 
         # compute vector field at the sampled time steps
-        vector_field = epsilon - input
+        vector_field = theta_1 - input
 
         # compute the mean squared error between the vector fields
         return torch.mean(
-            (self.forward(theta_prime, condition, times) - vector_field) ** 2, dim=-1
+            (self.forward(theta_t, condition, times) - vector_field) ** 2, dim=-1
         )
 
     def ode_fn(self, input: Tensor, condition: Tensor, t: Tensor) -> Tensor:
-        """
-        ODE flow function of the vector field estimator.
+        r"""
+        ODE flow function :math:`v(\theta_t, t, x_o)` of the vector field estimator.
+
+        The target distribution can be sampled from by solving the following ODE:
+
+        .. math::
+            d\theta_t = v(\theta_t, t; x_o) dt
+
+        where :math:`v(\theta_t, t; x_o)` is the vector field estimated by the neural network
+        (see Equation 1 in [1]_ with added conditioning on :math:`x_o`).
 
         Args:
-            input: variable whose distribution is estimated.
-            condition: Conditioning variable.
-            t: Time.
+            input: :math:`\theta_t`.
+            condition: Conditioning variable :math:`x_o`.
+            t: Time :math:`t`.
 
         Returns:
-            Estimated vector field.
+            Estimated vector field :math:`v(\theta_t, t; x_o)`. 
+            The shape is the same as the input.
         """
         return self.forward(input, condition, t)
 
     def score(self, input: Tensor, condition: Tensor, t: Tensor) -> Tensor:
-        """Score function of the vector field estimator.
+        r"""Score function of the vector field estimator.
+
+        The score function is calculated based on [3]_ (see Equation 13):
+
+        .. math::
+            \nabla_{\theta_t} \log p(\theta_t | x_o) = (- (1 - t) v(\theta_t, t; x_o) - \theta_0 ) / t
 
         Args:
             input: variable whose distribution is estimated.
@@ -156,41 +215,66 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
         Returns:
             Score function of the vector field estimator.
         """
+        # TODO: derive taking into account the noise sigma_min added to the vector field.
         v = self.forward(input, condition, t)
         return (-(1 - t) * v - input) / torch.maximum(t, torch.tensor(1e-6))
 
     def drift_fn(self, input: Tensor, times: Tensor) -> Tensor:
-        """Drift function for the flow matching estimator.
+        r"""Drift function for the flow matching estimator.
+
+        The drift function is calculated based on [3]_ (see Equation 7):
+
+        .. math::
+            f(t) = - \theta_t / (1 - t)
+
+        The drift function :math:`f(t)` and diffusion function :math:`\g(t)` enable SDE sampling:
+
+        .. math::
+            d\theta_t = [f(t) - g(t)^2 \nabla_{\theta_t} \log p(\theta_t | x_o)]dt + \g(t) dW_t
+
+        where :math:`dW_t` is the Wiener process.
 
         Args:
-            input: Original data, x0.
+            input: Parameters :math:`\theta_t`.
             times: SDE time variable in [0,1].
 
         Returns:
             Drift function at a given time.
         """
+        # TODO: derive taking into account the noise sigma_min added to the vector field.
         return -input / torch.maximum(1 - times, torch.tensor(1e-6))
 
     def diffusion_fn(self, input: Tensor, times: Tensor) -> Tensor:
-        """Diffusion function for the flow matching estimator.
+        r"""Diffusion function for the flow matching estimator.
+
+        The diffusion function is calculated based on [3]_ (see Equation 7):
+
+        .. math::
+            \g(t) = \sqrt{2t / (1 - t)}
 
         Args:
-            input: Original data, x0.
+            input: Parameters :math:`\theta_t`.
             times: SDE time variable in [0,1].
 
         Returns:
             Diffusion function at a given time.
         """
+        # TODO: derive taking into account the noise sigma_min added to the vector field.
         return torch.sqrt(2 * times / torch.maximum(1 - times, torch.tensor(1e-6)))
 
     def mean_t_fn(self, times: Tensor) -> Tensor:
-        r"""Linear coefficient of the perturbation kernel mean
+        r"""Linear coefficient of the perturbation kernel mean :math:`\mu_t(t)`
         for the flow matching estimator.
 
-        The general form of the perturbation kernel for rectified flows is:
-            mean_t(t) = (1 - t) * mean_0 + t * mean_base
+        The perturbation kernel for rectified flows with Gaussian base distribution is:
 
-        So far, the implementation of iid methods assumes that the mean_base is 0.
+        .. math::
+            N(\theta_t; \mu_t(t), \sigma_t(t)^2) ,
+            \mu_t(t) = (1 - t) \cdot \theta_0 + t \cdot \mu_{base}
+            \sigma_t(t) = t \cdot \sigma_{base}
+
+        So far, the implementation of iid methods assumes that the mean_base :math:`\mu_{base}` is 0.
+        Therefore, the linear coefficient of the perturbation kernel mean is simply :math:`1 - t`.
 
         Args:
             times: SDE time variable in [0,1].
@@ -198,17 +282,22 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
         Returns:
             Mean function at a given time.
         """
+        # TODO: derive taking into account the noise sigma_min added to the vector field.
         mean_t = 1 - times
         for _ in range(len(self.input_shape)):
             mean_t = mean_t.unsqueeze(-1)
         return mean_t
 
     def std_fn(self, times: Tensor) -> Tensor:
-        """Standard deviation of the perturbation kernel
+        r"""Standard deviation of the perturbation kernel :math:`\sigma_t(t)`
         for the flow matching estimator.
 
-        The general form of the perturbation kernel for rectified flows is:
-            std_t(t) = t * std_base
+        The perturbation kernel for rectified flows with Gaussian base distribution is:
+
+        .. math::
+            N(\theta_t; \mu_t(t), \sigma_t(t)^2) ,
+            \mu_t(t) = (1 - t) \cdot \theta_0 + t \cdot \mu_{base}
+            \sigma_t(t) = t \cdot \sigma_{base}
 
         Args:
             times: SDE time variable in [0,1].
@@ -216,6 +305,7 @@ class FlowMatchingEstimator(ConditionalVectorFieldEstimator):
         Returns:
             Standard deviation at a given time.
         """
+        # TODO: derive taking into account the noise sigma_min added to the vector field.
         std_t = times * self.std_base
         for _ in range(len(self.input_shape)):
             std_t = std_t.unsqueeze(-1)
