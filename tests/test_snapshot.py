f0e23fd89582b85c037c13ddee37880a68c66ee5
Kristof Schroeder <kristof_schroeder@web.de>
2025-03-20 19:57:59 +0100

    Set tolerance for snapshot test in test_snapshot.py

--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -102,4 +102,6 @@ def test_npse_snapshot(
     posterior = inference.build_posterior(score_estimator, sample_with=sample_with)
     posterior.set_default_x(x_o)
     samples = posterior.sample((num_samples,), iid_method=iid_method)
-    ndarrays_regression.check({'values': samples.numpy()})
+    ndarrays_regression.check(
+        {'values': samples.numpy()}, default_tolerance=dict(atol=1e-5, rtol=1e-4)
+    )
