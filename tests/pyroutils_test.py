ba253dfdc627a3d110a6d3c36f20b306cc0c7073
Seth Axen <seth@sethaxen.com>
2025-03-20 14:15:27 +0100

    Add test for hierarchical model

--- tests/pyroutils_test.py
+++ tests/pyroutils_test.py
@@ -17,6 +17,14 @@ from sbi.utils.pyroutils import (
 from tests.test_utils import check_c2st
 
 
+def flatten_mcmc_samples(mcmc_samples: dict[str, torch.Tensor]) -> torch.Tensor:
+    """Given a dict of tensors of MCMC draws, concatenate them along dimension 1."""
+    vars = sorted(mcmc_samples.keys())
+    return torch.cat(
+        [mcmc_samples[k].reshape(mcmc_samples[k].shape[0], -1) for k in vars], dim=1
+    )
+
+
 def single_level_prior_theta(num_dim=10):
     mu_0 = torch.zeros(num_dim)
     sigma_0 = torch.ones(num_dim) * 1.0
@@ -55,6 +63,48 @@ def pyro_gaussian_model(x_o=None, num_dim=None, num_trials=1, sigma_x=0.5):
         return x_o
 
 
+def hierarchical_gaussian_model(
+    x_o=None,
+    num_subjects=None,
+    num_trials=None,
+    sigma_x=0.5,
+    mu_0=0.0,
+    sigma_0=1.0,
+    alpha_0=1.0,
+    beta_0=1.0,
+):
+    """
+    Hierarchical Pyro model for Bayesian inference with Gaussian likelihoods.
+
+    Args:
+        x_o (torch.Tensor, optional): Observed data (N, D), if available.
+    """
+    if num_subjects is None or num_trials is None:
+        if x_o is None:
+            raise ValueError(
+                "num_subjects and num_trials must be provided if x_o is not provided"
+            )
+        else:
+            num_trials = num_trials or x_o.shape[0]
+            num_subjects = num_subjects or x_o.shape[1]
+
+    mu = pyro.sample("mu", dist.Normal(mu_0, sigma_0))  # shape: ()
+    tau = pyro.sample("tau", dist.InverseGamma(alpha_0, beta_0))  # shape: ()
+
+    # Sample subject-level parameters (batch shape: (num_subjects,), event shape: ())
+    with pyro.plate("subjects", num_subjects):
+        theta = pyro.sample("theta", dist.Normal(mu, tau.sqrt()))
+
+        # Sample observations (batch shape: (num_trials, num_subjects), event shape: ())
+        with pyro.plate("trials", num_trials):
+            x = pyro.sample("x", dist.Normal(theta, sigma_x), obs=x_o)
+
+    if x_o is None:
+        return theta, mu, tau, x
+    else:
+        return x_o
+
+
 @pytest.fixture
 def data_and_pyro_gaussian_mcmc_samples(num_dim, num_trials, num_samples, warmup_steps):
     """Generate observed data and return MCMC samples from the true model."""
@@ -71,8 +121,28 @@ def data_and_pyro_gaussian_mcmc_samples(num_dim, num_trials, num_samples, warmup
 
 
 @pytest.fixture
-def num_simulations(trainer_cls, num_dim, num_trials):
-    num_simulations = num_dim * num_trials * 200
+def data_and_pyro_hierarchical_gaussian_mcmc_samples(
+    num_subjects, num_trials, num_samples, warmup_steps
+):
+    """Generate observed data and return MCMC samples from the true model."""
+    # Get data we will condition on
+    _, _, _, x_o = hierarchical_gaussian_model(
+        num_subjects=num_subjects,
+        num_trials=num_trials,
+    )
+
+    # Get MCMC samples from a model using the true likelihood
+    nuts_kernel = NUTS(hierarchical_gaussian_model)
+    mcmc = MCMC(nuts_kernel, num_samples=num_samples, warmup_steps=warmup_steps)
+    mcmc.run(x_o=x_o)
+    pyro_samples = mcmc.get_samples()
+
+    return x_o, pyro_samples
+
+
+@pytest.fixture
+def num_simulations(trainer_cls, num_trials, num_dim=1, num_subjects=1):
+    num_simulations = num_dim * num_subjects * num_trials * 200
     if trainer_cls is NRE:
         num_simulations *= 5
     return num_simulations
@@ -85,6 +155,16 @@ def gaussian_simulation_data(num_simulations, num_dim):
     )(num_dim=num_dim, num_trials=1)
     theta = samples['theta'].squeeze(1)
     x = samples['x'].squeeze(1)
+    return theta, x
+
+
+@pytest.fixture
+def hierarchical_gaussian_simulation_data(num_simulations):
+    samples = Predictive(
+        hierarchical_gaussian_model, num_samples=num_simulations, parallel=True
+    )(num_subjects=1, num_trials=1)
+    theta = samples['theta'].squeeze([1])
+    x = samples['x'].squeeze([1])
     return theta, x
 
 
@@ -236,4 +316,74 @@ def test_pyro_gaussian_model(
         pyro_samples,
         tol=0.1,
         alg=f"pyro MCMC vs SBI-pyro-{trainer_cls.__name__} MCMC",
+    )
+
+
+@pytest.mark.parametrize("num_samples", [500])
+@pytest.mark.parametrize("warmup_steps", [500])
+@pytest.mark.parametrize("num_trials", [1, 3])
+@pytest.mark.parametrize("num_subjects", [1, 2])
+@pytest.mark.parametrize(
+    "trainer_cls, distribution_cls",
+    [
+        (NLE, ConditionalDensityEstimatorDistribution),
+        (NRE, RatioEstimatorDistribution),
+    ],
+)
+def test_pyro_hierarchical_gaussian_model(
+    data_and_pyro_hierarchical_gaussian_mcmc_samples,
+    trainer_cls,
+    distribution_cls,
+    hierarchical_gaussian_simulation_data,
+    num_samples,
+    warmup_steps,
+):
+    """Test consistency of MCMC samples between the true and estimated likelihood."""
+    x_o, pyro_samples = data_and_pyro_hierarchical_gaussian_mcmc_samples
+
+    theta, x = hierarchical_gaussian_simulation_data
+    prior = single_level_prior_theta(num_dim=1)
+    trainer = trainer_cls(prior=prior).append_simulations(theta=theta, x=x)
+    density_estimator = trainer.train()
+
+    # Define a model that uses the estimated likelihood
+    def sbi_pyro_hierarchical_model(
+        likelihood,
+        x_o,
+        mu_0=0.0,
+        sigma_0=1.0,
+        alpha_0=1.0,
+        beta_0=1.0,
+    ):
+        num_trials, num_subjects = x_o.shape
+        mu = pyro.sample("mu", dist.Normal(mu_0, sigma_0))  # shape: ()
+        tau = pyro.sample("tau", dist.InverseGamma(alpha_0, beta_0))  # shape: ()
+
+        # Sample subject-level parameters (batch shape: (num_subjects,))
+        with pyro.plate("subjects", num_subjects):
+            theta = pyro.sample("theta", dist.Normal(mu, tau.sqrt()))
+
+            # Sample observations (batch shape: (num_trials, num_subjects))
+            with pyro.plate("trials", num_trials):
+                # add a singleton dimension as expected by likelihood
+                x = pyro.sample(
+                    "x",
+                    distribution_cls(likelihood, theta.unsqueeze(-1)),
+                    obs=x_o.unsqueeze(-1),
+                )
+
+        return x
+
+    # Get MCMC samples from a model that uses the estimated likelihood
+    nuts_kernel = NUTS(sbi_pyro_hierarchical_model)
+    mcmc = MCMC(nuts_kernel, num_samples=num_samples, warmup_steps=warmup_steps)
+    mcmc.run(x_o=x_o, likelihood=density_estimator)
+    sbi_samples = mcmc.get_samples()
+
+    # Compare posterior samples
+    check_c2st(
+        flatten_mcmc_samples(sbi_samples),
+        flatten_mcmc_samples(pyro_samples),
+        tol=0.1,
+        alg=f"pyro MCMC vs SBI-pyro-hierarchical-{trainer_cls.__name__} MCMC",
     )
