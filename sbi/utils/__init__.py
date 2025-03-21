cc6b2cdeca847ddb6f3dd4feb4ce154c1dabac01
Seth Axen <seth@sethaxen.com>
2025-03-21 12:14:54 +0100

    docs: add utils to API reference (#1520)
    * Add utils to docs
    
    * Import to_pyro_distribution into utils
    
    * Fix to_pyro_distribution docstring

--- sbi/utils/__init__.py
+++ sbi/utils/__init__.py
@@ -3,6 +3,7 @@ from sbi.utils.analysis_utils import get_1d_marginal_peaks_from_kde
 from sbi.utils.io import get_data_root, get_log_root, get_project_root
 from sbi.utils.kde import KDEWrapper, get_kde
 from sbi.utils.potentialutils import pyro_potential_wrapper, transformed_potential
+from sbi.utils.pyroutils import to_pyro_distribution
 from sbi.utils.restriction_estimator import (
     RestrictedPrior,
     RestrictionEstimator,
