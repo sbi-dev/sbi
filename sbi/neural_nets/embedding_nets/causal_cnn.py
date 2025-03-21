269b092f132b69414840c31d1ad0da63d95709f8
Aranka Steyaert <aranka.steyaert@ugent.be>
2025-03-21 10:40:53 +0100

    changes requested in PR review

--- sbi/neural_nets/embedding_nets/causal_cnn.py
+++ sbi/neural_nets/embedding_nets/causal_cnn.py
@@ -1,27 +1,39 @@
 # This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
 # under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 
 from torch import Tensor, nn
 
 from sbi.neural_nets.embedding_nets.cnn import calculate_filter_output_size
 
 
-def CausalCNN_1d_block(in_channels, out_channels, kernel_size, dilation, activation):
-    """Returns a block that computes a causal convolution by left padding the input
+def causalConv1d(
+    in_channels: int,
+    out_channels: int,
+    kernel_size: int,
+    dilation: int = 1,
+    stride: int = 1,
+    activation: nn.Module = nn.LeakyReLU(inplace=True),
+) -> nn.Module:
+    """Returns a causal convolution by left padding the input
 
     Args:
         in_channels: number of input channels
         out_channels: number of output channels wanted
         kernel_size: wanted kernel size
-        dilation: dilation to use in the convolution,
-        activation: activation function to append to the convolution
+        dilation: dilation to use in the convolution.
+        stride: stride to use in the convolution.
+            Stride and dilation cannot both be > 1.
+        activation: activation function to apply after the convolution
 
     Returns:
-        an nn.Sequential object that combines left-padding, 1D convolution
-        and the provided activation
+        An nn.Sequential object that combines left-padding, 1D convolution
+        and the provided activation.
     """
+    assert not (dilation > 1 and stride > 1), (
+        "we don't allow combining stride with dilation."
+    )
     padding_size = dilation * (kernel_size - 1)
     padding = nn.ZeroPad1d(padding=(padding_size, 0))
     conv_layer = nn.Conv1d(
@@ -29,10 +41,99 @@ def CausalCNN_1d_block(in_channels, out_channels, kernel_size, dilation, activat
         out_channels=out_channels,
         kernel_size=kernel_size,
         dilation=dilation,
-        stride=1,
+        stride=stride,
         padding=0,
     )
     return nn.Sequential(padding, conv_layer, activation)
+
+
+def WaveNetSRLikeAggregator(
+    in_channels: int,
+    num_timepoints: int,
+    output_dim: int,
+    activation: nn.Module = nn.LeakyReLU(inplace=True),
+    kernel_sizes: Optional[List] = None,
+    intermediate_channel_sizes: Optional[List] = None,
+    stride_sizes: Union[int, List] = 1,
+) -> nn.Module:
+    """
+    Creates a non-causal 1D CNN aggregator based on the WaveNet speach recognition task
+
+    By default this function creates an aggregator with two CNN layers,
+    after every convolution a maxpooling operation halves the number of timepoints.
+    The final CNN will have as many channels as the desired output dimension.
+    A global average pooling operation is applied in the end. The dimension of the
+    output will thus be (batch_size, output_dim, 1) regardless of the input size.
+
+    Args:
+        in_channels: number of channels at input.
+        num_timepoints: length of the input.
+        output dim: wanted number of features as output.
+        activation: activation to apply after the convolution.
+        kernel_sizes: (optional) alter the kernel size used and the number of CNN layers
+            (through the length of the kernel size vector).
+        intermediate_channel_sizes: (optional) alter the intermediate channel sizes
+            used, should have length = len(kernel_sizes) - 1.
+        stride_sizes = Optional alter the stride used, either a vector of
+            len = len(kernel_sizes) or a single integer, in which case the same stride
+            is used in every convolution.
+
+    Returns:
+        nn.Module object that contains a sequence of CNN and max_pool layer
+            and finally a global average pooling layer.
+    """
+    aggregator_out_shape = (
+        in_channels,
+        num_timepoints,
+    )
+    if kernel_sizes is None:
+        kernel_sizes = [
+            min(9, aggregator_out_shape[-1]),
+            min(5, int(aggregator_out_shape[-1] / 2)),
+        ]
+    if intermediate_channel_sizes is None:
+        intermediate_channel_sizes = [64]
+    assert len(intermediate_channel_sizes) == len(kernel_sizes) - 1, (
+        "Provided kernel size list should be exactly one element longer "
+        "than channel size list."
+    )
+    intermediate_channel_sizes += [output_dim]
+    if isinstance(stride_sizes, List):
+        assert len(stride_sizes) == len(kernel_sizes), (
+            "Provided stride size list should be have the same size as"
+            "the kernel size list."
+        )
+    else:
+        stride_sizes = [stride_sizes] * len(kernel_sizes)
+
+    non_causal_layers = []
+    for ll in range(len(kernel_sizes)):
+        print(aggregator_out_shape)
+        conv_layer = nn.Conv1d(
+            in_channels=in_channels if ll == 0 else intermediate_channel_sizes[ll - 1],
+            out_channels=intermediate_channel_sizes[ll],
+            kernel_size=kernel_sizes[ll],
+            stride=stride_sizes[ll],
+            padding='same',
+        )
+        maxpool = nn.MaxPool1d(kernel_size=2 if aggregator_out_shape[-1] > 2 else 1)
+        non_causal_layers += [conv_layer, activation, maxpool]
+        aggregator_out_shape = (
+            intermediate_channel_sizes[ll],
+            int(
+                calculate_filter_output_size(
+                    aggregator_out_shape[-1],
+                    (kernel_sizes[ll] - 1) / 2,
+                    1,
+                    kernel_sizes[ll],
+                    stride_sizes[ll],
+                )
+                / 2
+            ),
+        )
+    print(aggregator_out_shape)
+    aggregator = nn.Sequential(*non_causal_layers, nn.AdaptiveAvgPool1d(1))
+    return aggregator
 
 
 class CausalCNNEmbedding(nn.Module):
@@ -50,12 +151,17 @@ class CausalCNNEmbedding(nn.Module):
         output_dim: int = 20,
     ):
         """Embedding network that uses 1D causal convolutions
+
         This is a simplified version of the architecture introduced for
         the speech recognition task in the WaveNet paper (van den Oord, et al. (2016))
 
         After several dilated causal convolutions (that maintain the dimensionality
         of the input), an aggregator network is used to bring down the dimensionality.
-        You can provide an aggregator network that you deem reasonable for your data
+        You can provide an aggregator network that you deem reasonable for your data.
+        If you do not provide an aggregator network yourself, a default aggregator
+        is used. This default aggregator is based on the WaveNet paper's description
+        of their Speech Recognition Task, and uses non-causal convolutions and pooling
+        layers, and global average poolingg to obtain a final low dimensional embedding.
 
         Args:
             input_shape: Dimensionality of the input e.g. (num_timepoints,),
@@ -86,8 +192,8 @@ class CausalCNNEmbedding(nn.Module):
         assert len(input_shape) == 1, "Currently only 1D causal CNNs are supported."
         self.input_shape = (in_channels, *input_shape)
 
-        tot_timepoints = input_shape[0]
-        assert tot_timepoints >= pool_kernel_size, (
+        total_timepoints = input_shape[0]
+        assert total_timepoints >= pool_kernel_size, (
             "Please ensure that the pool kernel size is not "
             "larger than the number of observed timepoints."
         )
@@ -121,47 +227,16 @@ class CausalCNNEmbedding(nn.Module):
         if aggregator is None:
             aggregator_out_shape = (
                 out_channels_per_layer[-1],
-                int(tot_timepoints / pool_kernel_size),
+                int(total_timepoints / pool_kernel_size),
             )
             assert aggregator_out_shape[-1] > 1, (
-                "Your dimensionality is allready small,"
+                "Your dimensionality is already small,"
                 "Please ensure a larger input size or use a custom aggregator."
             )
-            non_causal_kernel_sizes = [
-                min(9, aggregator_out_shape[-1]),
-                min(5, int(aggregator_out_shape[-1] / 2)),
-            ]
-            non_causal_channel_sizes = [64, output_dim]
-            non_causal_layers = []
-            for ll in range(len(non_causal_kernel_sizes)):
-                conv_layer = nn.Conv1d(
-                    in_channels=out_channels_per_layer[-1]
-                    if ll == 0
-                    else non_causal_channel_sizes[ll - 1],
-                    out_channels=non_causal_channel_sizes[ll],
-                    kernel_size=non_causal_kernel_sizes[ll],
-                    stride=1,
-                    padding='same',
-                )
-                maxpool = nn.AvgPool1d(
-                    kernel_size=2 if aggregator_out_shape[-1] > 2 else 1
-                )
-                non_causal_layers += [conv_layer, activation, maxpool]
-                aggregator_out_shape = (
-                    non_causal_channel_sizes[ll],
-                    int(
-                        calculate_filter_output_size(
-                            aggregator_out_shape[-1],
-                            1,
-                            1,
-                            non_causal_kernel_sizes[ll],
-                            1,
-                        )
-                        / 2
-                    ),
-                )
-            aggregator = nn.Sequential(
-                *non_causal_layers, nn.AvgPool1d(kernel_size=aggregator_out_shape[-1])
+            aggregator = WaveNetSRLikeAggregator(
+                aggregator_out_shape[0],
+                aggregator_out_shape[-1],
+                output_dim=output_dim,
             )
         self.aggregation = aggregator
 
@@ -171,6 +246,6 @@ class CausalCNNEmbedding(nn.Module):
         x = self.causal_cnns(x)
         x = self.pooling_layer(x)
         x = self.aggregation(x)
-        # ensure flattening when aggregator uses GAP
+        # ensure flattening when aggregator uses global average pooling
         x = x.view(batch_size, -1)
         return x
