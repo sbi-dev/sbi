802f6c730adc6b31ddf40d023147a91d60bebede
Matthijspals <matthijs-pals@hotmail.com>
2025-03-21 11:30:37 +0100

    add warning that scan does not support backwards pass yet

--- sbi/neural_nets/embedding_nets/lru.py
+++ sbi/neural_nets/embedding_nets/lru.py
@@ -1,3 +1,4 @@
+import warnings
 from math import sqrt
 from typing import Callable, Optional
 
@@ -27,7 +28,7 @@ class LRUEmbedding(nn.Module):
         state_dim: int = 20,
         hidden_dim: int = 40,
         num_blocks: int = 2,
-        r_min : float=0.0,
+        r_min: float = 0.0,
         r_max=1.0,
         phase_max=2 * np.pi,
         bidirectional: bool = True,
@@ -66,7 +67,6 @@ class LRUEmbedding(nn.Module):
                 inputs of each LRU Block, see `LRUBlock.forward()`.
             aggregate_fcn: Function to aggregate the sequence of hidden states. Can be
                 `"last_step"` to take the last hidden state, `"mean"` to take the mean
-
         """
         super().__init__()
 
@@ -233,6 +233,13 @@ class LRU(nn.Module):
             )
         if mode not in ("loop", "scan"):
             raise ValueError(f"Invalid {mode=}. Must be 'loop' or 'scan'.")
+        if mode == "scan":
+            warnings.warn(
+                "The scan mode currently does not support a backward pass, "
+                "the code will be updated once this is pytorch native",
+                stacklevel=2,
+            )
+
         self.state_dim = state_dim
         self.bidirectional = bidirectional
         self.mode = mode
