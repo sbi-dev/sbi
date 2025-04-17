269b5a185cfba5ced3a2598a29ab583e49822626
Matthijs Pals <matthijs-pals@hotmail.com>
2025-04-17 12:25:35 +0200

    add xfail condition python 3.13

--- tests/embedding_net_test.py
+++ tests/embedding_net_test.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 
 import math
+import sys
 
 import pytest
 import torch
@@ -550,8 +551,8 @@ def test_1d_ResNet_fc_embedding_net(input_shape, n_blocks, c_internal, c_hidden_
         pytest.param(
             "scan",
             marks=pytest.mark.xfail(
-                condition=tuple(map(int, torch.__version__.split('.')[:2])) < (2, 5),
-                reason="PyTorch's associative_scan only exists for torch >= 2.5",
+                condition=tuple(map(int, torch.__version__.split('.')[:2])) < (2, 5) or sys.version_info >= (3, 13),
+                reason="PyTorch's associative_scan only exists for torch >= 2.5 and Python >= 3.13",
                 strict=True,
             ),
         ),
@@ -603,8 +604,8 @@ def test_lru_isolated(
         pytest.param(
             "scan",
             marks=pytest.mark.xfail(
-                condition=tuple(map(int, torch.__version__.split('.')[:2])) < (2, 5),
-                reason="PyTorch's associative_scan only exists for torch >= 2.5",
+                condition=tuple(map(int, torch.__version__.split('.')[:2])) < (2, 5) or sys.version_info >= (3, 13),
+                reason="PyTorch's associative_scan only exists for torch >= 2.5 and Python >= 3.13",
                 strict=True,
             ),
         ),
@@ -663,8 +664,8 @@ def test_lru_block_isolated(
         pytest.param(
             "scan",
             marks=pytest.mark.xfail(
-                condition=tuple(map(int, torch.__version__.split('.')[:2])) < (2, 5),
-                reason="PyTorch's associative_scan only exists for torch >= 2.5",
+                condition=tuple(map(int, torch.__version__.split('.')[:2])) < (2, 5) or sys.version_info >= (3, 13),
+                reason="PyTorch's associative_scan only exists for torch >= 2.5 and Python >= 3.13",
                 strict=True,
             ),
         ),
@@ -787,8 +788,8 @@ def test_lru_pipeline(embedding_feat_dim: int = 17):
 
 
 @pytest.mark.xfail(
-    condition=tuple(map(int, torch.__version__.split('.')[:2])) < (2, 5),
-    reason="PyTorch's associative_scan only exists for torch >= 2.5",
+    condition=tuple(map(int, torch.__version__.split('.')[:2])) < (2, 5) or sys.version_info >= (3, 13),
+    reason="PyTorch's associative_scan only exists for torch >= 2.5 and Python >= 3.13",
     strict=True,
 )
 @pytest.mark.filterwarnings("ignore:Torchinductor")
