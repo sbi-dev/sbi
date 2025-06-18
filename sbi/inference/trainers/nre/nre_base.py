ca074ff0b3a873474dd856d013178a9c8ff3bbf5
abelaba <se.abel.abate@gmail.com>
2025-06-18 15:09:57 +0300

    Remove alias import for RatioEstimator

--- sbi/inference/trainers/nre/nre_base.py
+++ sbi/inference/trainers/nre/nre_base.py
@@ -18,7 +18,7 @@ from sbi.inference.posteriors.importance_posterior import ImportanceSamplingPost
 from sbi.inference.potentials import ratio_estimator_based_potential
 from sbi.inference.trainers.base import NeuralInference
 from sbi.neural_nets import classifier_nn
-from sbi.neural_nets.ratio_estimators import RatioEstimator as RatioEstimatorNeuralNet
+from sbi.neural_nets.ratio_estimators import RatioEstimator
 from sbi.utils import (
     check_estimator_arg,
     check_prior,
@@ -30,7 +30,7 @@ from sbi.utils.torchutils import repeat_rows
 class RatioEstimatorBuilder(Protocol):
     """Protocol for building a ratio estimator from data."""
 
-    def __call__(self, theta: Tensor, x: Tensor) -> RatioEstimatorNeuralNet:
+    def __call__(self, theta: Tensor, x: Tensor) -> RatioEstimator:
         """Build a ratio estimator from theta and x, which mainly inform the
         shape of the input and the condition to the neural network.
 
