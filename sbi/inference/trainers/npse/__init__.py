870a7544dc344ec3f04511ed26f4f4f152d26337
manuelgloeckler <manu.gloeckler@hotmail.de>
2025-07-16 13:50:29 +0200

    Backward compatibility for imports of NPSE and FMPE

---
+++ sbi/inference/trainers/npse/__init__.py
@@ -0,0 +1,4 @@
+# This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
+# under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
+# TODO: This folder is deprecated, remove it in the future.
+from sbi.inference.trainers.vfpe.npse import NPSE
