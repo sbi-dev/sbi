8d00f854b49eb692b9070be4c0bb9cde5f8f4853
abelaba <se.abel.abate@gmail.com>
2025-07-18 15:28:40 +0300

    add license header

--- tests/posterior_parameters_test.py
+++ tests/posterior_parameters_test.py
@@ -1,3 +1,6 @@
+# This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
+# under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
+
 import inspect
 from dataclasses import fields
 
