dfd51e49e4c10f79c10c35c72c3569665b6e4dbb
manuelgloeckler <38903899+manuelgloeckler@users.noreply.github.com>
2025-08-07 15:07:40 +0200

    Update sbi/neural_nets/factory.py
    Co-authored-by: Jan <janfb@users.noreply.github.com>

--- sbi/neural_nets/factory.py
+++ sbi/neural_nets/factory.py
@@ -530,9 +530,9 @@ def posterior_flow_nn(
 
     if z_score_theta is not None:
         raise ValueError(
-            "z_score_theta is not supported for FMPE. You can z-score"
-            "the inputs manually but doing so in the forward pass can"
-            "negatively impact performance."
+            "z_score_theta is not supported for FMPE. For simulator "
+            "parameters with highly different scales, we recommend "
+            "z-scoring the inputs manually beforehand."
         )
 
     kwargs = dict(
