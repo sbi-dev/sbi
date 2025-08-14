d1c13468c7e11dbefb47d7734e9fb65c589fbf1d
Jan <jan.boelts@mailbox.org>
2025-08-14 14:41:48 +0200

    add missing import

--- sbi/neural_nets/estimators/base.py
+++ sbi/neural_nets/estimators/base.py
@@ -2,7 +2,7 @@
 # under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
 
 from abc import ABC, abstractmethod
-from typing import Optional, Protocol, Tuple, TypeVar
+from typing import Optional, Protocol, Tuple, TypeVar, Union
 
 import torch
 from torch import Tensor, nn
