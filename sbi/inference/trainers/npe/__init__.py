1f8f054e5185a2ab09da584db8a977ebb1676d62
jsvetter <juliussimonvetter@gmail.com>
2026-02-23 20:45:56 +0100

    run mini bm and add imports

--- sbi/inference/trainers/npe/__init__.py
+++ sbi/inference/trainers/npe/__init__.py
@@ -6,6 +6,7 @@ from sbi.inference.trainers.npe.npe_a import NPE_A  # noqa: F401
 from sbi.inference.trainers.npe.npe_b import NPE_B  # noqa: F401
 from sbi.inference.trainers.npe.npe_base import PosteriorEstimatorTrainer  # noqa: F401
 from sbi.inference.trainers.npe.npe_c import NPE_C  # noqa: F401
+from sbi.inference.trainers.npe.npe_pfn import NPE_PFN  # noqa: F401
 
 SNPE_A = NPE_A
 SNPE_B = NPE_C
