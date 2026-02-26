ce192fbec9250adc4e49d43b891c5481da1a8f50
jsvetter <juliussimonvetter@gmail.com>
2026-02-26 12:14:43 +0100

    fix gaussian_linear variance/std consistency

--- tests/mini_sbibm/gaussian_linear.py
+++ tests/mini_sbibm/gaussian_linear.py
@@ -63,7 +63,7 @@ class GaussianLinear(Task):
         posterior = true_posterior_linear_gaussian_mvn_prior(
             x_o,
             torch.zeros(self.dim),
-            self.simulator_scale**2 * torch.eye(self.dim),
+            self.simulator_scale * torch.eye(self.dim),
             torch.zeros(self.dim),
             torch.eye(self.dim),
         )
