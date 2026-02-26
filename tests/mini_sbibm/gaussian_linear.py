fix scale (variance) to std

tests/mini_sbibm/gaussian_linear.py
@@ -63,7 +63,7 @@ class GaussianLinear(Task):
         posterior = true_posterior_linear_gaussian_mvn_prior(
             x_o,
             torch.zeros(self.dim),
-            self.simulator_scale * torch.eye(self.dim),
+            self.simulator_scale**2 * torch.eye(self.dim),
             torch.zeros(self.dim),
             torch.eye(self.dim),
         )
@@ -115,5 +115,5 @@ class GaussianLinear(Task):
         """
         return partial(
             diagonal_linear_gaussian,
-            std=self.simulator_scale,
+            std=self.simulator_scale**0.5,
         )