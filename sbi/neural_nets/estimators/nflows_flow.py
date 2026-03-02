fix shape broadcasting in nflows wrapper

sbi/neural_nets/estimators/nflows_flow.py
@@ -80,47 +80,56 @@ class NFlowsFlow(ConditionalDensityEstimator):
 
         Args:
             input: Inputs to evaluate the log probability on. Of shape
-                `(sample_dim, batch_dim, *event_shape)`.
+                `(sample_dim, batch_dim, *event_shape)` or
+                `(batch_dim, *event_shape)`.
             condition: Conditions of shape `(batch_dim, *event_shape)` or
-                 `(sample_dim, batch_dim, *event_shape)`.
+                `(sample_dim, batch_dim, *event_shape)`.
             **kwargs: Additional arguments.
 
-        Raises:
-            AssertionError: If `input_batch_dim != condition_batch_dim`.
-
         Returns:
-            Sample-wise log probabilities, shape `(input_sample_dim, input_batch_dim)`.
+            Sample-wise log probabilities, shape `(sample_dim, batch_dim)`.
         """
+        input_event_dims = len(self.input_shape)
+        condition_event_dims = len(self.condition_shape)
+
+        # Allow input without explicit sample_dim.
+        has_sample_dim = input.dim() > input_event_dims + 1
+        if not has_sample_dim:
+            input = input.unsqueeze(0)
+
         input_sample_dim = input.shape[0]
         input_batch_dim = input.shape[1]
 
-        if (
-            condition.ndim == input.ndim
-            and condition.shape[0] == input_sample_dim
-            and condition.shape[1] == input_batch_dim
-        ):
-            input = input.reshape(input_sample_dim * input_batch_dim, -1)
-            condition = condition.reshape(input_sample_dim * input_batch_dim, -1)
-            log_probs = self.net.log_prob(input, context=condition)
+        # Allow condition with or without sample_dim.
+        condition_has_sample_dim = condition.dim() > condition_event_dims + 1
+        if condition_has_sample_dim:
+            condition_batch_dim = condition.shape[1]
         else:
             condition_batch_dim = condition.shape[0]
-            condition_event_dims = len(condition.shape[1:])
 
-            assert condition_batch_dim == input_batch_dim, (
-                f"Batch shape of condition {condition_batch_dim} and input "
-                f"{input_batch_dim} do not match."
+        # Broadcast batch dimensions.
+        batch_dim = torch.broadcast_shapes((input_batch_dim,), (condition_batch_dim,))[
+            0
+        ]
+        input = input.expand(input_sample_dim, batch_dim, *self.input_shape)
+
+        if condition_has_sample_dim:
+            condition = condition.expand(
+                input_sample_dim, batch_dim, *self.condition_shape
             )
-
-            # Nflows needs to have a single batch dimension for condition and input.
-            input = input.reshape((input_batch_dim * input_sample_dim, -1))
-
-            # Repeat the condition to match `input_batch_dim * input_sample_dim`.
-            # Tuple of 1s, e.g. (1, 1, 1)
+            condition = condition.reshape(
+                input_sample_dim * batch_dim, *self.condition_shape
+            )
+        else:
+            condition = condition.expand(batch_dim, *self.condition_shape)
             ones_for_event_dims = (1,) * condition_event_dims
             condition = condition.repeat(input_sample_dim, *ones_for_event_dims)
 
-            log_probs = self.net.log_prob(input, context=condition)
-        return log_probs.reshape((input_sample_dim, input_batch_dim))
+        # Nflows needs a single batch dimension for condition and input.
+        input = input.reshape((batch_dim * input_sample_dim, -1))
+
+        log_probs = self.net.log_prob(input, context=condition)
+        return log_probs.reshape((input_sample_dim, batch_dim))
 
     def loss(self, input: Tensor, condition: Tensor) -> Tensor:
         r"""Return the negative log-probability for training the density estimator.